143e113324cae59ca0834cf671975fc4973417b8
Lars Berger <lars.berger@yahoo.com>
2020-01-16 01:24:11 +0100

    feat: created WindowManager class for handling window-related actions

---
+++ LarsWM/WindowManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LarsWM
+{
+    enum FocusDirection
+    {
+        Up,
+        Down,
+        Left,
+        Right,
+    }
+
+    class WindowManager
+    {
+
+    public void ShiftFocusInDirection(FocusDirection direction)
+    { }
+
+    public void ShiftFocusToWorkspace(Workspace workspace)
+    { }
+
+    public void MoveFocusedWindowToWorkspace(Window window, Workspace workspace)
+    { }
+
+    public void MoveFocusedWindowInDirection(Window window, FocusDirection direction)
+    { }
+
+    }
+}
+
