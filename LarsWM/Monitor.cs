6efc72596b89b801b1098cfa0cbef75714f2d938
Lars <lars.berger@yahoo.com>
2020-01-21 01:39:42 +0100

    feat: created Monitor class for holding properties about user's monitors

---
+++ LarsWM/Monitor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LarsWM
+{
+    public class Monitor
+    {
+        public int Index { get; private set; }
+        public string Name => Screen.DeviceName;
+        public int Width => Screen.WorkingArea.Width;
+        public int Height => Screen.WorkingArea.Height;
+        public int X => Screen.WorkingArea.X;
+        public int Y => Screen.WorkingArea.Y;
+
+        public Screen Screen { get; }
+
+        public Monitor(int index, Screen screen)
+        {
+            Index = index;
+            Screen = screen;
+        }
+    }
+}
