19a2762c9fc076e3524f1f6659c772d155d9bec1
Lars <lars.berger@yahoo.com>
2020-01-24 17:57:25 +0100

    feat: started methods for getting style & extended style of a Window

--- LarsWM/Window.cs
+++ LarsWM/Window.cs
@@ -6,5 +6,43 @@ namespace LarsWM
 {
     public class Window
     {
+        public IntPtr Hwnd { get; set; }
+
+        public Window(IntPtr hwnd)
+        {
+            Hwnd = hwnd;
+        }
+
+        /// <summary>
+        /// This window's style flags.
+        /// </summary>
+        public WindowStyleFlags Style
+        {
+            get
+            {
+                return unchecked((WindowStyleFlags)GetWindowLongPtr(_hwnd, (int)(GWL.GWL_STYLE)).ToInt64());
+            }
+            set
+            {
+                SetWindowLong(_hwnd, (int)GWL.GWL_STYLE, (int)value);
+            }
+
+        }
+
+        /// <summary>
+        /// This window's extended style flags.
+        /// </summary>
+        [CLSCompliant(false)]
+        public WindowExStyleFlags ExtendedStyle
+        {
+            get
+            {
+                return unchecked((WindowExStyleFlags)GetWindowLongPtr(_hwnd, (int)(GWL.GWL_EXSTYLE)).ToInt64());
+            }
+            set
+            {
+                SetWindowLong(_hwnd, (int)GWL.GWL_EXSTYLE, (int)value);
+            }
+        }
     }
 }
