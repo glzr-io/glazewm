0aa406543bc81fe1fa662f7465ce89789fb0d2b2
Lars <lars.berger@yahoo.com>
2020-01-26 01:21:42 +0100

    feat: added Process property to Window model

--- LarsWM/Window.cs
+++ LarsWM/Window.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
+using static LarsWM.WindowsApi.WindowsApiFacade;
 
 namespace LarsWM
 {
@@ -13,36 +15,38 @@ namespace LarsWM
             Hwnd = hwnd;
         }
 
+        public Process Process => GetProcessOfWindow(this);
+
         /// <summary>
         /// This window's style flags.
         /// </summary>
-        public WindowStyleFlags Style
-        {
-            get
-            {
-                return unchecked((WindowStyleFlags)GetWindowLongPtr(_hwnd, (int)(GWL.GWL_STYLE)).ToInt64());
-            }
-            set
-            {
-                SetWindowLong(_hwnd, (int)GWL.GWL_STYLE, (int)value);
-            }
+        //public WindowStyleFlags Style
+        //{
+        //    get
+        //    {
+        //        return unchecked((WindowStyleFlags)GetWindowLongPtr(_hwnd, (int)(GWL.GWL_STYLE)).ToInt64());
+        //    }
+        //    set
+        //    {
+        //        SetWindowLong(_hwnd, (int)GWL.GWL_STYLE, (int)value);
+        //    }
 
-        }
+        //}
 
         /// <summary>
         /// This window's extended style flags.
         /// </summary>
-        [CLSCompliant(false)]
-        public WindowExStyleFlags ExtendedStyle
-        {
-            get
-            {
-                return unchecked((WindowExStyleFlags)GetWindowLongPtr(_hwnd, (int)(GWL.GWL_EXSTYLE)).ToInt64());
-            }
-            set
-            {
-                SetWindowLong(_hwnd, (int)GWL.GWL_EXSTYLE, (int)value);
-            }
-        }
+        //[CLSCompliant(false)]
+        //public WindowExStyleFlags ExtendedStyle
+        //{
+        //    get
+        //    {
+        //        return unchecked((WindowExStyleFlags)GetWindowLongPtr(_hwnd, (int)(GWL.GWL_EXSTYLE)).ToInt64());
+        //    }
+        //    set
+        //    {
+        //        SetWindowLong(_hwnd, (int)GWL.GWL_EXSTYLE, (int)value);
+        //    }
+        //}
     }
 }
