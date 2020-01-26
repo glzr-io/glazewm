95cdf6012b4a77ab6cf9bf1cc3a0877ad9f7822d
Lars <lars.berger@yahoo.com>
2020-01-26 01:53:02 +0100

    feat: created UserConfigService with hardcoded values for windows to ignore

---
+++ LarsWM/UserConfig/UserConfigService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LarsWM.UserConfig
+{
+    public class UserConfigService
+    {
+        public static string[] WindowClassesToIgnore = new string[] {
+            "TaskManagerWindow",
+            "MSCTFIME UI",
+            "SHELLDLL_DefView",
+            "LockScreenBackstopFrame",
+            "Progman",
+        };
+
+        public static string[] ProcessNamesToIgnore = new string[] {
+            "SearchUI",
+            "ShellExperienceHost",
+            "LockApp",
+            "PeopleExperienceHost",
+            "StartMenuExperienceHost",
+        };
+    }
+}
