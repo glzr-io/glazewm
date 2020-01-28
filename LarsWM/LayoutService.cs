d3e9bd4b9fd83624e5d0ff6f71ddddf44f3335d7
Lars <lars.berger@yahoo.com>
2020-01-28 19:12:42 +0100

    feat: added method for calculating window positions when forcing initial layout

--- LarsWM/LayoutService.cs
+++ LarsWM/LayoutService.cs
@@ -1,14 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace LarsWM
 {
     class LayoutService
     {
-        public void CalculateInitialLayout()
+        public static List<WindowLocation> CalculateInitialLayout(Monitor monitor, List<Window> windows)
         {
-            
+            var windowLocations = new List<WindowLocation>();
+
+            //var windowWidth = (monitor.Width - UserConfig.UserConfigService.InnerGap) / windows.Count;
+            var windowWidth = (monitor.Width) / windows.Count();
+
+            var index = 1;
+            foreach(var window in windows)
+            {
+                var newWindowLocation = new WindowLocation(index * windowWidth, 0, windowWidth, monitor.Height);
+                windowLocations.Add(newWindowLocation);
+                index++;
+            }
+
+            return windowLocations;
         }
     }
 }
