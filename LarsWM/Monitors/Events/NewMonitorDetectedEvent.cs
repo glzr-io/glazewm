5d046fe54100c1efa74fb5c6ab759bc434024e4d
Lars <lars.berger@yahoo.com>
2020-01-30 20:52:38 +0100

    feat: added events for when monitor is added and when monitor is detected

---
+++ LarsWM/Monitors/Events/NewMonitorDetectedEvent.cs
@@ -0,0 +1,11 @@
+﻿using LarsWM.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LarsWM.Monitors.Events
+{
+    class NewMonitorDetectedEvent : IEvent
+    {
+    }
+}
