ee5f08deaf32955ce166eb45b446b5edd9ecceb6
Lars <lars.berger@yahoo.com>
2020-01-30 21:41:56 +0100

    feat: created basic UserConfig class

---
+++ LarsWM/UserConfigs/UserConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LarsWM.UserConfigs
+{
+    class UserConfig
+    {
+        public List<string> WindowClassesToIgnore = new List<string>();
+
+        public List<string> ProcessNamesToIgnore = new List<string>();
+
+        public int InnerGap = 20;
+        public int OuterGap = 20;
+    }
+}
