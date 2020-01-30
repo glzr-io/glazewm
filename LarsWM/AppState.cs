13ccce22d293d2c6d31a52724c1e0e5abfee6e8d
Lars <lars.berger@yahoo.com>
2020-01-30 20:43:54 +0100

    feat: added AppState class for holding application state

---
+++ LarsWM/AppState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LarsWM
+{
+    public class AppState
+    {
+        // Not sure whether to change this to a BehaviorSubject.
+        public List<Monitor> Monitors = new List<Monitor>();
+        public List<Workspace> Workspaces => Monitors.Select(m => m.WorkspacesInMonitor) as List<Workspace>;
+        public List<Window> Windows => Workspaces.Select(w => w.WindowsInWorkspace) as List<Window>;
+    }
+}
