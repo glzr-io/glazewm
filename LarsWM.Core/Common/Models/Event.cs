cf5a56a59eefea393c2c4cee6fe86d7334c2a739
Lars <lars.berger@yahoo.com>
2020-02-02 00:45:07 +0100

    feat: created Event class for all events to inherit from

---
+++ LarsWM.Core/Common/Models/Event.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LarsWM.Core.Common.Models
+{
+    public class Event
+    {
+        public string Name { get; set; }
+
+        public Event()
+        {
+            Name = GetType().Name;
+        }
+    }
+}
