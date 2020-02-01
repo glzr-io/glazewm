f0e0fd5139a2a75e81d62eeed03a03a4f0b985e9
Lars <lars.berger@yahoo.com>
2020-02-02 00:44:52 +0100

    feat: created Command class for all commands to inherit from

---
+++ LarsWM.Core/Common/Models/Command.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LarsWM.Core.Common.Models
+{
+    public class Command
+    {
+        public string Name { get; set; }
+
+        public Command()
+        {
+            Name = GetType().Name;
+        }
+    }
+}
