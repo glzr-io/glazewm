523f1500c22de49dde3ca43e373d0e04c0905d7c
Lars <lars.berger@yahoo.com>
2020-02-02 19:38:50 +0100

    feat: created unimplemented ReadUserConfigHandler

---
+++ LarsWM.Core/UserConfigs/CommandHandlers/ReadUserConfigHandler.cs
@@ -0,0 +1,14 @@
+﻿using LarsWM.Core.Common.Models;
+using LarsWM.Core.UserConfigs.Commands;
+using System;
+
+namespace LarsWM.Core.UserConfigs.CommandHandlers
+{
+    class ReadUserConfigHandler : ICommandHandler<ReadUserConfigCommand>
+    {
+        public void Handle(ReadUserConfigCommand command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
