5b9c0c411911586e2851d92e06999e7595bd385c
Lars <lars.berger@yahoo.com>
2020-02-02 17:55:57 +0100

    feat: added unimplemented AddMonitorHandler

---
+++ LarsWM.Core/Monitors/CommandHandlers/AddMonitorHandler.cs
@@ -0,0 +1,16 @@
+﻿using LarsWM.Core.Common.Models;
+using LarsWM.Core.Monitors.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LarsWM.Core.Monitors.CommandHandlers
+{
+    class AddMonitorHandler : ICommandHandler<AddMonitorCommand>
+    {
+        public void Handle(AddMonitorCommand command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
