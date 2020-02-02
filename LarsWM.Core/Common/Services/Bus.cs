346915e22e25771c8b6e738867207bd79817b56c
Lars <lars.berger@yahoo.com>
2020-02-02 13:47:30 +0100

    refactor: simplified logic in Bus.Invoke method

--- LarsWM.Core/Common/Services/Bus.cs
+++ LarsWM.Core/Common/Services/Bus.cs
@@ -7,37 +7,37 @@ using System.Text;
 namespace LarsWM.Core.Common.Services
 {
     /// <summary>
-    /// The event bus
+    /// Bus facilitates communication to command and event handlers.
     /// </summary>
     public sealed class Bus : IBus
     {
-        private static readonly List<Type> RegisteredCommandHandlers = new List<Type>();
-        private static readonly List<Type> RegisteredEventHandlers = new List<Type>();
+        private List<Type> _registeredCommandHandlers = new List<Type>();
+        private List<Type> _registeredEventHandlers = new List<Type>();
 
         /// <summary>
-        /// Sends command to appropriate command handler.
+        /// Sends command to appropriate command handlers.
         /// </summary>
         public void Invoke<T>(T command) where T : Command
         {
-            var commandType = typeof(T);
+            // Create a Type object representing the generic ICommandHandler type.
+            var commandHandlerGeneric = typeof(ICommandHandler<>);
+
+            // Create a Type object representing the constructed ICommandHandler generic.
+            var handlerTypeToCall = commandHandlerGeneric.MakeGenericType(command.GetType());
+
+            var handlersToCall = _registeredCommandHandlers.Where(handler => handlerTypeToCall.IsAssignableFrom(handler));
 
             // TODO: add centralised error handling here?
             // TODO: create CommandResponse interface with State (failure, success) and Data (any return value from command)?
-            foreach (var handler in RegisteredCommandHandlers)
+            foreach (var handler in handlersToCall)
             {
-                var handlerInterface = handler.GetInterfaces()[0];
-                var handlerCommandType = handlerInterface.GetGenericArguments()[0];
-
-                if (handlerCommandType == commandType)
-                {
-                    ICommandHandler<T> handlerInstance = Program.ServiceProvider.GetService(handler) as ICommandHandler<T>;
-                    handlerInstance.Handle(command);
-                }
+                ICommandHandler<T> handlerInstance = Program.ServiceProvider.GetService(handler) as ICommandHandler<T>;
+                handlerInstance.Handle(command);
             }
         }
 
         /// <summary>
-        /// Sends event to appropriate event handler.
+        /// Sends event to appropriate event handlers.
         /// </summary>
         public void RaiseEvent<T>(T @event) where T : Event
         {
@@ -46,12 +46,12 @@ namespace LarsWM.Core.Common.Services
 
         public void RegisterCommandHandler<T>()
         {
-            RegisteredCommandHandlers.Add(typeof(T));
+            _registeredCommandHandlers.Add(typeof(T));
         }
 
         public void RegisterEventHandler<T>()
         {
-            RegisteredEventHandlers.Add(typeof(T));
+            _registeredEventHandlers.Add(typeof(T));
         }
     }
 }
