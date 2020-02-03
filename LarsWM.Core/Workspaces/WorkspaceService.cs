854f56eb874d598e90c37819c2f643feb6ea7ec7
Lars <lars.berger@yahoo.com>
2020-02-03 23:23:07 +0100

    feat: created WorkspaceService for holding workspace-related state

---
+++ LarsWM.Core/Workspaces/WorkspaceService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LarsWM.Core.Workspaces
+{
+    class WorkspaceService
+    {
+        public List<Workspace> Workspaces { get; set; } = new List<Workspace>();
+    }
+}
