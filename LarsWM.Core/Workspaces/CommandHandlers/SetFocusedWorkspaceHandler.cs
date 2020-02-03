f1ba419e113cff9e9320fae490d72cc025c89718
Lars <lars.berger@yahoo.com>
2020-02-03 01:40:45 +0100

    feat: added partially implemented SetFocusedWorkspaceCommand and its handler

---
+++ LarsWM.Core/Workspaces/CommandHandlers/SetFocusedWorkspaceHandler.cs
@@ -0,0 +1,32 @@
+﻿using LarsWM.Core.Common.Models;
+using LarsWM.Core.Monitors;
+using LarsWM.Core.Workspaces.Commands;
+
+namespace LarsWM.Core.Workspaces.CommandHandlers
+{
+    class SetFocusedWorkspaceHandler : ICommandHandler<SetFocusedWorkspaceCommand>
+    {
+        private AppState _appState;
+        private MonitorService _monitorService;
+
+        public SetFocusedWorkspaceHandler(AppState appState, MonitorService monitorService)
+        {
+            _appState = appState;
+            _monitorService = monitorService;
+        }
+
+        public void Handle(SetFocusedWorkspaceCommand command)
+        {
+            throw new System.NotImplementedException();
+
+            var workspace = _workspaceService.GetWorkspaceById(command.WorkspaceId);
+            var parentMonitor = _monitorService.GetMonitorOfWorkspace(workspace);
+            parentMonitor.DisplayedWorkspace = workspace;
+
+            if (workspace.WindowsInWorkspace.Count() > 0)
+            {
+                // TODO: Set focus through windows api
+            }
+        }
+    }
+}
---
+++ LarsWM.Core/Workspaces/Commands/SetFocusedWorkspaceCommand.cs
@@ -0,0 +1,15 @@
+﻿using LarsWM.Core.Common.Models;
+using System;
+
+namespace LarsWM.Core.Workspaces.Commands
+{
+    class SetFocusedWorkspaceCommand : Command
+    {
+        public Guid WorkspaceId { get; private set; }
+
+        public SetFocusedWorkspaceCommand(Guid workspaceId)
+        {
+            WorkspaceId = workspaceId;
+        }
+    }
+}
