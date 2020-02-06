e84ffc3f0327340fee3796d6a1b3f6c501e2c0a8
Lars <lars.berger@yahoo.com>
2020-02-06 22:34:26 +0100

    feat: added KbLLHookStruct class definition to WindowsApiService

--- LarsWM.Core/Common/Services/WindowsApiService.cs
+++ LarsWM.Core/Common/Services/WindowsApiService.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Windows.Forms;
 
 namespace LarsWM.Core.WindowsApi
 {
@@ -180,7 +181,7 @@ namespace LarsWM.Core.WindowsApi
         [DllImport("user32.dll")]
         public static extern IntPtr GetShellWindow();
 
-        public delegate IntPtr HookProc(int code, UIntPtr wParam, IntPtr lParam);
+        public delegate IntPtr HookProc(int code, IntPtr wParam, IntPtr lParam);
 
         public enum HookType : int
         {
@@ -188,13 +189,26 @@ namespace LarsWM.Core.WindowsApi
             WH_MOUSE_LL = 14
         }
 
+        /// <summary>
+        /// Contains information about a low-level keyboard input event.
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential)]
+        public class KbLLHookStruct
+        {
+            public Keys vkCode;
+            public int scanCode;
+            public int flags;
+            public int time;
+            public int dwExtraInfo;
+        }
+
         [DllImport("user32.dll")]
         public static extern IntPtr SetWindowsHookEx(HookType hookType, [MarshalAs(UnmanagedType.FunctionPtr)] HookProc lpfn, IntPtr hMod, int dwThreadId);
 
         [DllImport("user32.dll")]
-        public static extern IntPtr CallNextHookEx([Optional] IntPtr hhk, int nCode, UIntPtr wParam, IntPtr lParam);
+        public static extern IntPtr CallNextHookEx([Optional] IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
 
         [DllImport("user32.dll")]
-        public static extern short GetKeyState(System.Windows.Forms.Keys nVirtKey);
+        public static extern short GetKeyState(Keys nVirtKey);
     }
 }
