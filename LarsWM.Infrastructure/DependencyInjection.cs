1bafbbfa4e470d53f04e2d00651862f9b88cae94
Lars <lars.berger@yahoo.com>
2020-02-11 09:05:53 +0100

    feat(infra): added static class for configuring services for infra layer

---
+++ LarsWM.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using LarsWM.Infrastructure.Bussing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LarsWM.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IBus, Bus>();
+
+            // TODO: Change WindowsApiFacade & WindowsApiService to be compatible with DI.
+            // TODO: Add service locator to infrastructure layer.
+
+            return services;
+        }
+    }
+}
