0c08438238725c300e8613b9e3302230070aadf7
Lars <lars.berger@yahoo.com>
2020-02-12 18:40:33 +0100

    feat(domain): added method for registering event/command handlers

--- LarsWM.Domain/DependencyInjection.cs
+++ LarsWM.Domain/DependencyInjection.cs
@@ -7,7 +7,9 @@ using LarsWM.Domain.UserConfigs.CommandHandlers;
 using LarsWM.Domain.Windows;
 using LarsWM.Domain.Workspaces;
 using LarsWM.Domain.Workspaces.CommandHandlers;
+using LarsWM.Infrastructure.Bussing;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace LarsWM.Domain
 {
@@ -26,6 +28,17 @@ namespace LarsWM.Domain
             services.AddSingleton<CreateWorkspaceHandler>();
 
             return services;
+        }
+
+        public static IServiceProvider RegisterDomainHandlers(this IServiceProvider serviceProvider)
+        {
+            var bus = serviceProvider.GetRequiredService<IBus>();
+            bus.RegisterCommandHandler<AddMonitorHandler>();
+            bus.RegisterCommandHandler<ReadUserConfigHandler>();
+            bus.RegisterCommandHandler<CreateWorkspaceHandler>();
+            bus.RegisterEventHandler<MonitorAddedHandler>();
+
+            return serviceProvider;
         }
     }
 }
