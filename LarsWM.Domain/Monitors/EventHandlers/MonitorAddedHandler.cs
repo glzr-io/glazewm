a26597bcac6c971874f27a91c1ac5798cc6b3ac1
Lars <lars.berger@yahoo.com>
2020-02-13 19:33:42 +0100

    feat(domain): invoke DisplayWorkspaceCommand from MonitorAddedHandler

--- LarsWM.Domain/Monitors/EventHandlers/MonitorAddedHandler.cs
+++ LarsWM.Domain/Monitors/EventHandlers/MonitorAddedHandler.cs
@@ -8,37 +8,20 @@ namespace LarsWM.Domain.Monitors.EventHandler
     class MonitorAddedHandler : IEventHandler<MonitorAddedEvent>
     {
         private IBus _bus;
-        private MonitorService _monitorService;
 
-        public MonitorAddedHandler(IBus bus, MonitorService monitorService)
+        public MonitorAddedHandler(IBus bus)
         {
             _bus = bus;
-            _monitorService = monitorService;
         }
 
         public void Handle(MonitorAddedEvent @event)
         {
-            foreach (var monitor in _monitorService.Monitors)
-            {
-                // Create an initial workspace for the monitor if one doesn't exist.
-                if (monitor.WorkspacesInMonitor.Count() == 0)
-                    _bus.Invoke(new CreateWorkspaceCommand(monitor.Id, 1));
-                    // TODO: invoke SetDisplayedWorkspaceCommand
-                    //_bus.Invoke(new DisplayWorkspaceCommand(workspaceId));
-            }
+            // Create an initial workspace for the monitor if one doesn't exist.
+            // TODO: Replace hardcoded Workspace.Index property.
+            var result = _bus.Invoke(new CreateWorkspaceCommand(@event.AddedMonitorId, 1));
 
-        // Old code:
-        // Create an initial Workspace for each Monitor
-        //    int index = 0;
-        //    foreach (var monitor in msg.monitors)
-        //    {
-        //        // TODO: add IsFocused property to focused window, workspace & monitor
-        //        var newWorkspace = new Workspace(index, new List<Window>());
-        //        monitor.WorkspacesInMonitor.Add(newWorkspace);
-        //        monitor.DisplayedWorkspace = newWorkspace;
-
-        //        index++;
-        //    }
+            // Set the displayed workspace to the newly created one.
+            _bus.Invoke(new DisplayWorkspaceCommand(result.AggregateId));
         }
     }
 }
