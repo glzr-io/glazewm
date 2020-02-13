a49ca0f747303d1bec18b264bb0d783e30649ce8
Lars <lars.berger@yahoo.com>
2020-02-13 09:08:06 +0100

    feat(infra): created CommandResponse class

---
+++ LarsWM.Infrastructure/Bussing/CommandResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LarsWM.Infrastructure.Bussing
+{
+    public class CommandResponse
+    {
+        public Boolean Success { get; private set; }
+        public Guid AggregateId { get; private set; }
+        public string Description { get; private set; }
+
+        public static CommandResponse Ok = new CommandResponse {Success = true};
+
+        public CommandResponse(Boolean success = false, Guid aggregateId = default)
+        {
+            Success = success;
+            AggregateId = aggregateId;
+            Description = String.Empty;
+        }
+    }
+}
