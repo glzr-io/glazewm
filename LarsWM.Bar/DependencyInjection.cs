aaf410647de5c725ed2301018ff4a92063804712
Lars <lars.berger@yahoo.com>
2020-02-13 20:13:37 +0100

    feat(bar): created static class for configuring services for Bar project

---
+++ LarsWM.Bar/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using LarsWM.Bar.CommandHandlers;
+using LarsWM.Bar.EventHandlers;
+using LarsWM.Infrastructure.Bussing;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace LarsWM.Bar
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddBarServices(this IServiceCollection services)
+        {
+            services.AddSingleton<LaunchBarOnMonitorHandler>();
+            services.AddSingleton<MonitorAddedHandler>();
+
+            return services;
+        }
+
+        public static IServiceProvider RegisterBarHandlers(this IServiceProvider serviceProvider)
+        {
+            var bus = serviceProvider.GetRequiredService<IBus>();
+            bus.RegisterCommandHandler<LaunchBarOnMonitorHandler>();
+            bus.RegisterEventHandler<MonitorAddedHandler>();
+
+            return serviceProvider;
+        }
+    }
+}
