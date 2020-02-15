29d7f64f5799c37cc1c3a0aaf324cb538bf660b2
Lars <lars.berger@yahoo.com>
2020-02-15 21:55:04 +0100

    feat(domain): successfully deserialize sample user config in ReadUserConfigHandler

--- LarsWM.Domain/UserConfigs/CommandHandlers/ReadUserConfigHandler.cs
+++ LarsWM.Domain/UserConfigs/CommandHandlers/ReadUserConfigHandler.cs
@@ -1,5 +1,9 @@
-﻿using LarsWM.Infrastructure.Bussing;
-using LarsWM.Domain.UserConfigs.Commands;
+﻿using LarsWM.Domain.UserConfigs.Commands;
+using LarsWM.Infrastructure.Bussing;
+using System;
+using System.IO;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
 
 namespace LarsWM.Domain.UserConfigs.CommandHandlers
 {
@@ -14,11 +18,22 @@ namespace LarsWM.Domain.UserConfigs.CommandHandlers
 
         public CommandResponse Handle(ReadUserConfigCommand command)
         {
+            var userConfigPath = Path.Combine(Directory.GetCurrentDirectory(), "UserConfigs/SampleUserConfig.yaml");
+
+            var userConfigLines = File.ReadAllLines(userConfigPath);
+            var input = new StringReader(string.Join(Environment.NewLine, userConfigLines));
+
+            var deserializer = new DeserializerBuilder()
+                .WithNamingConvention(PascalCaseNamingConvention.Instance)
+                .Build();
+
+            var deserializedConfig = deserializer.Deserialize<UserConfigFileDto>(input);
+
             // TODO: Read user config from file / constructed through shell script.
             var userConfig = new UserConfig();
             _userConfigService.UserConfig = userConfig;
 
-            return new CommandResponse(true, userConfig.Id);
+            return CommandResponse.Ok;
         }
     }
 }
