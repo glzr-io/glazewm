3e744a85ef0c28d42185bcf872daf4ecdfe67511
Lars <lars.berger@yahoo.com>
2020-02-16 23:29:00 +0100

    feat(domain): CreateWorkspaceCommand no longer assigns workspace to a monitor

--- LarsWM.Domain/Workspaces/CommandHandlers/CreateWorkspaceHandler.cs
+++ LarsWM.Domain/Workspaces/CommandHandlers/CreateWorkspaceHandler.cs
@@ -7,21 +7,16 @@ namespace LarsWM.Domain.Workspaces.CommandHandlers
     class CreateWorkspaceHandler : ICommandHandler<CreateWorkspaceCommand>
     {
         private WorkspaceService _workspaceService;
-        private MonitorService _monitorService;
 
-        public CreateWorkspaceHandler(WorkspaceService workspaceService, MonitorService monitorService)
+        public CreateWorkspaceHandler(WorkspaceService workspaceService)
         {
             _workspaceService = workspaceService;
-            _monitorService = monitorService;
         }
 
         public CommandResponse Handle(CreateWorkspaceCommand command)
         {
             var newWorkspace = new Workspace(command.WorkspaceName);
             _workspaceService.Workspaces.Add(newWorkspace);
-
-            var parentMonitor = _monitorService.GetMonitorById(command.ParentMonitorId);
-            parentMonitor.WorkspacesInMonitor.Add(newWorkspace);
 
             return new CommandResponse(true, newWorkspace.Id);
         }
