c1449e62d8003e07db4c6b72a6c931a44bd3bed5
Lars <lars.berger@yahoo.com>
2020-02-16 23:30:13 +0100

    feat(domain): created command and handler for assigning a workspace to a monitor

---
+++ LarsWM.Domain/Monitors/Commands/AssignWorkspaceToMonitorCommand.cs
@@ -0,0 +1,17 @@
+﻿using LarsWM.Infrastructure.Bussing;
+using System;
+
+namespace LarsWM.Domain.Monitors.Commands
+{
+    class AssignWorkspaceToMonitorCommand : Command
+    {
+        public Guid WorkspaceId { get; }
+        public Guid MonitorId { get; }
+
+        public AssignWorkspaceToMonitorCommand(Guid workspaceId, Guid monitorId)
+        {
+            WorkspaceId = workspaceId;
+            MonitorId = monitorId;
+        }
+    }
+}
