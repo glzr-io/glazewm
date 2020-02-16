aeee809d52766bb4b3b76dfc99db5850a11bb7d1
Lars <lars.berger@yahoo.com>
2020-02-16 19:11:49 +0100

    feat(boot): listen for window event hooks on app startup

--- LarsWM.Bootstrapper/Startup.cs
+++ LarsWM.Bootstrapper/Startup.cs
@@ -2,7 +2,9 @@ using LarsWM.Domain.Common.Services;
 using LarsWM.Domain.Monitors;
 using LarsWM.Domain.Monitors.Commands;
 using LarsWM.Domain.UserConfigs.Commands;
+using LarsWM.Domain.Windows;
 using LarsWM.Infrastructure.Bussing;
+using LarsWM.Infrastructure.WindowsApi;
 using System.Windows.Forms;
 
 namespace LarsWM.Bootstrapper
@@ -11,19 +13,25 @@ namespace LarsWM.Bootstrapper
     {
         private IBus _bus;
         private MonitorService _monitorService;
-
         private KeybindingService _keybindingService;
+        private WindowEventService _windowEventService;
+        private WindowHooksHandler _windowHooksHandler;
 
-        public Startup(IBus bus, MonitorService monitorService, KeybindingService keybindingService)
+        public Startup(IBus bus, MonitorService monitorService, KeybindingService keybindingService, WindowEventService windowEventService, WindowHooksHandler windowHooksHandler)
         {
             _bus = bus;
             _monitorService = monitorService;
             _keybindingService = keybindingService;
+            _windowEventService = windowEventService;
+            _windowHooksHandler = windowHooksHandler;
         }
 
         public void Init()
         {
             _keybindingService.Init();
+
+            _windowEventService.Init();
+            _windowHooksHandler.Configure();
 
             // Populate initial monitors, windows, workspaces and user config.
             PopulateInitialState();
