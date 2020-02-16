6d558d520c0b0beb4c17206cbc0bdd338d9cb4f1
Lars <lars.berger@yahoo.com>
2020-02-16 17:56:36 +0100

    feat(domain): created WindowsHookHandler for subscribing to emitted window hooks

---
+++ LarsWM.Domain/Windows/WindowHooksHandler.cs
@@ -0,0 +1,51 @@
+﻿using LarsWM.Infrastructure.WindowsApi;
+using LarsWM.Infrastructure.WindowsApi.Enums;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace LarsWM.Domain.Windows
+{
+    // TODO: Rename to WindowService once respository classes are created.
+    public class WindowHooksHandler
+    {
+        private WindowEventService _windowEventService { get; }
+
+        public WindowHooksHandler(WindowEventService windowEventService)
+        {
+            _windowEventService = windowEventService;
+        }
+
+        public void Configure()
+        {
+            _windowEventService.WindowHookSubject.Subscribe(observer =>
+            {
+                switch (observer.EventType)
+                {
+                    case EventConstant.EVENT_OBJECT_SHOW:
+                        Debug.WriteLine("show");
+                        break;
+                    case EventConstant.EVENT_OBJECT_DESTROY:
+                        Debug.WriteLine("destroy");
+                        break;
+                    case EventConstant.EVENT_OBJECT_CLOAKED:
+                        Debug.WriteLine("cloaked");
+                        break;
+                    case EventConstant.EVENT_OBJECT_UNCLOAKED:
+                        Debug.WriteLine("decloaked");
+                        break;
+                    case EventConstant.EVENT_SYSTEM_MINIMIZESTART:
+                        Debug.WriteLine("minimize start");
+                        break;
+                    case EventConstant.EVENT_SYSTEM_MINIMIZEEND:
+                        Debug.WriteLine("minimize end");
+                        break;
+                    case EventConstant.EVENT_SYSTEM_FOREGROUND:
+                        Debug.WriteLine("foreground");
+                        break;
+                }
+            });
+        }
+    }
+}
