b2dade18a22575cdd3e9750333327eb715df978b
Lars <lars.berger@yahoo.com>
2020-02-17 18:32:50 +0100

    feat(domain): created focus window command & handler (unimplemented)

---
+++ LarsWM.Domain/Windows/CommandHandlers/FocusWindowHandler.cs
@@ -0,0 +1,24 @@
+﻿using LarsWM.Domain.Windows.Commands;
+using LarsWM.Infrastructure.Bussing;
+
+namespace LarsWM.Domain.Windows.CommandHandlers
+{
+    class FocusWindowHandler : ICommandHandler<FocusWindowCommand>
+    {
+        private IBus _bus;
+
+        public FocusWindowHandler(IBus bus)
+        {
+            _bus = bus;
+        }
+
+        public CommandResponse Handle(FocusWindowCommand command)
+        {
+            var window = command.Window;
+
+            // TODO: If already focused, do nothing. If not focused, focus window.
+
+            return new CommandResponse(true, window.Id);
+        }
+    }
+}
---
+++ LarsWM.Domain/Windows/Commands/FocusWindowCommand.cs
@@ -0,0 +1,14 @@
+﻿using LarsWM.Infrastructure.Bussing;
+
+namespace LarsWM.Domain.Windows.Commands
+{
+    class FocusWindowCommand : Command
+    {
+        public Window Window { get; set; }
+
+        public FocusWindowCommand(Window window)
+        {
+            Window = window;
+        }
+    }
+}
