2f9597fdaa99ce4dbf8a10c317c1a05c7c37eab9
Lars <lars.berger@yahoo.com>
2020-02-20 21:49:45 +0100

    feat(domain): added method to ContainerService for getting Workspace/Monitor from child container

--- LarsWM.Domain/Containers/ContainerService.cs
+++ LarsWM.Domain/Containers/ContainerService.cs
@@ -1,4 +1,6 @@
 ﻿using LarsWM.Domain.Common.Models;
+using LarsWM.Domain.Monitors;
+using LarsWM.Domain.Workspaces;
 using System.Collections.Generic;
 
 namespace LarsWM.Domain.Containers
@@ -7,6 +9,26 @@ namespace LarsWM.Domain.Containers
     {
         //Tree<Container> Tree = new Tree<Container>();
         public List<Container> ContainerTree = new List<Container>();
+
+        public Monitor GetMonitorForContainer(Container container)
+        {
+            var parent = container.Parent;
+
+            while (parent != null && parent is Monitor == false)
+                parent = container.Parent;
+
+            return parent as Monitor;
+        }
+
+        public Workspace GetWorkspaceForContainer(Container container)
+        {
+            var parent = container.Parent;
+
+            while (parent != null && parent is Workspace == false)
+                parent = container.Parent;
+
+            return parent as Workspace;
+        }
     }
     // Workspace should have an orientation (horizontal, vertical), but shouldn't extend SplitContainer
     // Doesn't really matter what monitor is to the left or right, it matters what workspace is
