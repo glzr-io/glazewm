0d3d9b5adfa40382944c5071bb0c97c73cf39f03
Lars <lars.berger@yahoo.com>
2020-02-20 21:10:06 +0100

    feat(domain): added Container & SplitContainer models

---
+++ LarsWM.Domain/Common/Models/Container.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LarsWM.Domain.Common.Models
+{
+    public class Container
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Container Parent { get; set; } = null;
+        public List<Container> Children { get; set; } = new List<Container>();
+
+        public Container AddChild(Container container)
+        {
+            Children.Add(container);
+            return container;
+        }
+
+        public Container[] AddChildren(params Container[] containers)
+        {
+            return containers.Select(AddChild).ToArray();
+        }
+
+        public bool RemoveChild(Container node)
+        {
+            return Children.Remove(node);
+        }
+
+        // Not sure if needed.
+        public void Traverse(Action<Container> action)
+        {
+            action(this);
+            foreach (var child in Children)
+                child.Traverse(action);
+        }
+    }
+}
---
+++ LarsWM.Domain/Common/Models/SplitContainer.cs
@@ -0,0 +1,13 @@
+﻿namespace LarsWM.Domain.Common.Models
+{
+    public enum Orientation
+    {
+        Vertical,
+        Horizontal,
+    }
+
+    public class SplitContainer : Container
+    {
+        public Orientation Orientation { get; set; }
+    }
+}
