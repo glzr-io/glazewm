cb1608795a42877f48ba8a272a1f3578fd47ed30
Lars <lars.berger@yahoo.com>
2020-02-20 18:49:05 +0100

    feat(domain): call AddChild in AssignWorkspaceToMonitorHandler

--- LarsWM.Domain/Monitors/CommandHandlers/AssignWorkspaceToMonitorHandler.cs
+++ LarsWM.Domain/Monitors/CommandHandlers/AssignWorkspaceToMonitorHandler.cs
@@ -1,5 +1,4 @@
 ﻿using LarsWM.Domain.Monitors.Commands;
-using LarsWM.Domain.Workspaces;
 using LarsWM.Infrastructure.Bussing;
 
 namespace LarsWM.Domain.Monitors.CommandHandlers
@@ -7,23 +6,11 @@ namespace LarsWM.Domain.Monitors.CommandHandlers
     // TODO: Consider moving this to Workspaces domain.
     class AssignWorkspaceToMonitorHandler : ICommandHandler<AssignWorkspaceToMonitorCommand>
     {
-        private MonitorService _monitorService { get; }
-        private WorkspaceService _workspaceService { get; }
-
-        public AssignWorkspaceToMonitorHandler(MonitorService monitorService, WorkspaceService workspaceService)
-        {
-            _monitorService = monitorService;
-            _workspaceService = workspaceService;
-        }
-
         public dynamic Handle(AssignWorkspaceToMonitorCommand command)
         {
-            var monitor = _monitorService.GetMonitorById(command.MonitorId);
-            var workspace = _workspaceService.GetWorkspaceById(command.WorkspaceId);
+            command.Monitor.AddChild(command.Workspace);
 
-            monitor.WorkspacesInMonitor.Add(workspace);
-
-            return new CommandResponse(true, workspace.Id);
+            return new CommandResponse(true, command.Monitor.Id);
         }
     }
 }
