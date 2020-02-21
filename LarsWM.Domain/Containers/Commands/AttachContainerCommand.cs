c4af4beb56f263d53f0f3b601cdb493afd77df42
Lars <lars.berger@yahoo.com>
2020-02-21 17:59:12 +0100

    feat(domain): created AttachContainerCommand

---
+++ LarsWM.Domain/Containers/Commands/AttachContainerCommand.cs
@@ -0,0 +1,17 @@
+﻿using LarsWM.Domain.Common.Models;
+using LarsWM.Infrastructure.Bussing;
+
+namespace LarsWM.Domain.Monitors.Commands
+{
+    public class AttachContainerCommand : Command
+    {
+        public Container Parent { get; }
+        public Container NewChild { get; }
+
+        public AttachContainerCommand(Container parent, Container newChild)
+        {
+            Parent = parent;
+            NewChild = newChild;
+        }
+    }
+}
