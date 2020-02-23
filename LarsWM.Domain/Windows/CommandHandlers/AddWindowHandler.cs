d1e99e544ba37a7a5e97f0c9d97bf91d4078fb5f
Lars <lars.berger@yahoo.com>
2020-02-23 20:17:20 +0100

    feat(domain): reverted AddWindowHandler to be temporarily unimplemented

--- LarsWM.Domain/Windows/CommandHandlers/AddWindowHandler.cs
+++ LarsWM.Domain/Windows/CommandHandlers/AddWindowHandler.cs
@@ -1,10 +1,8 @@
-﻿using LarsWM.Domain.Monitors;
+﻿using System;
+using LarsWM.Domain.Monitors;
 using LarsWM.Domain.UserConfigs;
 using LarsWM.Domain.Windows.Commands;
 using LarsWM.Infrastructure.Bussing;
-using System.Diagnostics;
-using static LarsWM.Domain.Common.Services.WindowsApiFacade;
-using static LarsWM.Infrastructure.WindowsApi.WindowsApiService;
 
 namespace LarsWM.Domain.Windows.CommandHandlers
 {
@@ -21,35 +19,7 @@ namespace LarsWM.Domain.Windows.CommandHandlers
 
         public dynamic Handle(AddWindowCommand command)
         {
-            Window window = new Window(command.WindowHandle);
-
-            if (!IsWindowManageable(window))
-                return new CommandResponse(false, window.Id);
-
-            // Add window to its nearest workspace
-            var targetMonitor = _monitorService.GetMonitorFromUnaddedWindow(window);
-            //targetMonitor.DisplayedWorkspace.WindowsInWorkspace.Add(window);
-
-            return new CommandResponse(true, window.Id);
-        }
-
-        private bool IsWindowManageable(Window window)
-        {
-            var isApplicationWindow = IsWindowVisible(window.Hwnd) && !HasWindowStyle(window.Hwnd, WS.WS_CHILD) && !HasWindowExStyle(window.Hwnd, WS_EX.WS_EX_NOACTIVATE);
-
-            var isCurrentProcess = window.Process.Id == Process.GetCurrentProcess().Id;
-
-            var isExcludedClassName = _userConfigService.UserConfig.WindowClassesToIgnore.Contains(window.ClassName);
-            var isExcludedProcessName = _userConfigService.UserConfig.ProcessNamesToIgnore.Contains(window.Process.ProcessName);
-
-            var isShellWindow = window.Hwnd == GetShellWindow();
-
-            if (isApplicationWindow && !isCurrentProcess && !isExcludedClassName && !isExcludedProcessName && !isShellWindow)
-            {
-                return true;
-            }
-
-            return false;
+            throw new NotImplementedException();
         }
     }
 }
