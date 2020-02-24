d440e7051c26758d431671b8b0da2f901b2ba53b
Lars <lars.berger@yahoo.com>
2020-02-24 19:53:54 +0100

    feat(domain): partially implemented FocusWorkspaceHandler

---
+++ LarsWM.Domain/Workspaces/CommandHandlers/FocusWorkspaceHandler.cs
@@ -0,0 +1,33 @@
+﻿using LarsWM.Infrastructure.Bussing;
+using LarsWM.Domain.Monitors;
+using LarsWM.Domain.Workspaces.Commands;
+
+namespace LarsWM.Domain.Workspaces.CommandHandlers
+{
+    class FocusWorkspaceHandler : ICommandHandler<FocusWorkspaceCommand>
+    {
+        private IBus _bus;
+        private WorkspaceService _workspaceService;
+        private MonitorService _monitorService;
+
+        public FocusWorkspaceHandler(IBus bus, WorkspaceService workspaceService, MonitorService monitorService)
+        {
+            _bus = bus;
+            _workspaceService = workspaceService;
+            _monitorService = monitorService;
+        }
+
+        public dynamic Handle(FocusWorkspaceCommand command)
+        {
+            var workspace = command.Workspace;
+            var monitor = _monitorService.GetMonitorFromChildContainer(workspace);
+
+            if (monitor.DisplayedWorkspace != workspace)
+                _bus.Invoke(new DisplayWorkspaceCommand(workspace));
+
+            // TODO: Set focus to the last focused window on workspace.
+
+            return CommandResponse.Ok;
+        }
+    }
+}
---
+++ LarsWM.Domain/Workspaces/Commands/FocusWorkspaceCommand.cs
@@ -0,0 +1,14 @@
+﻿using LarsWM.Infrastructure.Bussing;
+
+namespace LarsWM.Domain.Workspaces.Commands
+{
+    public class FocusWorkspaceCommand : Command
+    {
+        public Workspace Workspace { get; }
+
+        public FocusWorkspaceCommand(Workspace workspace)
+        {
+            Workspace = workspace;
+        }
+    }
+}
