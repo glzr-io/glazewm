8e03c4469b2b44afdbfda62299c78e2fa95f4614
Lars <lars.berger@yahoo.com>
2020-02-29 17:13:42 +0100

    feat(domain): remove assigned workspace from inactive workspaces in  AttachWorkspaceToMonitorHandler

--- LarsWM.Domain/Workspaces/CommandHandlers/AttachWorkspaceToMonitorHandler.cs
+++ LarsWM.Domain/Workspaces/CommandHandlers/AttachWorkspaceToMonitorHandler.cs
@@ -5,11 +5,18 @@ namespace LarsWM.Domain.Workspaces.CommandHandlers
 {
     class AttachWorkspaceToMonitorHandler : ICommandHandler<AttachWorkspaceToMonitorCommand>
     {
+        private WorkspaceService _workspaceService;
+
+        public AttachWorkspaceToMonitorHandler(WorkspaceService workspaceService)
+        {
+            _workspaceService = workspaceService;
+        }
         public dynamic Handle(AttachWorkspaceToMonitorCommand command)
         {
             command.Monitor.AddChild(command.Workspace);
+            _workspaceService.InactiveWorkspaces.Remove(command.Workspace);
 
-            return new CommandResponse(true, command.Monitor.Id);
+            return new CommandResponse(true, command.Workspace.Id);
         }
     }
 }
