eccb63d6c5ec9dda6881da9af4f77ad358234352
Lars <lars.berger@yahoo.com>
2020-03-03 23:26:28 +0100

    feat(domain): created ResizeFocusedWindowCommand and its handler (unimplemented)

---
+++ LarsWM.Domain/Windows/CommandHandlers/ResizeFocusedWindowHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using LarsWM.Domain.Windows.Commands;
+using LarsWM.Infrastructure.Bussing;
+
+namespace LarsWM.Domain.Windows.CommandHandlers
+{
+    class ResizeFocusedWindowHandler : ICommandHandler<ResizeFocusedWindowCommand>
+    {
+        private WindowService _windowService;
+
+        public ResizeFocusedWindowHandler(WindowService windowService)
+        {
+            _windowService = windowService;
+        }
+
+        public dynamic Handle(ResizeFocusedWindowCommand command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
