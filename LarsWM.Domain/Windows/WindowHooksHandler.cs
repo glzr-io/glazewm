2f32050722ff60c8bf3fa7d234a0556052291dd4
Lars <lars.berger@yahoo.com>
2020-03-03 19:11:58 +0100

    feat(domain): call DetachContainerCommand on EVENT_OBJECT_DESTROY window event hook

--- LarsWM.Domain/Windows/WindowHooksHandler.cs
+++ LarsWM.Domain/Windows/WindowHooksHandler.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Linq;
+using LarsWM.Domain.Common.Models;
 using LarsWM.Domain.Containers;
+using LarsWM.Domain.Containers.Commands;
 using LarsWM.Domain.Windows.Commands;
 using LarsWM.Infrastructure.Bussing;
 using LarsWM.Infrastructure.WindowsApi;
@@ -51,9 +53,14 @@ namespace LarsWM.Domain.Windows
                         _bus.Invoke(new AddWindowCommand(observer.AffectedWindowHandle));
                         break;
                     case EventConstant.EVENT_OBJECT_DESTROY:
-                        // TODO: If window is in tree, dettach from currently displayed
-                        // workspace.
                         Debug.WriteLine("destroy");
+                        // If window is in tree, detach removed window from its parent.
+                        if (window != null)
+                        {
+                            _bus.Invoke(new DetachContainerCommand(window.Parent as SplitContainer, window));
+                            _bus.Invoke(new RedrawContainersCommand());
+                        }
+
                         break;
                     case EventConstant.EVENT_OBJECT_CLOAKED:
                         Debug.WriteLine("cloaked");
