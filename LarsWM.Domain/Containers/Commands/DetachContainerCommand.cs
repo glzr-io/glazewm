b9e069882113d94589ee27f378ca32810d4e1399
Lars <lars.berger@yahoo.com>
2020-03-03 19:10:33 +0100

    feat(domain): created DetachContainerCommand

---
+++ LarsWM.Domain/Containers/Commands/DetachContainerCommand.cs
@@ -0,0 +1,17 @@
+﻿using LarsWM.Domain.Common.Models;
+using LarsWM.Infrastructure.Bussing;
+
+namespace LarsWM.Domain.Containers.Commands
+{
+    public class DetachContainerCommand : Command
+    {
+        public SplitContainer Parent { get; }
+        public Container ChildToRemove { get; }
+
+        public DetachContainerCommand(SplitContainer parent, Container childToRemove)
+        {
+            Parent = parent;
+            ChildToRemove = childToRemove;
+        }
+    }
+}
