f301b359b72d9ed111c290cab456176709b3d053
Lars <lars.berger@yahoo.com>
2020-03-04 20:19:00 +0100

    feat(domain): partial implementation of ResizeFocusedWindowHandler

--- LarsWM.Domain/Windows/CommandHandlers/ResizeFocusedWindowHandler.cs
+++ LarsWM.Domain/Windows/CommandHandlers/ResizeFocusedWindowHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using LarsWM.Domain.UserConfigs;
 using LarsWM.Domain.Windows.Commands;
 using LarsWM.Infrastructure.Bussing;
 
@@ -7,14 +8,20 @@ namespace LarsWM.Domain.Windows.CommandHandlers
     class ResizeFocusedWindowHandler : ICommandHandler<ResizeFocusedWindowCommand>
     {
         private WindowService _windowService;
+        private UserConfigService _userConfigService;
 
-        public ResizeFocusedWindowHandler(WindowService windowService)
+        public ResizeFocusedWindowHandler(WindowService windowService, UserConfigService userConfigService)
         {
             _windowService = windowService;
+            _userConfigService = userConfigService;
         }
 
         public dynamic Handle(ResizeFocusedWindowCommand command)
         {
+            var resizePercentage = _userConfigService.UserConfig.ResizePercentage;
+            var focusedWindow = _windowService.FocusedWindow;
+            var parent = focusedWindow.Parent;
+
             throw new NotImplementedException();
         }
     }
--- LarsWM.Domain/Windows/Commands/ResizeFocusedWindowCommand.cs
+++ LarsWM.Domain/Windows/Commands/ResizeFocusedWindowCommand.cs
@@ -1,8 +1,15 @@
-﻿using LarsWM.Infrastructure.Bussing;
+﻿using LarsWM.Domain.Common.Enums;
+using LarsWM.Infrastructure.Bussing;
 
 namespace LarsWM.Domain.Windows.Commands
 {
     public class ResizeFocusedWindowCommand : Command
     {
+        public Direction Direction { get; }
+
+        public ResizeFocusedWindowCommand(Direction direction)
+        {
+            Direction = direction;
+        }
     }
 }
