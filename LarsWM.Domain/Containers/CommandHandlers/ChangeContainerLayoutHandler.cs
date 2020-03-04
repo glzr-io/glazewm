5e5d4e61d3321428f2d88d994fed651c4f9f88b1
Lars <lars.berger@yahoo.com>
2020-03-04 07:09:22 +0100

    feat(domain): created ChangeContainerLayoutCommand and its handler (unimplemented)

---
+++ LarsWM.Domain/Containers/CommandHandlers/ChangeContainerLayoutHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using LarsWM.Domain.Common.Enums;
+using LarsWM.Domain.Common.Models;
+using LarsWM.Domain.Containers.Commands;
+using LarsWM.Domain.UserConfigs;
+using LarsWM.Infrastructure.Bussing;
+
+namespace LarsWM.Domain.Containers.CommandHandlers
+{
+    class ChangeContainerLayoutHandler : ICommandHandler<ChangeContainerLayout>
+    {
+        private IBus _bus;
+        private ContainerService _containerService;
+        private readonly UserConfigService _userConfigService;
+
+        public ChangeContainerLayoutHandler(IBus bus, ContainerService containerService, UserConfigService userConfigService)
+        {
+            _bus = bus;
+            _containerService = containerService;
+            _userConfigService = userConfigService;
+        }
+
+        public dynamic Handle(ChangeContainerLayout command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
