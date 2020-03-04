9c83cb714fa9daf4a0321273f17af15998211750
Lars <lars.berger@yahoo.com>
2020-03-04 08:09:46 +0100

    feat(domain): recursively adjust X, Y, Height & Width of containers in RedrawContainersHandler

--- LarsWM.Domain/Containers/CommandHandlers/RedrawContainersHandler.cs
+++ LarsWM.Domain/Containers/CommandHandlers/RedrawContainersHandler.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using LarsWM.Domain.Common.Models;
 using LarsWM.Domain.Containers.Commands;
+using LarsWM.Domain.UserConfigs;
 using LarsWM.Domain.Windows;
 using LarsWM.Infrastructure.Bussing;
 using static LarsWM.Infrastructure.WindowsApi.WindowsApiService;
@@ -11,16 +13,61 @@ namespace LarsWM.Domain.Containers.CommandHandlers
     class RedrawContainersHandler : ICommandHandler<RedrawContainersCommand>
     {
         private ContainerService _containerService;
+        private UserConfigService _userConfigService;
 
-        public RedrawContainersHandler(ContainerService containerService)
+        public RedrawContainersHandler(ContainerService containerService, UserConfigService userConfigService)
         {
             _containerService = containerService;
+            _userConfigService = userConfigService;
         }
 
         public dynamic Handle(RedrawContainersCommand command)
         {
-            var containersToRedraw = _containerService.PendingContainersToRedraw;
+            var containersToRedraw = _containerService.SplitContainersToRedraw;
 
+            // Recursively adjust children of SplitContainers. If another SplitContainer is
+            // encountered, perform same function on its children.
+
+            var innerGap = _userConfigService.UserConfig.InnerGap;
+
+            // TODO: Instead of looping over containersToRedraw, create an enumerable of all SplitContainers within containersToRedraw.
+            foreach (var parentContainer in containersToRedraw)
+            {
+                var children = parentContainer.Children;
+
+                if (parentContainer.Layout == Common.Enums.Layout.Horizontal)
+                {
+                    // Available parent width is the width of the parent minus all inner gaps.
+                    var availableParentWidth = parentContainer.Width - (innerGap * (children.Count - 1));
+
+                    // Adjust widths of current child containers.
+                    foreach (var child in children)
+                    {
+                        child.Width = (int)(child.SizePercentage * availableParentWidth);
+                    }
+
+                    // Adjust x-coordinate of child containers.
+                    Container previousChild = null;
+                    foreach (var child in parentContainer.Children)
+                    {
+                        if (previousChild == null)
+                            child.X = parentContainer.X;
+
+                        else
+                            child.X = previousChild.X + previousChild.Width + innerGap;
+
+                        previousChild = child;
+                    }
+                }
+            }
+
+            PushUpdates(containersToRedraw);
+
+            return CommandResponse.Ok;
+        }
+
+        private void PushUpdates(List<SplitContainer> containersToRedraw)
+        {
             var windowsToRedraw = containersToRedraw
                 .SelectMany(container => container.Flatten())
                 .OfType<Window>()
@@ -45,8 +92,6 @@ namespace LarsWM.Domain.Containers.CommandHandlers
             EndDeferWindowPos(handle);
 
             containersToRedraw.Clear();
-
-            return CommandResponse.Ok;
         }
     }
 }
