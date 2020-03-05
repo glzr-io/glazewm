25b3fdeaea0e2e66f48d30d10b37e385fc70a615
Lars <lars.berger@yahoo.com>
2020-03-05 21:47:54 +0100

    feat(domain): handle change to vertical layout in ChangeContainerLayoutHandler

--- LarsWM.Domain/Containers/CommandHandlers/ChangeContainerLayoutHandler.cs
+++ LarsWM.Domain/Containers/CommandHandlers/ChangeContainerLayoutHandler.cs
@@ -3,26 +3,50 @@ using LarsWM.Domain.Common.Enums;
 using LarsWM.Domain.Common.Models;
 using LarsWM.Domain.Containers.Commands;
 using LarsWM.Domain.UserConfigs;
+using LarsWM.Domain.Windows;
 using LarsWM.Infrastructure.Bussing;
 
 namespace LarsWM.Domain.Containers.CommandHandlers
 {
-    class ChangeContainerLayoutHandler : ICommandHandler<ChangeContainerLayout>
+    class ChangeContainerLayoutHandler : ICommandHandler<ChangeContainerLayoutCommand>
     {
         private IBus _bus;
         private ContainerService _containerService;
-        private readonly UserConfigService _userConfigService;
+        private WindowService _windowService;
 
-        public ChangeContainerLayoutHandler(IBus bus, ContainerService containerService, UserConfigService userConfigService)
+        public ChangeContainerLayoutHandler(IBus bus, ContainerService containerService, WindowService windowService)
         {
             _bus = bus;
             _containerService = containerService;
-            _userConfigService = userConfigService;
+            _windowService = windowService;
         }
 
-        public dynamic Handle(ChangeContainerLayout command)
+        public dynamic Handle(ChangeContainerLayoutCommand command)
         {
-            throw new NotImplementedException();
+            var focusedWindow = _windowService.FocusedWindow;
+            var parent = focusedWindow.Parent as SplitContainer;
+
+            var newLayout = command.NewLayout;
+            var currentLayout = parent.Layout;
+
+            if (currentLayout == newLayout)
+                return CommandResponse.Ok;
+
+            if (newLayout == Layout.Vertical)
+            {
+                _bus.Invoke(new DetachContainerCommand(parent, focusedWindow));
+
+                var newParent = new SplitContainer
+                {
+                    Layout = Layout.Vertical,
+                    SizePercentage = 1
+                };
+
+                _bus.Invoke(new AttachContainerCommand(parent, newParent));
+                _bus.Invoke(new AttachContainerCommand(newParent, focusedWindow));
+            }
+
+            return CommandResponse.Ok;
         }
     }
 }
