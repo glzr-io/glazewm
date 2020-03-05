012faaa67582048ffcd763332b7d0e5c2dbf0793
Lars <lars.berger@yahoo.com>
2020-03-05 21:01:14 +0100

    feat(domain): remove Container property on ChangeContainerLayoutCommand
    * Instead change layout of focused container

---
+++ LarsWM.Domain/Containers/Commands/ChangeContainerLayoutCommand.cs
@@ -0,0 +1,16 @@
+﻿using LarsWM.Domain.Common.Enums;
+using LarsWM.Domain.Common.Models;
+using LarsWM.Infrastructure.Bussing;
+
+namespace LarsWM.Domain.Containers.Commands
+{
+    public class ChangeContainerLayoutCommand : Command
+    {
+        public Layout NewLayout { get; }
+
+        public ChangeContainerLayoutCommand(Layout newLayout)
+        {
+            NewLayout = newLayout;
+        }
+    }
+}
