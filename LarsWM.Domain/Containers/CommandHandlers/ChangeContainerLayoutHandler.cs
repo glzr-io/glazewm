392f4b791ccc244db4f01096ea55468f5df7689b
Lars <lars.berger@yahoo.com>
2020-03-06 21:49:29 +0100

    feat(domain): flatten parent split container if focused window is an only child in ChangeContainerLayoutHandler

--- LarsWM.Domain/Containers/CommandHandlers/ChangeContainerLayoutHandler.cs
+++ LarsWM.Domain/Containers/CommandHandlers/ChangeContainerLayoutHandler.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Linq;
 using LarsWM.Domain.Common.Enums;
 using LarsWM.Domain.Common.Models;
 using LarsWM.Domain.Containers.Commands;
 using LarsWM.Domain.UserConfigs;
 using LarsWM.Domain.Windows;
+using LarsWM.Domain.Workspaces;
 using LarsWM.Infrastructure.Bussing;
 
 namespace LarsWM.Domain.Containers.CommandHandlers
@@ -32,19 +34,44 @@ namespace LarsWM.Domain.Containers.CommandHandlers
             if (currentLayout == newLayout)
                 return CommandResponse.Ok;
 
-            if (newLayout == Layout.Vertical)
+            var isFocusedOnlyChild = focusedWindow.Siblings.Count() == 0;
+
+            // If the focused window is an only child of a workspace, change layout of workspace.
+            if (isFocusedOnlyChild && parent is Workspace)
             {
-                _bus.Invoke(new DetachContainerCommand(parent, focusedWindow));
-
-                var newParent = new SplitContainer
-                {
-                    Layout = Layout.Vertical,
-                    SizePercentage = 1
-                };
-
-                _bus.Invoke(new AttachContainerCommand(parent, newParent));
-                _bus.Invoke(new AttachContainerCommand(newParent, focusedWindow));
+                parent.Layout = newLayout;
+                return CommandResponse.Ok;
             }
+
+            // If the focused container is an only child and the parent is a normal split
+            // container, then flatten the split container.
+            if (isFocusedOnlyChild)
+            {
+                var grandparent = parent.Parent;
+                var splitContainerIndex = grandparent.Children.IndexOf(parent);
+
+                grandparent.RemoveChild(parent);
+                grandparent.Children[splitContainerIndex] = focusedWindow;
+
+                return CommandResponse.Ok;
+            }
+
+            var splitContainer = new SplitContainer
+            {
+                Layout = newLayout,
+                SizePercentage = focusedWindow.SizePercentage,
+                LastFocusedContainer = focusedWindow,
+                Parent = parent
+            };
+
+            var focusedIndex = parent.Children.IndexOf(focusedWindow);
+
+            _bus.Invoke(new AttachContainerCommand(splitContainer, focusedWindow));
+
+            parent.Children[focusedIndex] = splitContainer;
+
+            _containerService.SplitContainersToRedraw.Add(parent);
+            _bus.Invoke(new RedrawContainersCommand());
 
             return CommandResponse.Ok;
         }
