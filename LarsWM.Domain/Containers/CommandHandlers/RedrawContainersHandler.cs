3a2ba2c30ece6954d5dffb212387f2c59f7910b7
Lars <lars.berger@yahoo.com>
2020-03-09 19:22:35 +0100

    feat(domain): removed logic for calculating size/location of containers in RedrawContainersHandler

--- LarsWM.Domain/Containers/CommandHandlers/RedrawContainersHandler.cs
+++ LarsWM.Domain/Containers/CommandHandlers/RedrawContainersHandler.cs
@@ -24,78 +24,8 @@ namespace LarsWM.Domain.Containers.CommandHandlers
 
         public dynamic Handle(RedrawContainersCommand command)
         {
-            var splitContainersToRedraw = _containerService.SplitContainersToRedraw;
+            var containersToRedraw = _containerService.SplitContainersToRedraw;
 
-            // Enumerable of all split containers to redraw (including nested split containers).
-            var allSplitContainersToRedraw = splitContainersToRedraw
-                .SelectMany(container => container.Flatten())
-                .OfType<SplitContainer>()
-                .Distinct();
-
-            var innerGap = _userConfigService.UserConfig.InnerGap;
-
-            foreach (var parentContainer in allSplitContainersToRedraw)
-            {
-                var children = parentContainer.Children;
-
-                if (parentContainer.Layout == Layout.Horizontal)
-                {
-                    // Available parent width is the width of the parent minus all inner gaps.
-                    var availableParentWidth = parentContainer.Width - (innerGap * (children.Count - 1));
-
-                    // Adjust size and location of child containers.
-                    Container previousChild = null;
-                    foreach (var child in children)
-                    {
-                        // Direct children of parent have the same height and Y coord as parent in horizontal layouts.
-                        child.Height = parentContainer.Height;
-                        child.Y = parentContainer.Y;
-
-                        child.Width = (int)(child.SizePercentage * availableParentWidth);
-
-                        if (previousChild == null)
-                            child.X = parentContainer.X;
-
-                        else
-                            child.X = previousChild.X + previousChild.Width + innerGap;
-
-                        previousChild = child;
-                    }
-                }
-
-                if (parentContainer.Layout == Layout.Vertical)
-                {
-                    // Available parent height is the height of the parent minus all inner gaps.
-                    var availableParentHeight = parentContainer.Height - (innerGap * (children.Count - 1));
-
-                    // Adjust size and location of child containers.
-                    Container previousChild = null;
-                    foreach (var child in children)
-                    {
-                        // Direct children of parent have the same width and X coord as parent in vertical layouts.
-                        child.Width = parentContainer.Width;
-                        child.X = parentContainer.X;
-
-                        child.Height = (int)(child.SizePercentage * availableParentHeight);
-
-                        if (previousChild == null)
-                            child.Y = parentContainer.Y;
-
-                        else
-                            child.Y = previousChild.Y + previousChild.Height + innerGap;
-
-                        previousChild = child;
-                    }
-                }
-            }
-
-            PushUpdates(splitContainersToRedraw);
-
-            return CommandResponse.Ok;
-        }
-
-        private void PushUpdates(List<SplitContainer> containersToRedraw)
-        {
             var windowsToRedraw = containersToRedraw
                 .SelectMany(container => container.Flatten())
                 .OfType<Window>()
@@ -120,6 +50,8 @@ namespace LarsWM.Domain.Containers.CommandHandlers
             EndDeferWindowPos(handle);
 
             containersToRedraw.Clear();
+
+            return CommandResponse.Ok;
         }
     }
 }
