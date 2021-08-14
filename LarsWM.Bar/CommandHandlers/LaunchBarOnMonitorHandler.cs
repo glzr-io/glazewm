d5bc0cd3985aa51a02d9be61c3a10e26563076f7
Lars <lars.berger@yahoo.com>
2021-08-14 23:15:51 +0200

    feat(bar): partially implement responding to workspace attached event

--- LarsWM.Bar/CommandHandlers/LaunchBarOnMonitorHandler.cs
+++ LarsWM.Bar/CommandHandlers/LaunchBarOnMonitorHandler.cs
@@ -1,34 +1,49 @@
 ﻿using LarsWM.Bar.Commands;
 using LarsWM.Domain.Monitors;
+using LarsWM.Domain.Workspaces;
 using LarsWM.Infrastructure.Bussing;
 using System.Threading;
+using System.Diagnostics;
 using System.Windows.Forms;
+using System;
 
 namespace LarsWM.Bar.CommandHandlers
 {
   class LaunchBarOnMonitorHandler : ICommandHandler<LaunchBarOnMonitorCommand>
   {
     private MonitorService _monitorService { get; }
+    private WorkspaceService _workspaceService { get; }
     public Bus _bus { get; }
 
-    public LaunchBarOnMonitorHandler(MonitorService monitorService, Bus bus)
+    public LaunchBarOnMonitorHandler(MonitorService monitorService, WorkspaceService workspaceService, Bus bus)
     {
       _monitorService = monitorService;
+      _workspaceService = workspaceService;
       _bus = bus;
     }
 
+    // TODO: Set bar width to width of monitor and launch bar on given monitor.
     public dynamic Handle(LaunchBarOnMonitorCommand command)
     {
       var thread = new Thread(() =>
       {
-        // TODO: Set bar width to width of monitor and launch bar on given monitor.
-        var bar = new MainWindow(command.Monitor, _bus);
+        var bar = new MainWindow(command.Monitor, _workspaceService, _bus);
         bar.Show();
         Application.Run();
       });
+
       thread.Name = "LarsWMBar";
       thread.SetApartmentState(ApartmentState.STA);
       thread.Start();
+
+      // Application app = new Application();
+
+      // app.SetUiContext(System.Threading.SynchronizationContext.Current);
+      // app.Show();
+      // Application.Run();
+
+      // Application app = new Application();
+      // app.Run(new MainWindow(command.Monitor, _workspaceService, _bus));
 
       return CommandResponse.Ok;
     }
--- LarsWM.Bar/MainWindow.xaml.cs
+++ LarsWM.Bar/MainWindow.xaml.cs
@@ -1,9 +1,14 @@
 ﻿using LarsWM.Domain.Monitors;
 using LarsWM.Domain.Workspaces;
 using LarsWM.Domain.Workspaces.Commands;
+using LarsWM.Domain.Workspaces.Events;
 using LarsWM.Infrastructure.Bussing;
+using System.Collections.ObjectModel;
+using System.Reactive.Linq;
+using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace LarsWM.Bar
 {
@@ -13,26 +18,54 @@ namespace LarsWM.Bar
   public partial class MainWindow : Window
   {
     private Bus _bus { get; }
+    private static object _lock = new object();
+    private WorkspaceService _workspaceService { get; }
 
-    public MainWindow(Monitor monitor, Bus bus)
+    public MainWindow(Monitor monitor, WorkspaceService workspaceService, Bus bus)
     {
       _bus = bus;
 
       InitializeComponent();
 
+      // TODO: Bind padding, bg color, button bg color and font from user config.
       this.Top = monitor.Y;
       this.Left = monitor.X;
       this.Width = monitor.Width;
-
       // TODO: Change height to be set in XAML.
       this.Height = 50;
 
-      // TODO: Bind padding, bg color, button bg color and font from user config.
+      var workspaces = new ObservableCollection<Workspace>();
+      BindingOperations.EnableCollectionSynchronization(workspaces, _lock);
 
-      var workspaces = monitor.Children;
+      foreach (var workspace in monitor.Children)
+        workspaces.Add(workspace as Workspace);
+
       workspaceItems.ItemsSource = workspaces;
-    }
 
+      _bus.Events.Where(@event => @event is WorkspaceAttachedEvent).Subscribe(observer =>
+          {
+            // App.Current.Dispatcher.Invoke((Action)delegate
+            // {
+            //   workspaces.Clear();
+            //   foreach (var workspace in monitor.Children)
+            //     workspaces.Add(workspace as Workspace);
+            // });
+
+            // UiContext.Send((x) =>
+            // {
+            //   workspaces.Clear();
+            //   foreach (var workspace in monitor.Children)
+            //     workspaces.Add(workspace as Workspace);
+            // }, null);
+
+            workspaces.Clear();
+            foreach (var workspace in monitor.Children)
+              workspaces.Add(workspace as Workspace);
+
+            CollectionViewSource.GetDefaultView(workspaces).Refresh();
+          }
+      );
+    }
 
     private void OnWorkspaceButtonClick(object sender, RoutedEventArgs e)
     {
