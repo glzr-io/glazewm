ce7dd9d7d64e49abb0a87f3dc96aed6afbea4d04
Lars <lars.berger@yahoo.com>
2021-08-14 18:46:41 +0200

    feat(domain): raise event after workspace is attached

--- LarsWM.Domain/Workspaces/CommandHandlers/AttachWorkspaceToMonitorHandler.cs
+++ LarsWM.Domain/Workspaces/CommandHandlers/AttachWorkspaceToMonitorHandler.cs
@@ -1,4 +1,5 @@
 ﻿using LarsWM.Domain.Workspaces.Commands;
+using LarsWM.Domain.Workspaces.Events;
 using LarsWM.Infrastructure.Bussing;
 
 namespace LarsWM.Domain.Workspaces.CommandHandlers
@@ -6,15 +7,20 @@ namespace LarsWM.Domain.Workspaces.CommandHandlers
   class AttachWorkspaceToMonitorHandler : ICommandHandler<AttachWorkspaceToMonitorCommand>
   {
     private WorkspaceService _workspaceService;
+    public Bus _bus { get; }
 
-    public AttachWorkspaceToMonitorHandler(WorkspaceService workspaceService)
+    public AttachWorkspaceToMonitorHandler(WorkspaceService workspaceService, Bus bus)
     {
       _workspaceService = workspaceService;
+      _bus = bus;
     }
+
     public dynamic Handle(AttachWorkspaceToMonitorCommand command)
     {
       command.Monitor.AddChild(command.Workspace);
       _workspaceService.InactiveWorkspaces.Remove(command.Workspace);
+
+      _bus.RaiseEvent(new WorkspaceAttachedEvent(command.Workspace));
 
       return new CommandResponse(true, command.Workspace.Id);
     }
