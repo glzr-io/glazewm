06276bc21e9ca5db0f9b0532aaa624b37b79b51b
Lars <lars.berger@yahoo.com>
2021-08-14 18:45:48 +0200

    feat(domain): create new `Event` class for when workspaces are attached

---
+++ LarsWM.Domain/Workspaces/Events/WorkspaceAttachedEvent.cs
@@ -0,0 +1,14 @@
+﻿using LarsWM.Infrastructure.Bussing;
+
+namespace LarsWM.Domain.Workspaces.Events
+{
+  public class WorkspaceAttachedEvent : Event
+  {
+    public Workspace AttachedWorkspace { get; }
+
+    public WorkspaceAttachedEvent(Workspace attachedWorkspace)
+    {
+      AttachedWorkspace = attachedWorkspace;
+    }
+  }
+}
