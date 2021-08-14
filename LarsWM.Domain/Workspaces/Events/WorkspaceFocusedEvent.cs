084d1fbb032116d0b07b3427ef07c8c338790404
Lars <lars.berger@yahoo.com>
2021-08-14 18:45:58 +0200

    feat(domain): create new `Event` class for when workspaces are focused

---
+++ LarsWM.Domain/Workspaces/Events/WorkspaceFocusedEvent.cs
@@ -0,0 +1,14 @@
+﻿using LarsWM.Infrastructure.Bussing;
+
+namespace LarsWM.Domain.Workspaces.Events
+{
+  public class WorkspaceFocusedEvent : Event
+  {
+    public Workspace FocusedWorkspace { get; }
+
+    public WorkspaceFocusedEvent(Workspace focusedWorkspace)
+    {
+      FocusedWorkspace = focusedWorkspace;
+    }
+  }
+}
