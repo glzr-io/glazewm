1b52d2d4ffb8cbaaaeea6f989f2c5a434192c68d
Lars <lars.berger@yahoo.com>
2021-08-18 01:23:40 +0200

    feat(domain): raise `FocusChangedEvent` whenever a focus stack is set

--- LarsWM.Domain/Containers/CommandHandlers/CreateFocusStackHandler.cs
+++ LarsWM.Domain/Containers/CommandHandlers/CreateFocusStackHandler.cs
@@ -1,10 +1,18 @@
 ﻿using LarsWM.Domain.Containers.Commands;
+using LarsWM.Domain.Workspaces.Events;
 using LarsWM.Infrastructure.Bussing;
 
 namespace LarsWM.Domain.Containers.CommandHandlers
 {
   class CreateFocusStackHandler : ICommandHandler<CreateFocusStackCommand>
   {
+    private Bus _bus;
+
+    public CreateFocusStackHandler(Bus bus)
+    {
+      _bus = bus;
+    }
+
     public dynamic Handle(CreateFocusStackCommand command)
     {
       var target = command.FocusedContainer;
@@ -15,6 +23,8 @@ namespace LarsWM.Domain.Containers.CommandHandlers
         target.Parent.LastFocusedContainer = target;
         target = target.Parent;
       }
+
+      _bus.RaiseEvent(new FocusChangedEvent(command.FocusedContainer));
 
       return CommandResponse.Ok;
     }
