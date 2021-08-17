195ab76386bb89169f5f693bc83f359d10080785
Lars <lars.berger@yahoo.com>
2021-08-17 23:07:27 +0200

    feat(domain): create `CreateFocusStackCommand` and its handler

---
+++ LarsWM.Domain/Containers/CommandHandlers/CreateFocusStackHandler.cs
@@ -0,0 +1,22 @@
+﻿using LarsWM.Domain.Containers.Commands;
+using LarsWM.Infrastructure.Bussing;
+
+namespace LarsWM.Domain.Containers.CommandHandlers
+{
+  class CreateFocusStackHandler : ICommandHandler<CreateFocusStackCommand>
+  {
+    public dynamic Handle(CreateFocusStackCommand command)
+    {
+      var target = command.FocusedContainer;
+
+      // Traverse upwards, creating a focus stack from the focused container.
+      while (target.Parent != null)
+      {
+        target.Parent.LastFocusedContainer = target;
+        target = target.Parent;
+      }
+
+      return CommandResponse.Ok;
+    }
+  }
+}
