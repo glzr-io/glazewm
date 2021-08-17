e124b238aed1bc8657950b808882cad67c3d91be
Lars <lars.berger@yahoo.com>
2021-08-18 01:22:43 +0200

    feat(domain): create new `Event` class for when focus is changed

---
+++ LarsWM.Domain/Containers/Events/FocusChangedEvent.cs
@@ -0,0 +1,15 @@
+﻿using LarsWM.Domain.Containers;
+using LarsWM.Infrastructure.Bussing;
+
+namespace LarsWM.Domain.Workspaces.Events
+{
+  public class FocusChangedEvent : Event
+  {
+    public Container FocusedContainer { get; }
+
+    public FocusChangedEvent(Container focusedContainer)
+    {
+      FocusedContainer = focusedContainer;
+    }
+  }
+}
