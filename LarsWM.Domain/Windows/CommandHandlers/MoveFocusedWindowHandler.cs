356125222372ae3b3a420a37b614ae627a117950
Lars Berger <lars.berger@yahoo.com>
2021-08-21 14:56:08 +0200

    feat(domain): create `MoveFocusedWindowCommand` and its handler (unimplemented)

---
+++ LarsWM.Domain/Windows/CommandHandlers/MoveFocusedWindowHandler.cs
@@ -0,0 +1,37 @@
+﻿using LarsWM.Domain.Containers;
+using LarsWM.Domain.UserConfigs;
+using LarsWM.Domain.Windows.Commands;
+using LarsWM.Infrastructure.Bussing;
+
+namespace LarsWM.Domain.Windows.CommandHandlers
+{
+  class MoveFocusedWindowHandler : ICommandHandler<MoveFocusedWindowCommand>
+  {
+    private Bus _bus;
+    private WindowService _windowService;
+    private UserConfigService _userConfigService;
+    private ContainerService _containerService;
+
+    public MoveFocusedWindowHandler(Bus bus, WindowService windowService, UserConfigService userConfigService, ContainerService containerService)
+    {
+      _bus = bus;
+      _windowService = windowService;
+      _userConfigService = userConfigService;
+      _containerService = containerService;
+    }
+
+    public dynamic Handle(MoveFocusedWindowCommand command)
+    {
+      var focusedWindow = _containerService.FocusedContainer as Window;
+
+      // Ignore cases where focused container is not a window.
+      if (focusedWindow == null)
+        return CommandResponse.Ok;
+
+      var layout = (focusedWindow.Parent as SplitContainer).Layout;
+      var direction = command.Direction;
+
+      return CommandResponse.Ok;
+    }
+  }
+}
