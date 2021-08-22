68902e6e24cfe3b4e2e837ec0ae545acf8a80844
Lars Berger <lars.berger@yahoo.com>
2021-08-22 19:41:45 +0200

    feat(domain): create `ReplaceContainerCommand` and its handler

---
+++ LarsWM.Domain/Containers/CommandHandlers/ReplaceContainerHandler.cs
@@ -0,0 +1,37 @@
+﻿using LarsWM.Domain.Containers.Commands;
+using LarsWM.Infrastructure.Bussing;
+
+namespace LarsWM.Domain.Containers.CommandHandlers
+{
+  class ReplaceContainerHandler : ICommandHandler<ReplaceContainerCommand>
+  {
+    private ContainerService _containerService;
+
+    public ReplaceContainerHandler(ContainerService containerService)
+    {
+      _containerService = containerService;
+    }
+
+    public dynamic Handle(ReplaceContainerCommand command)
+    {
+      var containerToReplace = command.ContainerToReplace;
+      var replacementContainer = command.ReplacementContainer;
+      var targetIndex = containerToReplace.SelfAndSiblings.IndexOf(containerToReplace);
+
+      // Replace the split container with the focused window.
+      containerToReplace.Parent.Children[targetIndex] = replacementContainer;
+      replacementContainer.Parent = containerToReplace.Parent;
+      replacementContainer.SizePercentage = containerToReplace.SizePercentage;
+
+      // Correct focus stack references to replaced container.
+      if (containerToReplace.Parent.LastFocusedContainer == containerToReplace)
+        containerToReplace.Parent.LastFocusedContainer = replacementContainer;
+
+      // TODO: Not sure whether redrawing is necessary, will see after fixing detach command.
+      _containerService.SplitContainersToRedraw.Add(containerToReplace.Parent as SplitContainer);
+      _containerService.SplitContainersToRedraw.Add(replacementContainer.Parent as SplitContainer);
+
+      return CommandResponse.Ok;
+    }
+  }
+}
---
+++ LarsWM.Domain/Containers/Commands/ReplaceContainerCommand.cs
@@ -0,0 +1,16 @@
+﻿using LarsWM.Infrastructure.Bussing;
+
+namespace LarsWM.Domain.Containers.Commands
+{
+  public class ReplaceContainerCommand : Command
+  {
+    public Container ContainerToReplace { get; }
+    public Container ReplacementContainer { get; }
+
+    public ReplaceContainerCommand(Container containerToReplace, Container replacementContainer)
+    {
+      ContainerToReplace = containerToReplace;
+      ReplacementContainer = replacementContainer;
+    }
+  }
+}
