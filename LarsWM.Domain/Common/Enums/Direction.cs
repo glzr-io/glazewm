88856062c5a459c4742ed16019feccb0ebf9e9ac
Lars Berger <lars.berger@yahoo.com>
2021-08-27 01:12:28 +0200

    feat(domain): add extension methods for `Direction` enum for getting the inverse direction and layout for direction

--- LarsWM.Domain/Common/Enums/Direction.cs
+++ LarsWM.Domain/Common/Enums/Direction.cs
@@ -1,4 +1,6 @@
-﻿namespace LarsWM.Domain.Common.Enums
+﻿using System;
+
+namespace LarsWM.Domain.Common.Enums
 {
   public enum Direction
   {
@@ -6,5 +8,37 @@
     DOWN,
     LEFT,
     RIGHT,
+  }
+
+  public static class DirectionExtensions
+  {
+    /// <summary>
+    /// Get the inverse of a given direction (eg. `Direction.UP` is the inverse of `Direction.DOWN`).
+    /// </summary>
+    public static Direction Inverse(this Direction direction)
+    {
+      switch (direction)
+      {
+        case Direction.UP:
+          return Direction.DOWN;
+        case Direction.DOWN:
+          return Direction.UP;
+        case Direction.LEFT:
+          return Direction.RIGHT;
+        case Direction.RIGHT:
+          return Direction.LEFT;
+        default: throw new ArgumentOutOfRangeException();
+      }
+    }
+
+    /// <summary>
+    /// Get the layout that is needed for when moving or switching focus in given direction (eg. a
+    /// horizontal layout when moving horizontally).
+    /// </summary>
+    public static Layout GetCorrespondingLayout(this Direction direction)
+    {
+      return (direction == Direction.LEFT || direction == Direction.RIGHT)
+        ? Layout.Horizontal : Layout.Vertical;
+    }
   }
 }
