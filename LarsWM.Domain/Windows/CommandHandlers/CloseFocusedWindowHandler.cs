bb8f6cf23158b333efbbc3a11ff5a51ac3c5ec35
Lars Berger <lars.berger@yahoo.com>
2021-08-27 11:16:48 +0200

    feat(domain): create `CloseFocusedWindowCommand` and its handler

---
+++ LarsWM.Domain/Windows/CommandHandlers/CloseFocusedWindowHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using LarsWM.Domain.Containers;
+using LarsWM.Domain.Windows.Commands;
+using LarsWM.Infrastructure.Bussing;
+using static LarsWM.Infrastructure.WindowsApi.WindowsApiService;
+
+namespace LarsWM.Domain.Windows.CommandHandlers
+{
+  class CloseFocusedWindowHandler : ICommandHandler<CloseFocusedWindowCommand>
+  {
+    private Bus _bus;
+    private ContainerService _containerService;
+
+    public CloseFocusedWindowHandler(Bus bus, ContainerService containerService)
+    {
+      _bus = bus;
+      _containerService = containerService;
+    }
+
+    public dynamic Handle(CloseFocusedWindowCommand command)
+    {
+      var focusedWindow = _containerService.FocusedContainer as Window;
+
+      if (focusedWindow == null)
+        return CommandResponse.Ok;
+
+      SendMessage(focusedWindow.Hwnd, SendMessageType.WM_CLOSE, IntPtr.Zero, IntPtr.Zero);
+
+      return CommandResponse.Ok;
+    }
+  }
+}
