6e302512a960ec780862de9258e07fa7d7dad209
Lars Berger <lars.berger@yahoo.com>
2021-09-01 02:17:57 +0200

    feat(domain): create yaml deserializer for validating data annotations

---
+++ LarsWM.Domain/UserConfigs/ValidatingDeserializer.cs
@@ -0,0 +1,30 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using YamlDotNet.Core;
+using YamlDotNet.Serialization;
+
+namespace LarsWM.Domain.UserConfigs
+{
+  public class ValidatingDeserializer : INodeDeserializer
+  {
+    private readonly INodeDeserializer _nodeDeserializer;
+
+    public ValidatingDeserializer(INodeDeserializer nodeDeserializer)
+    {
+      _nodeDeserializer = nodeDeserializer;
+    }
+
+    public bool Deserialize(IParser parser, Type expectedType,
+        Func<IParser, Type, object> nestedObjectDeserializer, out object value)
+    {
+      if (!_nodeDeserializer.Deserialize(parser, expectedType, nestedObjectDeserializer, out value))
+        return false;
+
+      // Validate using the provided data annotations (eg. `[Required]`).
+      var context = new ValidationContext(value, null, null);
+      Validator.ValidateObject(value, context, true);
+
+      return true;
+    }
+  }
+}
