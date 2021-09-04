433ac7de6d8b4e0d84d1ebf9bd64ffd40509cc6b
Lars Berger <lars.berger@yahoo.com>
2021-09-04 12:12:45 +0200

    feat(boot): set process DPI awareness to `PerMonitorAwareV2`

--- LarsWM.Bootstrapper/Program.cs
+++ LarsWM.Bootstrapper/Program.cs
@@ -4,6 +4,7 @@ using LarsWM.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Diagnostics;
+using static LarsWM.Infrastructure.WindowsApi.WindowsApiService;
 
 namespace LarsWM.Bootstrapper
 {
@@ -16,6 +17,9 @@ namespace LarsWM.Bootstrapper
     static void Main()
     {
       Debug.WriteLine("Application started");
+
+      // Set the process-default DPI awareness.
+      SetProcessDpiAwarenessContext(DpiAwarenessContext.Context_PerMonitorAwareV2);
 
       var serviceCollection = new ServiceCollection();
       serviceCollection.AddInfrastructureServices();
