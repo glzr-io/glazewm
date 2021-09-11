550d421a41856f3f8d9469c2b915e38999cca303
Lars Berger <lars.berger@yahoo.com>
2021-09-11 12:17:41 +0200

    feat(domain): create `ChangeFocusedContainerLayoutCommand` and its handler

---
+++ LarsWM.Domain/Containers/CommandHandlers/ChangeFocusedContainerLayoutHandler.cs
@@ -0,0 +1,33 @@
+﻿using LarsWM.Domain.Containers.Commands;
+using LarsWM.Domain.Windows;
+using LarsWM.Infrastructure.Bussing;
+using static LarsWM.Infrastructure.WindowsApi.WindowsApiService;
+
+namespace LarsWM.Domain.Containers.CommandHandlers
+{
+  class ChangeFocusedContainerLayoutHandler : ICommandHandler<ChangeFocusedContainerLayoutCommand>
+  {
+    private Bus _bus;
+    private ContainerService _containerService;
+
+    public ChangeFocusedContainerLayoutHandler(Bus bus, ContainerService containerService)
+    {
+      _bus = bus;
+      _containerService = containerService;
+    }
+
+    public CommandResponse Handle(ChangeFocusedContainerLayoutCommand command)
+    {
+      var focusedContainer = _containerService.FocusedContainer;
+      var foregroundWindow = GetForegroundWindow();
+
+      // Ignore cases where focused container is a window that's not in foreground.
+      if (focusedContainer is Window && foregroundWindow != (focusedContainer as Window).Hwnd)
+        return CommandResponse.Ok;
+
+      _bus.Invoke(new ChangeContainerLayoutCommand(focusedContainer, command.NewLayout));
+
+      return CommandResponse.Ok;
+    }
+  }
+}
---
+++ LarsWM.Domain/Containers/Commands/ChangeFocusedContainerLayoutCommand.cs
@@ -0,0 +1,15 @@
+﻿using LarsWM.Domain.Common.Enums;
+using LarsWM.Infrastructure.Bussing;
+
+namespace LarsWM.Domain.Containers.Commands
+{
+  public class ChangeFocusedContainerLayoutCommand : Command
+  {
+    public Layout NewLayout { get; }
+
+    public ChangeFocusedContainerLayoutCommand(Layout newLayout)
+    {
+      NewLayout = newLayout;
+    }
+  }
+}
