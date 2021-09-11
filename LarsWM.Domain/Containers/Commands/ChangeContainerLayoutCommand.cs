1e930765bf80f5425960c13abf62f8812ff60fca
Lars Berger <lars.berger@yahoo.com>
2021-09-11 12:01:55 +0200

    feat(domain): add `ChangeContainerLayoutCommand` argument for container to change * Instead of always using focused container. Also allows command to be called with empty workspaces.

--- LarsWM.Domain/Containers/Commands/ChangeContainerLayoutCommand.cs
+++ LarsWM.Domain/Containers/Commands/ChangeContainerLayoutCommand.cs
@@ -1,15 +1,16 @@
 ﻿using LarsWM.Domain.Common.Enums;
-using LarsWM.Domain.Common.Models;
 using LarsWM.Infrastructure.Bussing;
 
 namespace LarsWM.Domain.Containers.Commands
 {
   public class ChangeContainerLayoutCommand : Command
   {
+    public Container Container { get; }
     public Layout NewLayout { get; }
 
-    public ChangeContainerLayoutCommand(Layout newLayout)
+    public ChangeContainerLayoutCommand(Container container, Layout newLayout)
     {
+      Container = container;
       NewLayout = newLayout;
     }
   }
