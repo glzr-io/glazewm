c0cfcb2700156d41b0256208e44e34f36e0bc76e
Lars Berger <lars.berger@yahoo.com>
2021-09-14 02:06:22 +0200

    feat(infra): create `Event` classes for relevant window events

---
+++ LarsWM.Infrastructure/WindowsApi/Events/WindowHiddenEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using LarsWM.Infrastructure.Bussing;
+
+namespace LarsWM.Infrastructure.WindowsApi.Events
+{
+  public class WindowHiddenEvent : Event
+  {
+    public IntPtr WindowHandle { get; }
+
+    public WindowHiddenEvent(IntPtr windowHandle)
+    {
+      WindowHandle = windowHandle;
+    }
+  }
+}
---
+++ LarsWM.Infrastructure/WindowsApi/Events/WindowMinimizeEndedEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using LarsWM.Infrastructure.Bussing;
+
+namespace LarsWM.Infrastructure.WindowsApi.Events
+{
+  public class WindowMinimizeEndedEvent : Event
+  {
+    public IntPtr WindowHandle { get; }
+
+    public WindowMinimizeEndedEvent(IntPtr windowHandle)
+    {
+      WindowHandle = windowHandle;
+    }
+  }
+}
