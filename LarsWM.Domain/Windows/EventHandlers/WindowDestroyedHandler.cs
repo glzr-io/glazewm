1a75d8bdd617aa233510d0842591bc08528a1358
Lars Berger <lars.berger@yahoo.com>
2021-09-14 14:51:28 +0200

    feat(domain): create handlers for show, destroy, and focus window events

---
+++ LarsWM.Domain/Windows/EventHandlers/WindowDestroyedHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using LarsWM.Domain.Containers.Commands;
+using LarsWM.Domain.Windows.Commands;
+using LarsWM.Infrastructure.Bussing;
+using LarsWM.Infrastructure.WindowsApi.Events;
+
+namespace LarsWM.Domain.Windows.EventHandlers
+{
+  class WindowDestroyedHandler : IEventHandler<WindowDestroyedEvent>
+  {
+    private Bus _bus;
+    private WindowService _windowService;
+
+    public WindowDestroyedHandler(Bus bus, WindowService windowService)
+    {
+      _bus = bus;
+      _windowService = windowService;
+    }
+
+    public void Handle(WindowDestroyedEvent @event)
+    {
+      var window = _windowService.GetWindows()
+        .FirstOrDefault(window => window.Hwnd == @event.WindowHandle);
+
+      if (window == null)
+        return;
+
+      // If window is in tree, detach the removed window from its parent.
+      _bus.Invoke(new RemoveWindowCommand(window));
+      _bus.Invoke(new RedrawContainersCommand());
+    }
+  }
+}
---
+++ LarsWM.Domain/Windows/EventHandlers/WindowShownHandler.cs
@@ -0,0 +1,21 @@
+﻿using LarsWM.Domain.Windows.Commands;
+using LarsWM.Infrastructure.Bussing;
+using LarsWM.Infrastructure.WindowsApi.Events;
+
+namespace LarsWM.Domain.Windows.EventHandlers
+{
+  class WindowShownHandler : IEventHandler<WindowShownEvent>
+  {
+    private Bus _bus;
+
+    public WindowShownHandler(Bus bus)
+    {
+      _bus = bus;
+    }
+
+    public void Handle(WindowShownEvent @event)
+    {
+      _bus.Invoke(new AddWindowCommand(@event.WindowHandle));
+    }
+  }
+}
