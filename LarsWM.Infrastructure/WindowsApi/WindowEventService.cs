b5dc47f88a3ff4c9691d75e474be14e276744025
Lars Berger <lars.berger@yahoo.com>
2021-09-14 02:03:46 +0200

    feat(infra): listen for window hidden event in window event hook

--- LarsWM.Infrastructure/WindowsApi/WindowEventService.cs
+++ LarsWM.Infrastructure/WindowsApi/WindowEventService.cs
@@ -9,8 +9,6 @@ namespace LarsWM.Infrastructure.WindowsApi
 {
   public class WindowEventService
   {
-    // TODO: Change from Subject to Observable.
-    // TODO: Create the Observable in constructor instead of `Init` method.
     public Subject<WindowHookEvent> WindowHookSubject = new Subject<WindowHookEvent>();
 
     public static int CHILDID_SELF = 0;
@@ -29,29 +27,31 @@ namespace LarsWM.Infrastructure.WindowsApi
 
     public void Init()
     {
-      var callback = new WindowEventProc((IntPtr hWinEventHook, EventConstant eventType, IntPtr hwnd, ObjectIdentifier idObject, int idChild, uint dwEventThread, uint dwmsEventTime) =>
-      {
-        var isWindowEvent = idChild == CHILDID_SELF && idObject == ObjectIdentifier.OBJID_WINDOW && hwnd != IntPtr.Zero;
-
-        if (!isWindowEvent)
-          return;
-
-        var windowHookEvent = new WindowHookEvent(eventType, hwnd);
-        WindowHookSubject.OnNext(windowHookEvent);
-      });
-
-      // SetWinEventHook requires a message loop within the thread that is executing the code.
-      var thread = new Thread(() =>
-      {
-        SetWinEventHook(EventConstant.EVENT_OBJECT_DESTROY, EventConstant.EVENT_OBJECT_SHOW, IntPtr.Zero, callback, 0, 0, 0);
-        SetWinEventHook(EventConstant.EVENT_OBJECT_CLOAKED, EventConstant.EVENT_OBJECT_UNCLOAKED, IntPtr.Zero, callback, 0, 0, 0);
-        SetWinEventHook(EventConstant.EVENT_SYSTEM_MINIMIZESTART, EventConstant.EVENT_SYSTEM_MINIMIZEEND, IntPtr.Zero, callback, 0, 0, 0);
-        SetWinEventHook(EventConstant.EVENT_SYSTEM_FOREGROUND, EventConstant.EVENT_SYSTEM_FOREGROUND, IntPtr.Zero, callback, 0, 0, 0);
-        Application.Run();
-      });
+      var thread = new Thread(() => CreateWindowEventHook());
       thread.Name = "LarsWMWindowHooks";
       thread.Start();
+    }
 
+    private void CreateWindowEventHook()
+    {
+      SetWinEventHook(EventConstant.EVENT_OBJECT_DESTROY, EventConstant.EVENT_OBJECT_HIDE, IntPtr.Zero, WindowEventHookProc, 0, 0, 0);
+      SetWinEventHook(EventConstant.EVENT_OBJECT_CLOAKED, EventConstant.EVENT_OBJECT_UNCLOAKED, IntPtr.Zero, WindowEventHookProc, 0, 0, 0);
+      SetWinEventHook(EventConstant.EVENT_SYSTEM_MINIMIZESTART, EventConstant.EVENT_SYSTEM_MINIMIZEEND, IntPtr.Zero, WindowEventHookProc, 0, 0, 0);
+      SetWinEventHook(EventConstant.EVENT_SYSTEM_FOREGROUND, EventConstant.EVENT_SYSTEM_FOREGROUND, IntPtr.Zero, WindowEventHookProc, 0, 0, 0);
+
+      // `SetWindowsHookEx` requires a message loop within the thread that is executing the code.
+      Application.Run();
+    }
+
+    private void WindowEventHookProc(IntPtr hWinEventHook, EventConstant eventType, IntPtr hwnd, ObjectIdentifier idObject, int idChild, uint dwEventThread, uint dwmsEventTime)
+    {
+      var isWindowEvent = idChild == CHILDID_SELF && idObject == ObjectIdentifier.OBJID_WINDOW && hwnd != IntPtr.Zero;
+
+      if (!isWindowEvent)
+        return;
+
+      var windowHookEvent = new WindowHookEvent(eventType, hwnd);
+      WindowHookSubject.OnNext(windowHookEvent);
     }
   }
 }
