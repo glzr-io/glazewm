bbe7bc1d42e8d66a5a4df11ffbc41e6f9366b666
Lars Berger <lars.berger@yahoo.com>
2021-09-14 15:47:59 +0200

    feat(domain): create handler for window hidden event

---
+++ LarsWM.Domain/Windows/EventHandlers/WindowHiddenHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using LarsWM.Domain.Containers.Commands;
+using LarsWM.Domain.Windows.Commands;
+using LarsWM.Infrastructure.Bussing;
+using LarsWM.Infrastructure.WindowsApi.Events;
+
+namespace LarsWM.Domain.Windows.EventHandlers
+{
+  class WindowHiddenHandler : IEventHandler<WindowHiddenEvent>
+  {
+    private Bus _bus;
+    private WindowService _windowService;
+
+    public WindowHiddenHandler(Bus bus, WindowService windowService)
+    {
+      _bus = bus;
+      _windowService = windowService;
+    }
+
+    public void Handle(WindowHiddenEvent @event)
+    {
+      var window = _windowService.GetWindows()
+        .FirstOrDefault(window => window.Hwnd == @event.WindowHandle);
+
+      // Ignore cases where the window isn't managed or is actually supposed to be hidden.
+      if (window == null || window.IsHidden == true)
+        return;
+
+      // Detach the hidden window from its parent.
+      _bus.Invoke(new RemoveWindowCommand(window));
+      _bus.Invoke(new RedrawContainersCommand());
+    }
+  }
+}
