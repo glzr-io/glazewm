058064a97f58009fa2661507996c605ee1c6cd12
Lars Berger <lars.berger@yahoo.com>
2021-09-15 10:58:29 +0200

    fix(domain): avoid adding windows that are already managed

--- LarsWM.Domain/Windows/EventHandlers/WindowShownHandler.cs
+++ LarsWM.Domain/Windows/EventHandlers/WindowShownHandler.cs
@@ -1,4 +1,5 @@
-﻿using LarsWM.Domain.Windows.Commands;
+﻿using System.Linq;
+using LarsWM.Domain.Windows.Commands;
 using LarsWM.Infrastructure.Bussing;
 using LarsWM.Infrastructure.WindowsApi.Events;
 
@@ -7,14 +8,23 @@ namespace LarsWM.Domain.Windows.EventHandlers
   class WindowShownHandler : IEventHandler<WindowShownEvent>
   {
     private Bus _bus;
+    private WindowService _windowService;
 
-    public WindowShownHandler(Bus bus)
+    public WindowShownHandler(Bus bus, WindowService windowService)
     {
       _bus = bus;
+      _windowService = windowService;
     }
 
     public void Handle(WindowShownEvent @event)
     {
+      var window = _windowService.GetWindows()
+        .FirstOrDefault(window => window.Hwnd == @event.WindowHandle);
+
+      // Ignore cases where window is already managed.
+      if (window != null)
+        return;
+
       _bus.Invoke(new AddWindowCommand(@event.WindowHandle));
     }
   }
