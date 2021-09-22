77a96ec7e628f5d0bd8bf94b3d86553179a9db1f
Lars Berger <lars.berger@yahoo.com>
2021-09-22 18:52:33 +0200

    feat(domain): throw user error if there aren't enough workspaces for # of monitors

--- LarsWM.Domain/Monitors/EventHandlers/MonitorAddedHandler.cs
+++ LarsWM.Domain/Monitors/EventHandlers/MonitorAddedHandler.cs
@@ -3,8 +3,6 @@ using LarsWM.Domain.Monitors.Events;
 using LarsWM.Domain.Workspaces.Commands;
 using System.Linq;
 using LarsWM.Domain.Workspaces;
-using LarsWM.Domain.Monitors.CommandHandlers;
-using LarsWM.Domain.Monitors.Commands;
 
 namespace LarsWM.Domain.Monitors.EventHandler
 {
@@ -19,15 +17,14 @@ namespace LarsWM.Domain.Monitors.EventHandler
       _workspaceService = workspaceService;
     }
 
+    // TODO: Move this to `AddMonitorHandler`.
     public void Handle(MonitorAddedEvent @event)
     {
       // Get first workspace that is not active.
-      var inactiveWorkspace = _workspaceService.InactiveWorkspaces[0];
+      var inactiveWorkspace = _workspaceService.InactiveWorkspaces.ElementAtOrDefault(0);
 
       if (inactiveWorkspace == null)
-      {
-        // TODO: Show some kind of error notification.
-      }
+        throw new FatalUserException("At least 1 workspace is required per monitor.");
 
       // Assign the workspace to the newly added monitor.
       _bus.Invoke(new AttachWorkspaceToMonitorCommand(inactiveWorkspace, @event.AddedMonitor));
