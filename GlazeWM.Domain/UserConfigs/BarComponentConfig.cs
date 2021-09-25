dae22c71980efc2eba50462da511ad4a05fb5ef9
Lars Berger <lars.berger@yahoo.com>
2021-09-25 20:47:58 +0200

    feat(domain): create class for bar component configs; move out shared `BarConfig` properties to new class

---
+++ GlazeWM.Domain/UserConfigs/BarComponentConfig.cs
@@ -0,0 +1,7 @@
+namespace GlazeWM.Domain.UserConfigs
+{
+  public class BarComponentConfig : CommonBarAttributes
+  {
+    public string Margin { get; set; } = "0 10 0 0";
+  }
+}
--- GlazeWM.Domain/UserConfigs/BarConfig.cs
+++ GlazeWM.Domain/UserConfigs/BarConfig.cs
@@ -1,24 +1,8 @@
 namespace GlazeWM.Domain.UserConfigs
 {
-  public class BarConfig
+  public class BarConfig : CommonBarAttributes
   {
     public uint Height { get; set; } = 25;
-    public string Background { get; set; } = "#101010";
-    public string FontFamily { get; set; } = "Segoe UI";
-    public string FontSize { get; set; } = "12";
-    public string BorderColor { get; set; } = "#8192B3";
-
-    /// <summary>
-    /// Width of the border in pixels. To set a different border width for each side, specify four
-    /// values (eg. "5 0 5 0"). The borders widths apply to the top, right, bottom, and left in that
-    /// order.
-    /// </summary>
-    public string BorderWidth { get; set; } = "0 0 0 0";
-
-    /// <summary>
-    /// Padding in pixels.
-    /// </summary>
-    public string Padding { get; set; } = "1 6 1 6";
-    public double Opacity { get; set; } = 1;
+    public double Opacity { get; set; } = 0.9;
   }
 }
