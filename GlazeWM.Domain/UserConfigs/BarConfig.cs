bd64a373fc9343db68063802c0e8d6b965f2a82e
Lars Berger <lars.berger@yahoo.com>
2021-09-26 15:17:59 +0200

    feat(domain): add hard-coded component configs to `BarConfig`

--- GlazeWM.Domain/UserConfigs/BarConfig.cs
+++ GlazeWM.Domain/UserConfigs/BarConfig.cs
@@ -1,8 +1,30 @@
+using System.Collections.Generic;
+
 namespace GlazeWM.Domain.UserConfigs
 {
   public class BarConfig : CommonBarAttributes
   {
     public uint Height { get; set; } = 25;
+
     public double Opacity { get; set; } = 0.9;
+
+    public List<BarComponentConfig> ComponentsLeft = CreateMockComponentConfig();
+    public List<BarComponentConfig> ComponentsCenter = new List<BarComponentConfig>();
+    public List<BarComponentConfig> ComponentsRight = CreateMockComponentConfig();
+
+    private static List<BarComponentConfig> CreateMockComponentConfig()
+    {
+      var sampleConfig1 = new WorkspacesComponentConfig()
+      {
+        Type = "workspaces",
+      };
+
+      var sampleConfig2 = new ClockComponentConfig()
+      {
+        Type = "clock",
+      };
+
+      return new List<BarComponentConfig> { sampleConfig1, sampleConfig2 };
+    }
   }
 }
