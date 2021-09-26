a389b6a3dc3bb60d65bef300518b29a535ed4998
Lars Berger <lars.berger@yahoo.com>
2021-09-26 15:48:20 +0200

    feat(bar): create `UserControl`s for clock and workspaces component types (unimplemented)

---
+++ GlazeWM.Bar/Components/WorkspacesComponent.xaml.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GlazeWM.Bar.Components
+{
+  /// <summary>
+  /// Interaction logic for WorkspacesComponent.xaml
+  /// </summary>
+  public partial class WorkspacesComponent : UserControl
+  {
+    public WorkspacesComponent()
+    {
+      InitializeComponent();
+    }
+  }
+}
+
