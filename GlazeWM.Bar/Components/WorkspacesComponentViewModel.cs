b71315ebb9d67b58ba43b81014d531db47269e35
Lars Berger <lars.berger@yahoo.com>
2021-09-27 21:34:50 +0200

    feat(bar): create basic view model for workspaces component

---
+++ GlazeWM.Bar/Components/WorkspacesComponentViewModel.cs
@@ -0,0 +1,45 @@
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Threading;
+using GlazeWM.Domain.Monitors;
+using GlazeWM.Domain.Workspaces;
+
+namespace GlazeWM.Bar
+{
+  public class WorkspacesComponentViewModel : INotifyPropertyChanged
+  {
+    public event PropertyChangedEventHandler PropertyChanged;
+    public ObservableCollection<Workspace> Workspaces { get; set; } = new ObservableCollection<Workspace>();
+    private readonly BarViewModel _barViewModel;
+    private readonly WorkspaceService _workspaceService;
+    private Dispatcher _dispatcher => _barViewModel.Dispatcher;
+    private Monitor _monitor => _barViewModel.Monitor;
+
+    public WorkspacesComponentViewModel(BarViewModel barViewModel, WorkspaceService workspaceService)
+    {
+      _barViewModel = barViewModel;
+      _workspaceService = workspaceService;
+    }
+
+    public void InitializeState()
+    {
+      UpdateWorkspaces();
+    }
+
+    public void UpdateWorkspaces()
+    {
+      _dispatcher.Invoke(() =>
+      {
+        Workspaces.Clear();
+
+        foreach (var workspace in _monitor.Children)
+          Workspaces.Add(workspace as Workspace);
+      });
+    }
+
+    private void OnPropertyChanged(string propertyName)
+    {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+  }
+}
