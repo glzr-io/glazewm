7bee68087d3319ac51dc9961c85844b2e087f0e9
Lars Berger <lars.berger@yahoo.com>
2021-09-29 02:01:29 +0200

    feat(bar): move out component grid from `ComponentContainer`; rename `ComponentContainer` -> `ComponentPortal`

---
+++ GlazeWM.Bar/Components/ComponentPortal.xaml.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GlazeWM.Domain.UserConfigs;
+
+namespace GlazeWM.Bar.Components
+{
+  /// <summary>
+  /// Interaction logic for ComponentPortal.xaml
+  /// </summary>
+  public partial class ComponentPortal : UserControl
+  {
+    public ComponentPortal()
+    {
+      InitializeComponent();
+    }
+  }
+}
+
