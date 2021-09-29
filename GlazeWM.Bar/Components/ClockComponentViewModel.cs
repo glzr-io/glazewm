e714175f57ef1d97fa648c5832d60f4fb06ae221
Lars Berger <lars.berger@yahoo.com>
2021-09-29 18:58:48 +0200

    feat(bar): implement view and view model for clock component

--- GlazeWM.Bar/Components/ClockComponentViewModel.cs
+++ GlazeWM.Bar/Components/ClockComponentViewModel.cs
@@ -1,9 +1,19 @@
+using System;
+using System.Reactive.Linq;
+
 namespace GlazeWM.Bar.Components
 {
   public class ClockComponentViewModel : ComponentViewModel
   {
+    public string FormattedTime =>
+      $"{DateTime.Now.ToShortTimeString()} {DateTime.Now.ToShortDateString()}";
+
     public ClockComponentViewModel(BarViewModel parentViewModel) : base(parentViewModel)
     {
+      var updateInterval = TimeSpan.FromSeconds(1);
+
+      Observable.Interval(updateInterval)
+        .Subscribe(_ => OnPropertyChanged(nameof(FormattedTime)));
     }
   }
 }
