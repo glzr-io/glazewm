2accb4c10791dbf494afb9f997222a7f57227f03
Lars Berger <lars.berger@yahoo.com>
2021-09-30 22:50:52 +0200

    feat(infra): convert YAML to JSON prior to deserializing

--- GlazeWM.Infrastructure/Yaml/YamlDeserializationService.cs
+++ GlazeWM.Infrastructure/Yaml/YamlDeserializationService.cs
@@ -1,23 +1,30 @@
 using System.IO;
+using Newtonsoft.Json;
 using YamlDotNet.Serialization;
 using YamlDotNet.Serialization.NamingConventions;
-using YamlDotNet.Serialization.NodeDeserializers;
 
 namespace GlazeWM.Infrastructure.Yaml
 {
   public class YamlDeserializationService
   {
-    IDeserializer _deserializer = new DeserializerBuilder()
+    IDeserializer _yamlDeserializer = new DeserializerBuilder()
       .WithNamingConvention(PascalCaseNamingConvention.Instance)
-      .WithNodeDeserializer(
-        inner => new ValidatingDeserializer(inner),
-        component => component.InsteadOf<ObjectNodeDeserializer>()
-      )
       .Build();
 
+    ISerializer _jsonSerializer = new SerializerBuilder()
+      .JsonCompatible()
+      .Build();
+
+    /// <summary>
+    /// The YAML deserializing library doesn't have support for polymorphic objects. Because of
+    /// this, the YAML is first converted into JSON and then deserialized via `Newtonsoft.Json`.
+    /// </summary>
     public T Deserialize<T>(TextReader input)
     {
-      return _deserializer.Deserialize<T>(input);
+      var yamlObject = _yamlDeserializer.Deserialize(input);
+      var jsonString = _jsonSerializer.Serialize(yamlObject);
+
+      return JsonConvert.DeserializeObject<T>(jsonString);
     }
   }
 }
