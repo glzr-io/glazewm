666cece2e1a12d84933ca2da5a739eac31d92878
Lars Berger <lars.berger@yahoo.com>
2021-10-01 15:58:46 +0200

    feat(bar): add bindings for component margin, background, border width, border color, and padding

--- GlazeWM.Bar/Components/ComponentViewModel.cs
+++ GlazeWM.Bar/Components/ComponentViewModel.cs
@@ -1,17 +1,27 @@
 using GlazeWM.Bar.Common;
 using GlazeWM.Domain.UserConfigs;
+using GlazeWM.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace GlazeWM.Bar.Components
 {
   public class ComponentViewModel : ViewModelBase
   {
+    private BarService _barService = ServiceLocator.Provider.GetRequiredService<BarService>();
     protected readonly BarViewModel _parentViewModel;
     protected readonly BarComponentConfig _baseConfig;
+    public string Background => _baseConfig.Background;
+    public string FontFamily => _baseConfig.FontFamily;
+    public string FontSize => _baseConfig.FontSize;
+    public string BorderColor => _baseConfig.BorderColor;
+    public string BorderWidth => _barService.ShorthandToXamlProperty(_baseConfig.BorderWidth);
+    public string Padding => _barService.ShorthandToXamlProperty(_baseConfig.Padding);
+    public string Margin => _barService.ShorthandToXamlProperty(_baseConfig.Margin);
 
-    public ComponentViewModel(BarViewModel parentViewModel, BarComponentConfig config)
+    public ComponentViewModel(BarViewModel parentViewModel, BarComponentConfig baseConfig)
     {
       _parentViewModel = parentViewModel;
-      _config = config;
+      _baseConfig = baseConfig;
     }
   }
 }
