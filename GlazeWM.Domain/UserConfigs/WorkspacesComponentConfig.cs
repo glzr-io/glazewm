a92bf48877bcb09d19300ff5c55e3f40eca07fa4
Lars Berger <lars.berger@yahoo.com>
2021-10-02 15:58:32 +0200

    feat(domain): add `WorkspaceComponentConfig` properties for styling focused, displayed, and normal workspaces

--- GlazeWM.Domain/UserConfigs/WorkspacesComponentConfig.cs
+++ GlazeWM.Domain/UserConfigs/WorkspacesComponentConfig.cs
@@ -2,5 +2,19 @@ namespace GlazeWM.Domain.UserConfigs
 {
   public class WorkspacesComponentConfig : BarComponentConfig
   {
+    public string FocusedWorkspaceBorderWidth { get; set; } = "0";
+    public string FocusedWorkspaceBorderColor { get; set; } = "blue";
+    public string FocusedWorkspaceBackground { get; set; } = "#8192B3";
+    public string FocusedWorkspaceForeground { get; set; } = "white";
+
+    public string DisplayedWorkspaceBorderWidth { get; set; } = "0";
+    public string DisplayedWorkspaceBorderColor { get; set; } = "blue";
+    public string DisplayedWorkspaceBackground { get; set; } = "#42403e";
+    public string DisplayedWorkspaceForeground { get; set; } = "white";
+
+    public string DefaultWorkspaceBorderWidth { get; set; } = "0";
+    public string DefaultWorkspaceBorderColor { get; set; } = "blue";
+    public string DefaultWorkspaceBackground { get; set; } = "transparent";
+    public string DefaultWorkspaceForeground { get; set; } = "white";
   }
 }
