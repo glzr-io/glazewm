8955b38f08d2e9a6c7a77be899e83c84f583282e
Lars Berger <lars.berger@yahoo.com>
2021-10-08 18:00:02 +0200

    feat(domain): create `ToggleFocusedWindowFloatingCommand` and its handler

---
+++ GlazeWM.Domain/Windows/CommandHandlers/ToggleFocusedWindowFloatingHandler.cs
@@ -0,0 +1,31 @@
+﻿using GlazeWM.Domain.Containers;
+using GlazeWM.Domain.Windows.Commands;
+using GlazeWM.Infrastructure.Bussing;
+
+namespace GlazeWM.Domain.Windows.CommandHandlers
+{
+  class ToggleFocusedWindowFloatingHandler : ICommandHandler<ToggleFocusedWindowFloatingCommand>
+  {
+    private Bus _bus;
+    private ContainerService _containerService;
+
+    public ToggleFocusedWindowFloatingHandler(Bus bus, ContainerService containerService)
+    {
+      _bus = bus;
+      _containerService = containerService;
+    }
+
+    public CommandResponse Handle(ToggleFocusedWindowFloatingCommand command)
+    {
+      var focusedWindow = _containerService.FocusedContainer as TilingWindow;
+
+      // Ignore cases where focused container is not a window.
+      if (focusedWindow == null)
+        return CommandResponse.Ok;
+
+      _bus.Invoke(new ToggleFloatingCommand(focusedWindow));
+
+      return CommandResponse.Ok;
+    }
+  }
+}
