8c3ecd736d0b774a1b065dafb2cd9fa7e21170a9
Lars Berger <lars.berger@yahoo.com>
2021-10-16 14:37:18 +0200

    feat(domain): create `FlattenSplitContainerCommand` and its handler

---
+++ GlazeWM.Domain/Containers/Commands/FlattenSplitContainerCommand.cs
@@ -0,0 +1,14 @@
+﻿using GlazeWM.Infrastructure.Bussing;
+
+namespace GlazeWM.Domain.Containers.Commands
+{
+  public class FlattenSplitContainerCommand : Command
+  {
+    public SplitContainer ContainerToFlatten { get; }
+
+    public FlattenSplitContainerCommand(SplitContainer parent)
+    {
+      ContainerToFlatten = parent;
+    }
+  }
+}
