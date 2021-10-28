209dd66e3f78d5ecf89cc659ed81a09b711b08de
Lars Berger <lars.berger@yahoo.com>
2021-10-28 22:41:07 +0200

    feat(domain): create `AttachAndResizeContainerCommand` and its handler

---
+++ GlazeWM.Domain/Containers/Commands/AttachAndResizeContainerCommand.cs
@@ -0,0 +1,26 @@
+﻿using GlazeWM.Infrastructure.Bussing;
+
+namespace GlazeWM.Domain.Containers.Commands
+{
+  public class AttachAndResizeContainerCommand : Command
+  {
+    public Container Parent { get; }
+    public Container ChildToAdd { get; }
+    public int InsertPosition { get; }
+
+    // Insert child as end element if `insertPosition` is not provided.
+    public AttachAndResizeContainerCommand(Container parent, Container childToAdd)
+    {
+      Parent = parent;
+      ChildToAdd = childToAdd;
+      InsertPosition = parent.Children.Count;
+    }
+
+    public AttachAndResizeContainerCommand(Container parent, Container childToAdd, int insertPosition)
+    {
+      Parent = parent;
+      ChildToAdd = childToAdd;
+      InsertPosition = insertPosition;
+    }
+  }
+}
