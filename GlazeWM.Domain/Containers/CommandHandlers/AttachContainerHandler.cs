70f1cb4c3f4c8e28b14b1aa57db9213af4ff6df4
Lars Berger <lars.berger@yahoo.com>
2021-10-29 19:22:47 +0200

    feat(domain): avoid resizing siblings in `AttachContainerHandler`

--- GlazeWM.Domain/Containers/CommandHandlers/AttachContainerHandler.cs
+++ GlazeWM.Domain/Containers/CommandHandlers/AttachContainerHandler.cs
@@ -1,6 +1,4 @@
-﻿using System.Linq;
 using GlazeWM.Domain.Containers.Commands;
-using GlazeWM.Domain.Windows;
 using GlazeWM.Infrastructure.Bussing;
 
 namespace GlazeWM.Domain.Containers.CommandHandlers
@@ -30,9 +28,6 @@ namespace GlazeWM.Domain.Containers.CommandHandlers
       parent.Children.Insert(command.InsertPosition, childToAdd);
       childToAdd.Parent = parent;
 
-      if (childToAdd is TilingWindow || childToAdd is SplitContainer)
-        AdjustSiblingSizes(childToAdd);
-
       _containerService.ContainersToRedraw.Add(parent);
 
       // Adjust focus order of ancestors if the attached container is focused.
@@ -40,16 +35,6 @@ namespace GlazeWM.Domain.Containers.CommandHandlers
         _bus.Invoke(new SetFocusedDescendantCommand(childToAdd));
 
       return CommandResponse.Ok;
-    }
-
-    private void AdjustSiblingSizes(Container childToAdd)
-    {
-      var resizableSiblings = childToAdd.SelfAndSiblings.Where(container => container is IResizable);
-      double defaultPercent = 1.0 / resizableSiblings.Count();
-
-      // Adjust `SizePercentage` of the added container and its siblings.
-      foreach (var sibling in resizableSiblings)
-        (sibling as IResizable).SizePercentage = defaultPercent;
     }
   }
 }
