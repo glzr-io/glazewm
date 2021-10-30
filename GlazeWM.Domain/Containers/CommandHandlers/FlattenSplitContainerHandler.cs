7d042d48932f7d952d7c8a409737ad40d9799dec
Lars Berger <lars.berger@yahoo.com>
2021-10-30 16:44:11 +0200

    refactor(domain): explicitly detach child containers in `FlattenSplitContainerHandler`

--- GlazeWM.Domain/Containers/CommandHandlers/FlattenSplitContainerHandler.cs
+++ GlazeWM.Domain/Containers/CommandHandlers/FlattenSplitContainerHandler.cs
@@ -19,33 +19,33 @@ namespace GlazeWM.Domain.Containers.CommandHandlers
     public CommandResponse Handle(FlattenSplitContainerCommand command)
     {
       var containerToFlatten = command.ContainerToFlatten;
-      var parent = containerToFlatten.Parent;
-      var children = containerToFlatten.Children;
 
       // Keep references to properties of container to flatten prior to detaching.
+      var originalParent = containerToFlatten.Parent;
+      var originalChildren = containerToFlatten.Children.ToList();
       var originalFocusIndex = containerToFlatten.FocusIndex;
       var originalIndex = containerToFlatten.Index;
       var originalFocusOrder = containerToFlatten.ChildFocusOrder.ToList();
 
-      _bus.Invoke(new DetachContainerCommand(containerToFlatten));
-
-      // Insert children of detached split container at the original index.
-      foreach (var (child, index) in children.WithIndex())
+      foreach (var (child, index) in originalChildren.WithIndex())
       {
-        _bus.Invoke(new AttachContainerCommand(parent, child, originalIndex + index));
+        // Insert children of the split container at its original index in the parent. The split
+        // container will automatically detach once its last child is detached.
+        _bus.Invoke(new DetachContainerCommand(child));
+        _bus.Invoke(new AttachContainerCommand(originalParent, child, originalIndex + index));
 
         (child as IResizable).SizePercentage = (containerToFlatten as IResizable).SizePercentage
           * (child as IResizable).SizePercentage;
       }
 
       // Correct focus order of the inserted containers.
-      foreach (var child in children)
+      foreach (var child in originalChildren)
       {
         var childFocusIndex = originalFocusOrder.IndexOf(child);
-        parent.ChildFocusOrder.ShiftToIndex(originalFocusIndex + childFocusIndex, child);
+        originalParent.ChildFocusOrder.ShiftToIndex(originalFocusIndex + childFocusIndex, child);
       }
 
-      _containerService.ContainersToRedraw.Add(parent);
+      _containerService.ContainersToRedraw.Add(originalParent);
 
       return CommandResponse.Ok;
     }
