84b7a0cf98773928c22f8c6b91afd2a1bc9a2337
Lars Berger <lars.berger@yahoo.com>
2021-10-30 16:45:38 +0200

    feat(domain): throw error when attempting to attach an already attached container

--- GlazeWM.Domain/Containers/CommandHandlers/AttachContainerHandler.cs
+++ GlazeWM.Domain/Containers/CommandHandlers/AttachContainerHandler.cs
@@ -1,3 +1,4 @@
+﻿using System;
 using GlazeWM.Domain.Containers.Commands;
 using GlazeWM.Infrastructure.Bussing;
 
@@ -19,20 +20,15 @@ namespace GlazeWM.Domain.Containers.CommandHandlers
       var parent = command.Parent;
       var childToAdd = command.ChildToAdd;
 
-      // Get whether the container has focus prior to making any state changes.
-      var isFocusedContainer = _containerService.FocusedContainer == childToAdd;
-
       if (childToAdd.Parent != null)
-        _bus.Invoke(new DetachContainerCommand(childToAdd));
+        throw new Exception("Attempting to attach an already attached container. This is a bug.");
 
+      // TODO: Insert at end of parent's `ChildFocusOrder`.
       parent.Children.Insert(command.InsertPosition, childToAdd);
       childToAdd.Parent = parent;
 
       _containerService.ContainersToRedraw.Add(parent);
 
-      // Adjust focus order of ancestors if the attached container is focused.
-      if (isFocusedContainer)
-        _bus.Invoke(new SetFocusedDescendantCommand(childToAdd));
 
       return CommandResponse.Ok;
     }
