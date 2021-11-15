f64ae71d6b2bfb097b040b4f2f4d0d8575d0cb18
Lars Berger <lars.berger@yahoo.com>
2021-11-15 03:25:06 +0100

    feat(domain): add `MinimizedWindow` property for its previous state

--- GlazeWM.Domain/Windows/MinimizedWindow.cs
+++ GlazeWM.Domain/Windows/MinimizedWindow.cs
@@ -5,11 +5,15 @@ namespace GlazeWM.Domain.Windows
 {
   public sealed class MinimizedWindow : Window
   {
+    public WindowType PreviousState;
+
     public MinimizedWindow(
       IntPtr hwnd,
-      WindowRect floatingPlacement
+      WindowRect floatingPlacement,
+      WindowType previousState
     ) : base(hwnd, floatingPlacement)
     {
+      PreviousState = previousState;
     }
   }
 }
