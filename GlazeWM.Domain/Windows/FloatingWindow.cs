815e90787b6eec0789fa454e4b1926b89d796b5f
Lars Berger <lars.berger@yahoo.com>
2021-11-15 03:26:12 +0100

    feat(domain): get width/height/x/y of floating windows based on `FloatingPlacement`

--- GlazeWM.Domain/Windows/FloatingWindow.cs
+++ GlazeWM.Domain/Windows/FloatingWindow.cs
@@ -5,6 +5,14 @@ namespace GlazeWM.Domain.Windows
 {
   public sealed class FloatingWindow : Window
   {
+    public override int Width => FloatingPlacement.Right - FloatingPlacement.Left;
+
+    public override int Height => FloatingPlacement.Bottom - FloatingPlacement.Top;
+
+    public override int X => FloatingPlacement.Left;
+
+    public override int Y => FloatingPlacement.Top;
+
     public FloatingWindow(IntPtr hwnd, WindowRect floatingPlacement) : base(hwnd, floatingPlacement)
     {
     }
