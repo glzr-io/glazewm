7cd7f2da6aebff1f045ed2a4b4f48863a9d71a7a
Lars Berger <lars.berger@yahoo.com>
2021-11-22 02:36:08 +0100

    feat(domain): add `ShowAllWindowsCommand` and its handler

---
+++ GlazeWM.Domain/Windows/CommandHandlers/ShowAllWindowsHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using GlazeWM.Domain.Windows.Commands;
+using GlazeWM.Infrastructure.Bussing;
+using static GlazeWM.Infrastructure.WindowsApi.WindowsApiService;
+
+namespace GlazeWM.Domain.Windows.CommandHandlers
+{
+  class ShowAllWindowsHandler : ICommandHandler<ShowAllWindowsCommand>
+  {
+    private WindowService _windowService;
+
+    public ShowAllWindowsHandler(WindowService windowService)
+    {
+      _windowService = windowService;
+    }
+
+    public CommandResponse Handle(ShowAllWindowsCommand command)
+    {
+      // Reset all managed windows to their floating positions.
+      foreach (var window in _windowService.GetWindows())
+      {
+        var flags = SWP.SWP_FRAMECHANGED | SWP.SWP_NOACTIVATE | SWP.SWP_NOCOPYBITS |
+          SWP.SWP_NOZORDER | SWP.SWP_NOOWNERZORDER | SWP.SWP_NOSENDCHANGING | SWP.SWP_SHOWWINDOW;
+
+        SetWindowPos(
+          window.Hwnd,
+          IntPtr.Zero,
+          window.FloatingPlacement.Left,
+          window.FloatingPlacement.Top,
+          window.FloatingPlacement.Right - window.FloatingPlacement.Left,
+          window.FloatingPlacement.Bottom - window.FloatingPlacement.Top,
+          flags
+        );
+      }
+
+      return CommandResponse.Ok;
+    }
+  }
+}
