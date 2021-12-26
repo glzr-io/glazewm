7521816ff69101cb82499fdee94798e522f73d99
Lars Berger <lars.berger@yahoo.com>
2021-12-26 17:14:56 +0000

    feat(infra): add `WindowRect` method for translating to center of another `WindowRect`

--- GlazeWM.Infrastructure/WindowsApi/WindowRect.cs
+++ GlazeWM.Infrastructure/WindowsApi/WindowRect.cs
@@ -21,5 +21,26 @@
     /// The y-coordinate of the lower-right corner of the rectangle.
     /// </summary>
     public int Bottom { get; set; }
+
+    public int X => Left;
+
+    public int Y => Top;
+
+    public int Width => Right - Left;
+
+    public int Height => Bottom - Top;
+
+    public static WindowRect TranslateToCenter(WindowRect rect, WindowRect outerRect)
+    {
+      var translatedRect = new WindowRect();
+
+      translatedRect.Left = outerRect.X + (outerRect.Width / 2) - (rect.Width / 2);
+      translatedRect.Right = translatedRect.Left + rect.Width;
+
+      translatedRect.Top = outerRect.Y + (outerRect.Height / 2) - (rect.Height / 2);
+      translatedRect.Bottom = translatedRect.Top + rect.Height;
+
+      return translatedRect;
+    }
   }
 }
