01674e78da1b454d1eb100de9ea8cb2fd1e314ee
Lars Berger <lars.berger@yahoo.com>
2022-01-03 23:17:54 +0000

    feat(infra): create `Event` class for when the working area of a monitor is changed

---
+++ GlazeWM.Domain/Monitors/Events/WorkingAreaResizedEvent.cs
@@ -0,0 +1,14 @@
+﻿using GlazeWM.Infrastructure.Bussing;
+
+namespace GlazeWM.Domain.Monitors.Events
+{
+  public class WorkingAreaResizedEvent : Event
+  {
+    public Monitor AffectedMonitor { get; }
+
+    public WorkingAreaResizedEvent(Monitor affectedMonitor)
+    {
+      AffectedMonitor = affectedMonitor;
+    }
+  }
+}
