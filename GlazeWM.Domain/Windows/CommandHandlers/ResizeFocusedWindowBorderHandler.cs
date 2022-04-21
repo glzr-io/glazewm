ed83c7d5c45faf5b86584e7b21d9cd24884d7cf2
Lars Berger <lars.berger@yahoo.com>
2022-04-21 14:54:36 +0200

    feat(domain): create `ResizeFocusedWindowBordersCommand` and its handler

---
+++ GlazeWM.Domain/Windows/CommandHandlers/ResizeFocusedWindowBorderHandler.cs
@@ -0,0 +1,37 @@
+﻿using GlazeWM.Domain.Containers;
+using GlazeWM.Domain.Containers.Commands;
+using GlazeWM.Domain.Windows.Commands;
+using GlazeWM.Infrastructure.Bussing;
+
+namespace GlazeWM.Domain.Windows.CommandHandlers
+{
+  class ResizeFocusedWindowBorderHandler : ICommandHandler<ResizeFocusedWindowBordersCommand>
+  {
+    private readonly Bus _bus;
+    private readonly ContainerService _containerService;
+
+    public ResizeFocusedWindowBorderHandler(Bus bus, ContainerService containerService)
+    {
+      _bus = bus;
+      _containerService = containerService;
+    }
+
+    public CommandResponse Handle(ResizeFocusedWindowBordersCommand command)
+    {
+      var focusedWindow = _containerService.FocusedContainer as Window;
+
+      // Ignore cases where focused container is not a window.
+      if (focusedWindow == null)
+        return CommandResponse.Ok;
+
+      // Only redraw the window if it's tiling.
+      if (focusedWindow is TilingWindow)
+      {
+        _containerService.ContainersToRedraw.Add(focusedWindow);
+        _bus.Invoke(new RedrawContainersCommand());
+      }
+
+      return CommandResponse.Ok;
+    }
+  }
+}
---
+++ GlazeWM.Domain/Windows/Commands/ResizeFocusedWindowBordersCommand.cs
@@ -0,0 +1,15 @@
+﻿using GlazeWM.Infrastructure.Bussing;
+using System;
+
+namespace GlazeWM.Domain.Windows.Commands
+{
+  public class ResizeFocusedWindowBordersCommand : Command
+  {
+    public IntPtr WindowHandle { get; }
+
+    public ResizeFocusedWindowBordersCommand(IntPtr windowHandle)
+    {
+      WindowHandle = windowHandle;
+    }
+  }
+}
