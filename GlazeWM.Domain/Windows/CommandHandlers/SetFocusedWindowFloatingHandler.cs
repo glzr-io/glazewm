f78d8ed53a148da0b3d1d20b67a1741e38442c8d
Lars Berger <lars.berger@yahoo.com>
2022-05-04 10:03:53 +0200

    feat(domain): create `SetFocusedWindowFloatingCommand` and its handler

---
+++ GlazeWM.Domain/Windows/CommandHandlers/SetFocusedWindowFloatingHandler.cs
@@ -0,0 +1,31 @@
+﻿using GlazeWM.Domain.Containers;
+using GlazeWM.Domain.Windows.Commands;
+using GlazeWM.Infrastructure.Bussing;
+
+namespace GlazeWM.Domain.Windows.CommandHandlers
+{
+  class SetFocusedWindowFloatingHandler : ICommandHandler<SetFocusedWindowFloatingCommand>
+  {
+    private Bus _bus;
+    private ContainerService _containerService;
+
+    public SetFocusedWindowFloatingHandler(Bus bus, ContainerService containerService)
+    {
+      _bus = bus;
+      _containerService = containerService;
+    }
+
+    public CommandResponse Handle(SetFocusedWindowFloatingCommand command)
+    {
+      var focusedWindow = _containerService.FocusedContainer as Window;
+
+      // Ignore cases where focused container is not a window or not in foreground.
+      if (focusedWindow == null || !_containerService.IsFocusSynced)
+        return CommandResponse.Ok;
+
+      _bus.Invoke(new SetFloatingCommand(focusedWindow));
+
+      return CommandResponse.Ok;
+    }
+  }
+}
