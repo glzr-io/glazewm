d07fbaffd83f70e1e85120857045890ef2139455
Lars Berger <lars.berger@yahoo.com>
2022-05-04 10:03:28 +0200

    feat(domain): create separate `SetFloatingCommand` and handler for floating a window

---
+++ GlazeWM.Domain/Windows/Commands/SetFloatingCommand.cs
@@ -0,0 +1,14 @@
+﻿using GlazeWM.Infrastructure.Bussing;
+
+namespace GlazeWM.Domain.Windows.Commands
+{
+  public class SetFloatingCommand : Command
+  {
+    public Window Window { get; }
+
+    public SetFloatingCommand(Window window)
+    {
+      Window = window;
+    }
+  }
+}
