a167b8c79ee001e7bf3db3a5ca067f11a43cef54
Lars Berger <lars.berger@yahoo.com>
2022-07-31 16:28:12 +0800

    feat(domain): snap window to original position if not being resized

--- GlazeWM.Domain/Windows/EventHandlers/WindowMovedOrResizedHandler.cs
+++ GlazeWM.Domain/Windows/EventHandlers/WindowMovedOrResizedHandler.cs
@@ -1,6 +1,7 @@
 using System.Linq;
 using GlazeWM.Domain.Common.Enums;
 using GlazeWM.Domain.Common.Utils;
+using GlazeWM.Domain.Containers;
 using GlazeWM.Domain.Containers.Commands;
 using GlazeWM.Domain.Containers.Events;
 using GlazeWM.Domain.Monitors;
@@ -18,19 +19,22 @@ namespace GlazeWM.Domain.Windows.EventHandlers
     private readonly Bus _bus;
     private readonly WindowService _windowService;
     private readonly MonitorService _monitorService;
+    private readonly ContainerService _containerService;
     private readonly ILogger<WindowMovedOrResizedHandler> _logger;
 
     public WindowMovedOrResizedHandler(
       Bus bus,
       WindowService windowService,
       MonitorService monitorService,
-      ILogger<WindowMovedOrResizedHandler> logger
+      ILogger<WindowMovedOrResizedHandler> logger,
+      ContainerService containerService
     )
     {
       _bus = bus;
       _windowService = windowService;
       _monitorService = monitorService;
       _logger = logger;
+      _containerService = containerService;
     }
 
     public void Handle(WindowMovedOrResizedEvent @event)
@@ -55,9 +59,19 @@ namespace GlazeWM.Domain.Windows.EventHandlers
 
     private void UpdateTilingWindow(TilingWindow window)
     {
-      var currentPlacement = WindowService.GetPlacementOfHandle(window.Hwnd).NormalPosition;
+      // Snap window to its original position even if it's not being resized.
+      var hasNoResizableSiblings = window.Parent is Workspace
+        && !window.SiblingsOfType(typeof(IResizable)).Any();
+
+      if (hasNoResizableSiblings)
+      {
+        _containerService.ContainersToRedraw.Add(window);
+        _bus.Invoke(new RedrawContainersCommand());
+        return;
+      }
 
       // Remove invisible borders from current placement to be able to compare window width/height.
+      var currentPlacement = WindowService.GetPlacementOfHandle(window.Hwnd).NormalPosition;
       var adjustedPlacement = new WindowRect
       {
         Left = currentPlacement.Left + window.BorderDelta.DeltaLeft,
@@ -70,7 +84,6 @@ namespace GlazeWM.Domain.Windows.EventHandlers
       var deltaHeight = adjustedPlacement.Height - window.Height;
 
       // TODO: Avoid unnecessary resize call if either delta is 0.
-      // TODO: Window does not snap back to original position when there are no sibling windows.
       _bus.Invoke(new ResizeWindowCommand(window, ResizeDimension.WIDTH, $"{deltaWidth}px"));
       _bus.Invoke(new ResizeWindowCommand(window, ResizeDimension.HEIGHT, $"{deltaHeight}px"));
     }
