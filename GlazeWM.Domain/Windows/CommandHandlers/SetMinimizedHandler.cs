879a36d2f71d6417eeb2953be37787066bdfd36a
Lars Berger <lars.berger@yahoo.com>
2022-08-11 02:27:52 +0800

    feat(domain): create `SetMinimizedCommand` and its handler

---
+++ GlazeWM.Domain/Windows/CommandHandlers/SetMinimizedHandler.cs
@@ -0,0 +1,18 @@
+﻿using GlazeWM.Domain.Windows.Commands;
+using GlazeWM.Infrastructure.Bussing;
+using static GlazeWM.Infrastructure.WindowsApi.WindowsApiService;
+
+namespace GlazeWM.Domain.Windows.CommandHandlers
+{
+  internal class SetMinimizedHandler : ICommandHandler<SetMinimizedCommand>
+  {
+    public CommandResponse Handle(SetMinimizedCommand command)
+    {
+      var window = command.Window;
+
+      ShowWindow(window.Hwnd, ShowWindowCommands.MINIMIZE);
+
+      return CommandResponse.Ok;
+    }
+  }
+}
