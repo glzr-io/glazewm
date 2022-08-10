afbe2e4b989e37f971f0d9cc83beeb2004e61389
Lars Berger <lars.berger@yahoo.com>
2022-08-11 02:49:28 +0800

    feat(domain): create `ToggleMaximizedCommand` and its handler

---
+++ GlazeWM.Domain/Windows/CommandHandlers/ToggleMaximizedHandler.cs
@@ -0,0 +1,21 @@
+﻿using GlazeWM.Domain.Windows.Commands;
+using GlazeWM.Infrastructure.Bussing;
+using static GlazeWM.Infrastructure.WindowsApi.WindowsApiService;
+
+namespace GlazeWM.Domain.Windows.CommandHandlers
+{
+  internal class ToggleMaximizedHandler : ICommandHandler<ToggleMaximizedCommand>
+  {
+    public CommandResponse Handle(ToggleMaximizedCommand command)
+    {
+      var window = command.Window;
+
+      if (window.HasWindowStyle(WS.WS_MAXIMIZE))
+        ShowWindow(window.Hwnd, ShowWindowCommands.RESTORE);
+      else
+        ShowWindow(window.Hwnd, ShowWindowCommands.MAXIMIZE);
+
+      return CommandResponse.Ok;
+    }
+  }
+}
