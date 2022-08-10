34e289da7c86e59e79731f6e1731d927c684199b
Lars Berger <lars.berger@yahoo.com>
2022-08-11 02:41:59 +0800

    feat(domain): create `SetMaximizedCommand` and its handler

---
+++ GlazeWM.Domain/Windows/Commands/SetMaximizedCommand.cs
@@ -0,0 +1,14 @@
+﻿using GlazeWM.Infrastructure.Bussing;
+
+namespace GlazeWM.Domain.Windows.Commands
+{
+  public class SetMaximizedCommand : Command
+  {
+    public Window Window { get; }
+
+    public SetMaximizedCommand(Window window)
+    {
+      Window = window;
+    }
+  }
+}
