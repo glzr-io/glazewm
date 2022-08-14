61a45d42606580364cfc41c1fe5e0547a13ed043
Lars Berger <lars.berger@yahoo.com>
2022-08-15 02:22:25 +0800

    feat(infra): replace deserialization using `NewtonSoft.Json` with `System.Text.Json`

--- GlazeWM.Infrastructure/Yaml/YamlDeserializationService.cs
+++ GlazeWM.Infrastructure/Yaml/YamlDeserializationService.cs
@@ -1,16 +1,14 @@
 using System.Collections.Generic;
 using System.IO;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using YamlDotNet.Serialization;
-using YamlDotNet.Serialization.NamingConventions;
 
 namespace GlazeWM.Infrastructure.Yaml
 {
   public class YamlDeserializationService
   {
     private readonly IDeserializer _yamlDeserializer = new DeserializerBuilder()
-      .WithNamingConvention(UnderscoredNamingConvention.Instance)
       .Build();
 
     private readonly ISerializer _jsonSerializer = new SerializerBuilder()
@@ -19,30 +17,37 @@ namespace GlazeWM.Infrastructure.Yaml
 
     /// <summary>
     /// The YAML deserializing library doesn't have support for polymorphic objects. Because of
-    /// this, the YAML is first converted into JSON and then deserialized via `Newtonsoft.Json`.
+    /// this, the YAML is first converted into JSON and then deserialized via `System.Text.Json`.
     /// </summary>
     public T Deserialize<T>(TextReader input, List<JsonConverter> converters)
     {
       var yamlObject = _yamlDeserializer.Deserialize(input);
       var jsonString = _jsonSerializer.Serialize(yamlObject);
 
-      var jsonDeserializerSettings = GetJsonDeserializerSettings(converters);
-      return JsonConvert.DeserializeObject<T>(jsonString, jsonDeserializerSettings);
+      var jsonDeserializerSettings = GetJsonDeserializerOptions(converters);
+      return JsonSerializer.Deserialize<T>(jsonString, jsonDeserializerSettings);
     }
 
-    private static JsonSerializerSettings GetJsonDeserializerSettings(
+    private static JsonSerializerOptions GetJsonDeserializerOptions(
       List<JsonConverter> converters
     )
     {
-      return new()
+      var jsonDeserializerOptions = new JsonSerializerOptions()
       {
-        MissingMemberHandling = MissingMemberHandling.Error,
-        ContractResolver = new DefaultContractResolver
-        {
-          NamingStrategy = new SnakeCaseNamingStrategy()
-        },
-        Converters = converters
+        // TODO: Use built-in snake case policy once support is added.
+        // Ref: https://github.com/dotnet/runtime/issues/782
+        PropertyNamingPolicy = new SnakeCaseNamingPolicy(),
+        NumberHandling = JsonNumberHandling.AllowReadingFromString,
+        IncludeFields = true,
       };
+
+      foreach (var converter in converters)
+        jsonDeserializerOptions.Converters.Add(converter);
+
+      // Enable strings to be mapped to a C# enum (eg. `BarPosition` enum).
+      jsonDeserializerOptions.Converters.Add(new JsonStringEnumConverter());
+
+      return jsonDeserializerOptions;
     }
   }
 }
