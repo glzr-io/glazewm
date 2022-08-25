8eefd517ec45a95e17d4565b3381b7d3d37ee3c9
Lars Berger <lars.berger@yahoo.com>
2022-08-26 02:18:05 +0800

    refactor(domain): remove unused default window rules

--- GlazeWM.Domain/UserConfigs/UserConfigService.cs
+++ GlazeWM.Domain/UserConfigs/UserConfigService.cs
@@ -18,73 +18,14 @@ namespace GlazeWM.Domain.UserConfigs
       "./.glaze-wm/config.yaml"
     );
 
-    /// <summary>
-    /// Path to the sample user config file.
-    /// </summary>
-    public string SampleUserConfigPath = Path.Combine(
-      Directory.GetCurrentDirectory(),
-      "./sample-config.yaml"
-    );
-
     public readonly List<WindowRuleConfig> DefaultWindowRules = GetDefaultWindowRules();
 
     private static List<WindowRuleConfig> GetDefaultWindowRules()
     {
       var windowRules = new List<WindowRuleConfig>();
 
-      var classNamesToIgnore = new List<string> {
-        // Tray on primary screen.
-        "Shell_TrayWnd",
-        // Trays on secondary screens.
-        "Shell_SecondaryTrayWnd",
-        // Task manager.
-        "TaskManagerWindow",
-        // Microsoft Text Framework service IME.
-        "MSCTFIME UI",
-        // Desktop window (holds wallpaper & desktop icons).
-        "SHELLDLL_DefView",
-        // Background for lock screen.
-        "LockScreenBackstopFrame",
-        // Windows 10 shell.
-        "Progman",
-        // Windows 7 open Start Menu.
-        "DV2ControlHost",
-        // Windows 8 charm bar.
-        "Shell_CharmWindow",
-      };
-
-      foreach (var className in classNamesToIgnore)
-      {
-        var windowRule = new WindowRuleConfig()
-        {
-          MatchClassName = className,
-          Command = "ignore",
-        };
-
-        windowRules.Add(windowRule);
-      }
-
-      var processNamesToIgnore = new List<string> {
-        "SearchUI",
-        "ShellExperienceHost",
-        "LockApp",
-        "PeopleExperienceHost",
-        "StartMenuExperienceHost",
-      };
-
-      foreach (var processName in processNamesToIgnore)
-      {
-        var windowRule = new WindowRuleConfig()
-        {
-          MatchProcessName = processName,
-          Command = "ignore",
-        };
-
-        windowRules.Add(windowRule);
-      }
-
       var classNamesToFloat = new List<string> {
-        // Dialog shown when moving and deleting files.
+        // Windows 10 dialog shown when moving and deleting files.
         "OperationStatusWindow",
       };
 
