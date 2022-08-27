dd3e235735dc7682b006526459c5491abec0993a
Lars Berger <lars.berger@yahoo.com>
2022-08-27 11:58:40 +0800

    feat(infra): create `JsonElement` extension methods for getting converted property name

---
+++ GlazeWM.Infrastructure/Serialization/JsonElementExtensions.cs
@@ -0,0 +1,51 @@
+using System.Text.Json;
+
+namespace GlazeWM.Infrastructure.Serialization
+{
+  public static class JsonElementExtensions
+  {
+    /// <summary>
+    /// Get JSON property where property name has been converted according to naming policy.
+    /// </summary>
+    private static JsonElement GetConvertedProperty(
+      this JsonElement element,
+      string propertyName,
+      JsonSerializerOptions options)
+    {
+      // Convert name according to given naming policy.
+      var convertedName =
+        options.PropertyNamingPolicy?.ConvertName(propertyName) ?? propertyName;
+
+      return element.GetProperty(convertedName);
+    }
+
+    public static string GetStringProperty(
+      this JsonElement element,
+      string propertyName,
+      JsonSerializerOptions options)
+    {
+      return element.GetConvertedProperty(propertyName, options).GetString();
+    }
+
+    public static int GetInt64Property(
+      this JsonElement element,
+      string propertyName,
+      JsonSerializerOptions options)
+    {
+      return element.GetConvertedProperty(propertyName, options).GetInt32();
+    }
+
+    public static double GetDoubleProperty(
+      this JsonElement element,
+      string propertyName,
+      JsonSerializerOptions options)
+    {
+      return element.GetConvertedProperty(propertyName, options).GetDouble();
+    }
+
+    private static string ConvertName(string propertyName, JsonSerializerOptions options)
+    {
+      return options.PropertyNamingPolicy?.ConvertName(propertyName) ?? propertyName;
+    }
+  }
+}
