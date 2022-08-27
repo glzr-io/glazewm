ebb8585b2d7be08d4f1d46f7b3469d9ab5e06eb4
Lars Berger <lars.berger@yahoo.com>
2022-08-27 21:23:27 +0800

    feat(domain): create `CreateRecoveryCacheCommand` and its handler

---
+++ GlazeWM.Domain/Common/CommandHandlers/CreateRecoveryCacheHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.Json.Serialization;
+using GlazeWM.Domain.Common.Commands;
+using GlazeWM.Domain.Containers;
+using GlazeWM.Infrastructure.Bussing;
+using GlazeWM.Infrastructure.Serialization;
+
+namespace GlazeWM.Domain.Common.CommandHandlers
+{
+  public class CreateRecoveryCacheHandler : ICommandHandler<CreateRecoveryCacheCommand>
+  {
+    private readonly JsonService _jsonService;
+    private readonly ContainerService _containerService;
+    private readonly RecoveryCacheService _recoveryCacheService;
+
+    public CreateRecoveryCacheHandler(
+      JsonService jsonService,
+      ContainerService containerService,
+      RecoveryCacheService recoveryCacheService)
+    {
+      _jsonService = jsonService;
+      _containerService = containerService;
+      _recoveryCacheService = recoveryCacheService;
+    }
+
+    public CommandResponse Handle(CreateRecoveryCacheCommand command)
+    {
+      var recoveryCacheJson = _jsonService.Serialize(
+        _containerService.ContainerTree,
+        new List<JsonConverter>() { new ContainerConverter() }
+      );
+
+      // Write JSON cache to disk.
+      File.WriteAllText(_recoveryCacheService.RecoveryCachePath, recoveryCacheJson);
+
+      return CommandResponse.Ok;
+    }
+  }
+}
---
+++ GlazeWM.Domain/Common/Commands/CreateRecoveryCacheCommand.cs
@@ -0,0 +1,8 @@
+using GlazeWM.Infrastructure.Bussing;
+
+namespace GlazeWM.Domain.Common.Commands
+{
+  public class CreateRecoveryCacheCommand : Command
+  {
+  }
+}
