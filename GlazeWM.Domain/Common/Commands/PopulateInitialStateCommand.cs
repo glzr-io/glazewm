7483586a9b091ef0241f4a0db6932a9600e76bdb
Lars Berger <lars.berger@yahoo.com>
2022-08-28 03:30:34 +0800

    feat(domain): create `PopulateInitialStateCommand` and its handler

---
+++ GlazeWM.Domain/Common/Commands/PopulateInitialStateCommand.cs
@@ -0,0 +1,11 @@
+using GlazeWM.Infrastructure.Bussing;
+
+namespace GlazeWM.Domain.Common.Commands
+{
+  /// <summary>
+  /// Populate initial monitors, windows, workspaces, and user config.
+  /// </summary>
+  public class PopulateInitialStateCommand : Command
+  {
+  }
+}
