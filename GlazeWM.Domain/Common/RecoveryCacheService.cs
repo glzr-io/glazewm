82e7ef8fa303c19dfc0d8fd7b7eb613d7a938b18
Lars Berger <lars.berger@yahoo.com>
2022-08-27 21:04:50 +0800

    feat(domain): create `RecoveryCacheService` for getting cached container tree

---
+++ GlazeWM.Domain/Common/RecoveryCacheService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json.Serialization;
+using GlazeWM.Domain.Containers;
+using GlazeWM.Infrastructure.Serialization;
+
+namespace GlazeWM.Domain.Common
+{
+  public class RecoveryCacheService
+  {
+    private readonly JsonService _jsonService;
+
+    /// <summary>
+    /// Path to the recovery cache file.
+    /// </summary>
+    public readonly string RecoveryCachePath = Path.Combine(
+      Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+      "./.glaze-wm/state.backup"
+    );
+
+    public RecoveryCacheService(JsonService jsonService)
+    {
+      _jsonService = jsonService;
+    }
+
+    /// <summary>
+    /// Read recovery cache from disk.
+    /// </summary>
+    public Container GetRecoveryCache()
+    {
+      var recoveryCache = File.ReadAllText(RecoveryCachePath);
+
+      return _jsonService.Deserialize<Container>(
+        recoveryCache,
+        new List<JsonConverter>() { new ContainerConverter() }
+      );
+    }
+  }
+}
