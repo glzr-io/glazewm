c83b3af07e5ffbf5942aad88e87673281686d695
Lars Berger <lars.berger@yahoo.com>
2022-09-22 19:21:30 +0800

    feat(domain): add `SetNativeFocusCommand` and its handler

---
+++ GlazeWM.Domain/Containers/CommandHandlers/SetNativeFocusHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using GlazeWM.Domain.Containers.Commands;
+using GlazeWM.Domain.Windows;
+using GlazeWM.Domain.Workspaces;
+using GlazeWM.Infrastructure.Bussing;
+using static GlazeWM.Infrastructure.WindowsApi.WindowsApiService;
+
+namespace GlazeWM.Domain.Containers.CommandHandlers
+{
+  internal class SetNativeFocusHandler : ICommandHandler<SetNativeFocusCommand>
+  {
+    private readonly Bus _bus;
+
+    public SetNativeFocusHandler(Bus bus)
+    {
+      _bus = bus;
+    }
+
+    public CommandResponse Handle(SetNativeFocusCommand command)
+    {
+      var containerToFocus = command.ContainerToFocus;
+
+      if (containerToFocus is Window)
+      {
+        // Set as foreground window if it's not already set. This will trigger `EVENT_SYSTEM_FOREGROUND`
+        // window event and its handler. This, in turn, calls `SetFocusedDescendant`.
+        KeybdEvent(0, 0, 0, 0);
+        SetForegroundWindow((containerToFocus as Window).Handle);
+      }
+      else if (containerToFocus is Workspace)
+      {
+        // Setting focus to the desktop window does not emit `EVENT_SYSTEM_FOREGROUND` window event, so
+        // `SetFocusedDescendant` has to be manually called.
+        KeybdEvent(0, 0, 0, 0);
+        SetForegroundWindow(GetDesktopWindow());
+        _bus.Invoke(new SetFocusedDescendantCommand(containerToFocus));
+      }
+      else
+        throw new Exception("Invalid container type to focus. This is a bug.");
+
+      return CommandResponse.Ok;
+    }
+  }
+}
---
+++ GlazeWM.Domain/Containers/Commands/SetNativeFocusCommand.cs
@@ -0,0 +1,14 @@
+﻿using GlazeWM.Infrastructure.Bussing;
+
+namespace GlazeWM.Domain.Containers.Commands
+{
+  public class SetNativeFocusCommand : Command
+  {
+    public Container ContainerToFocus { get; }
+
+    public SetNativeFocusCommand(Container containerToFocus)
+    {
+      ContainerToFocus = containerToFocus;
+    }
+  }
+}
