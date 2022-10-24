1088667d7424c13ce46246ab6939e7589d4de86f
Lars Berger <lars.berger@yahoo.com>
2022-10-24 14:15:01 +0800

    feat(infra): add new `HandleFatalExceptionCommand` and its handler

---
+++ GlazeWM.Infrastructure/Common/Commands/HandleFatalExceptionCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using GlazeWM.Infrastructure.Bussing;
+
+namespace GlazeWM.Infrastructure.Common.Commands
+{
+  public class HandleFatalExceptionCommand : Command
+  {
+    public Exception Exception { get; }
+
+    public HandleFatalExceptionCommand(Exception exception)
+    {
+      Exception = exception;
+    }
+  }
+}
