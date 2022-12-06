190181fafb8044653725b8ef3fd4df7c9482d278
Lars Berger <lars.berger@yahoo.com>
2022-12-06 16:10:43 +0800

    refactor(domain): use `SetTilingCommand` in `ToggleFloatingHandler`

--- GlazeWM.Domain/Windows/CommandHandlers/ToggleFloatingHandler.cs
+++ GlazeWM.Domain/Windows/CommandHandlers/ToggleFloatingHandler.cs
@@ -1,7 +1,4 @@
-﻿using GlazeWM.Domain.Containers;
-using GlazeWM.Domain.Containers.Commands;
-using GlazeWM.Domain.Windows.Commands;
-using GlazeWM.Domain.Workspaces;
+﻿using GlazeWM.Domain.Windows.Commands;
 using GlazeWM.Infrastructure.Bussing;
 
 namespace GlazeWM.Domain.Windows.CommandHandlers
@@ -20,49 +17,11 @@ namespace GlazeWM.Domain.Windows.CommandHandlers
       var window = command.Window;
 
       if (window is FloatingWindow)
-        UnsetFloating(window as FloatingWindow);
+        _bus.Invoke(new SetTilingCommand(window));
       else
         _bus.Invoke(new SetFloatingCommand(window));
 
       return CommandResponse.Ok;
-    }
-
-    private void UnsetFloating(FloatingWindow floatingWindow)
-    {
-      // Keep reference to the window's ancestor workspace prior to detaching.
-      var workspace = WorkspaceService.GetWorkspaceFromChildContainer(floatingWindow);
-
-      var insertionTarget = workspace.LastFocusedDescendantOfType<IResizable>();
-
-      var tilingWindow = new TilingWindow(
-        floatingWindow.Handle,
-        floatingWindow.FloatingPlacement,
-        floatingWindow.BorderDelta,
-        0
-      );
-
-      _bus.Invoke(
-        new ReplaceContainerCommand(
-          tilingWindow,
-          floatingWindow.Parent,
-          floatingWindow.Index
-        )
-      );
-
-      // Insert the created tiling window after the last focused descendant of the workspace.
-      if (insertionTarget == null)
-        _bus.Invoke(new MoveContainerWithinTreeCommand(tilingWindow, workspace, 0, true));
-      else
-        _bus.Invoke(
-          new MoveContainerWithinTreeCommand(
-            tilingWindow,
-            insertionTarget.Parent,
-            insertionTarget.Index + 1,
-            true
-          )
-        );
-
-      _bus.Invoke(new RedrawContainersCommand());
     }
   }
 }
