9bdce2b9be5e8a06d1109a925bdd71c315571a44
Lars Berger <lars.berger@yahoo.com>
2022-12-06 16:08:16 +0800

    feat(domain): create `SetTilingCommand` and its handler

---
+++ GlazeWM.Domain/Windows/CommandHandlers/SetTilingHandler.cs
@@ -0,0 +1,58 @@
+﻿using GlazeWM.Domain.Containers;
+using GlazeWM.Domain.Containers.Commands;
+using GlazeWM.Domain.Windows.Commands;
+using GlazeWM.Domain.Workspaces;
+using GlazeWM.Infrastructure.Bussing;
+
+namespace GlazeWM.Domain.Windows.CommandHandlers
+{
+  internal class SetTilingHandler : ICommandHandler<SetTilingCommand>
+  {
+    private readonly Bus _bus;
+
+    public SetTilingHandler(Bus bus)
+    {
+      _bus = bus;
+    }
+
+    public CommandResponse Handle(SetTilingCommand command)
+    {
+      var window = command.Window;
+
+      if (window is TilingWindow)
+        return CommandResponse.Ok;
+
+      // Keep reference to the window's ancestor workspace prior to detaching.
+      var workspace = WorkspaceService.GetWorkspaceFromChildContainer(window);
+
+      var insertionTarget = workspace.LastFocusedDescendantOfType<IResizable>();
+
+      var tilingWindow = new TilingWindow(
+        window.Handle,
+        window.FloatingPlacement,
+        window.BorderDelta,
+        0
+      );
+
+      // Replace the original window with the created tiling window.
+      _bus.Invoke(new ReplaceContainerCommand(tilingWindow, window.Parent, window.Index));
+
+      // Insert the created tiling window after the last focused descendant of the workspace.
+      if (insertionTarget is null)
+        _bus.Invoke(new MoveContainerWithinTreeCommand(tilingWindow, workspace, 0, true));
+      else
+        _bus.Invoke(
+          new MoveContainerWithinTreeCommand(
+            tilingWindow,
+            insertionTarget.Parent,
+            insertionTarget.Index + 1,
+            true
+          )
+        );
+
+      _bus.Invoke(new RedrawContainersCommand());
+
+      return CommandResponse.Ok;
+    }
+  }
+}
---
+++ GlazeWM.Domain/Windows/Commands/SetTilingCommand.cs
@@ -0,0 +1,14 @@
+﻿using GlazeWM.Infrastructure.Bussing;
+
+namespace GlazeWM.Domain.Windows.Commands
+{
+  public class SetTilingCommand : Command
+  {
+    public Window Window { get; }
+
+    public SetTilingCommand(Window window)
+    {
+      Window = window;
+    }
+  }
+}
