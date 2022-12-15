74615eec4f03a9f45a119bba16870d4b3327da5d
Lars Berger <lars.berger@yahoo.com>
2022-12-16 00:57:49 +0800

    feat(bar): display the active binding mode in component

--- GlazeWM.Bar/Components/BindingModeComponentViewModel.cs
+++ GlazeWM.Bar/Components/BindingModeComponentViewModel.cs
@@ -1,21 +1,33 @@
 using System;
-using System.Globalization;
 using System.Reactive.Linq;
+using System.Windows.Threading;
+using GlazeWM.Domain.Containers;
+using GlazeWM.Domain.Monitors.Events;
 using GlazeWM.Domain.UserConfigs;
+using GlazeWM.Infrastructure;
+using GlazeWM.Infrastructure.Bussing;
 
 namespace GlazeWM.Bar.Components
 {
   public class BindingModeComponentViewModel : ComponentViewModel
   {
+    private Dispatcher _dispatcher => _parentViewModel.Dispatcher;
+    private readonly Bus _bus = ServiceLocator.GetRequiredService<Bus>();
+    private readonly ContainerService _containerService =
+     ServiceLocator.GetRequiredService<ContainerService>();
+
+    /// <summary>
+    /// Name of the currently active binding mode (if one is active).
+    /// </summary>
+    public string ActiveBindingMode => _containerService.ActiveBindingMode;
+
     public BindingModeComponentViewModel(
       BarViewModel parentViewModel,
       BindingModeComponentConfig config) : base(parentViewModel, config)
     {
-      // Update the displayed time every second.
-      var updateInterval = TimeSpan.FromSeconds(1);
-
-      // Observable.Interval(updateInterval)
-      //   .Subscribe(_ => OnPropertyChanged(nameof("j")));
+      _bus.Events.OfType<BindingModeChangedEvent>().Subscribe(_ =>
+        _dispatcher.Invoke(() => OnPropertyChanged(nameof(ActiveBindingMode)))
+      );
     }
   }
 }
