e032af4e85a49e681b82fd872422655c398bc880
Lars Berger <lars.berger@yahoo.com>
2022-12-16 17:05:38 +0800

    feat(domain,bar): add tiling direction bar component

---
+++ GlazeWM.Bar/Components/TilingDirectionComponentViewModel.cs
@@ -0,0 +1,13 @@
+using GlazeWM.Domain.UserConfigs;
+
+namespace GlazeWM.Bar.Components
+{
+  public class TilingDirectionComponentViewModel : ComponentViewModel
+  {
+    public TilingDirectionComponentViewModel(
+      BarViewModel parentViewModel,
+      TilingDirectionComponentConfig config) : base(parentViewModel, config)
+    {
+    }
+  }
+}
