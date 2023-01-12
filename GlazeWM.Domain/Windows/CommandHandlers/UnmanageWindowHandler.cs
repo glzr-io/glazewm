cfafb9a88ab829d358969a6d38e055740581709b
Lars Berger <lars.berger@yahoo.com>
2023-01-12 13:04:34 +0800

    feat(domain): reset focus to `PendingFocusContainer` on next focus event

--- GlazeWM.Domain/Windows/CommandHandlers/UnmanageWindowHandler.cs
+++ GlazeWM.Domain/Windows/CommandHandlers/UnmanageWindowHandler.cs
@@ -2,16 +2,19 @@ using GlazeWM.Domain.Containers;
 using GlazeWM.Domain.Containers.Commands;
 using GlazeWM.Domain.Windows.Commands;
 using GlazeWM.Infrastructure.Bussing;
+using static GlazeWM.Infrastructure.WindowsApi.WindowsApiService;
 
 namespace GlazeWM.Domain.Windows.CommandHandlers
 {
   internal sealed class UnmanageWindowHandler : ICommandHandler<UnmanageWindowCommand>
   {
     private readonly Bus _bus;
+    private readonly ContainerService _containerService;
 
-    public UnmanageWindowHandler(Bus bus)
+    public UnmanageWindowHandler(Bus bus, ContainerService containerService)
     {
       _bus = bus;
+      _containerService = containerService;
     }
 
     public CommandResponse Handle(UnmanageWindowCommand command)
@@ -26,13 +29,29 @@ namespace GlazeWM.Domain.Windows.CommandHandlers
       else
         _bus.Invoke(new DetachContainerCommand(window));
 
-      // The OS automatically switches focus to a different window after closing. Use `InvokeAsync`
-      // to ensure focus gets set to `containerToFocus` *after* the OS sets focus. This will cause
-      // focus to briefly flicker to the OS focus target and then to the WM's focus target.
-      // TODO: Consider moving this out to `WindowHiddenHandler` and `WindowClosedHandler` after
-      // redraw. More likely that it runs after OS focus event.
-      _bus.InvokeAsync(new SetNativeFocusCommand(focusTarget));
+      var hasFocusableWindows = WindowService.GetAllWindowHandles().Exists(handle =>
+      {
+        // TODO: This needs to be changed to match the criteria for the OS to focus a
+        // window on close.
+        var isVisible = WindowService.IsHandleVisible(handle);
+        var canFocus = WindowService.HandleHasWindowExStyle(
+          handle,
+          WS_EX.WS_EX_NOACTIVATE
+        );
+        return isVisible && canFocus;
+      });
 
+      // The OS automatically switches focus to a different window after closing. If
+      // there are focusable windows, then set focus *after* the OS sets focus. This will
+      // cause focus to briefly flicker to the OS focus target and then to the WM's focus
+      // target.
+      if (hasFocusableWindows)
+      {
+        _containerService.PendingFocusContainer = focusTarget;
+        return CommandResponse.Ok;
+      }
+
+      _bus.Invoke(new SetNativeFocusCommand(focusTarget));
       return CommandResponse.Ok;
     }
   }
--- GlazeWM.Domain/Windows/EventHandlers/WindowFocusedHandler.cs
+++ GlazeWM.Domain/Windows/EventHandlers/WindowFocusedHandler.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using GlazeWM.Domain.Common.Utils;
+using GlazeWM.Domain.Containers;
 using GlazeWM.Domain.Containers.Commands;
 using GlazeWM.Domain.Containers.Events;
 using GlazeWM.Infrastructure.Bussing;
@@ -11,21 +12,35 @@ namespace GlazeWM.Domain.Windows.EventHandlers
   internal sealed class WindowFocusedHandler : IEventHandler<WindowFocusedEvent>
   {
     private readonly Bus _bus;
-    private readonly WindowService _windowService;
+    private readonly ContainerService _containerService;
     private readonly ILogger<WindowFocusedHandler> _logger;
+    private readonly WindowService _windowService;
 
     public WindowFocusedHandler(
       Bus bus,
-      WindowService windowService,
-      ILogger<WindowFocusedHandler> logger)
+      ContainerService containerService,
+      ILogger<WindowFocusedHandler> logger,
+      WindowService windowService)
     {
       _bus = bus;
-      _windowService = windowService;
+      _containerService = containerService;
       _logger = logger;
+      _windowService = windowService;
     }
 
     public void Handle(WindowFocusedEvent @event)
     {
+      var pendingFocusContainer = _containerService.PendingFocusContainer;
+
+      // Override the container to set focus to (ie. when changing focus after a window is
+      // closed or hidden).
+      if (pendingFocusContainer is not null)
+      {
+        _bus.Invoke(new SetNativeFocusCommand(pendingFocusContainer));
+        _containerService.PendingFocusContainer = null;
+        return;
+      }
+
       var window = _windowService.GetWindows()
         .FirstOrDefault(window => window.Handle == @event.WindowHandle);
 
