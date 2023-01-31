7a00ce349b36b938c80e92fa686fad306c6fcd6e
pamesta <pamestapinigine@gmail.com>
2023-02-01 01:29:24 +0200

    restore floatingWindow floatingPlacement to read only

--- GlazeWM.Domain/Windows/FloatingWindow.cs
+++ GlazeWM.Domain/Windows/FloatingWindow.cs
@@ -5,53 +5,13 @@ namespace GlazeWM.Domain.Windows
 {
   public sealed class FloatingWindow : Window
   {
-    public override int Width
-    {
-      get
-      {
-        return FloatingPlacement.Width;
-      }
-      set
-      {
-        FloatingPlacement = Rect.FromXYCoordinates(X, Y, value, Height);
-      }
-    }
+    public override int Width => FloatingPlacement.Width;
 
-    public override int Height
-    {
-      get
-      {
-        return FloatingPlacement.Height;
-      }
-      set
-      {
-        FloatingPlacement = Rect.FromXYCoordinates(X, Y, Width, value);
-      }
-    }
+    public override int Height => FloatingPlacement.Height;
 
-    public override int X
-    {
-      get
-      {
-        return FloatingPlacement.X;
-      }
-      set
-      {
-        FloatingPlacement = Rect.FromXYCoordinates(value, Y, Width, Height);
-      }
-    }
+    public override int X => FloatingPlacement.X;
 
-    public override int Y
-    {
-      get
-      {
-        return FloatingPlacement.Y;
-      }
-      set
-      {
-        FloatingPlacement = Rect.FromXYCoordinates(X, value, Width, Height);
-      }
-    }
+    public override int Y => FloatingPlacement.Y;
 
     public FloatingWindow(
       IntPtr handle,
