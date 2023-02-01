886b06a39910d92e13b2ba1def454499fd0f3dad
blam <notblam@gmail.com>
2023-02-01 09:42:48 -0500

    cleanup

--- GlazeWM.Bar/Components/SystemTrayComponentViewModel.cs
+++ GlazeWM.Bar/Components/SystemTrayComponentViewModel.cs
@@ -1,23 +1,13 @@
 using System.Diagnostics;
-using GlazeWM.Domain.Containers;
 using GlazeWM.Domain.UserConfigs;
-using GlazeWM.Infrastructure;
-using GlazeWM.Infrastructure.Bussing;
-using ManagedShell;
 
 namespace GlazeWM.Bar.Components
 {
   public class SystemTrayComponentViewModel : ComponentViewModel
   {
-    // private SystemTrayComponentConfig _config => _componentConfig as SystemTrayComponentConfig;
-
-    // public string Text => _config.Text;
-
     public SystemTrayComponentViewModel(
       BarViewModel parentViewModel,
       SystemTrayComponentConfig config) : base(parentViewModel, config)
-    {
-      Debug.WriteLine("--");
-    }
+    { }
   }
 }
