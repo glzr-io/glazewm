d906321df84f67c8634f285946523d841993bd8e
blam <notblam@gmail.com>
2023-02-01 12:56:57 -0500

    cleanup

--- GlazeWM.Bar/Components/NotifyIcon.xaml.cs
+++ GlazeWM.Bar/Components/NotifyIcon.xaml.cs
@@ -1,12 +1,8 @@
-using System;
-using System.Diagnostics;
-using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using ManagedShell.Common.Helpers;
 using ManagedShell.Interop;
-using ManagedShell.WindowsTray;
 
 namespace GlazeWM.Bar.Components
 {
