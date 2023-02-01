d7b80e7600db928d58fccd833df1ac2dfd4f07f1
blam <notblam@gmail.com>
2023-02-01 09:34:40 -0500

    cleanup

--- GlazeWM.Domain/UserConfigs/SystemTrayComponentConfig.cs
+++ GlazeWM.Domain/UserConfigs/SystemTrayComponentConfig.cs
@@ -3,12 +3,12 @@ namespace GlazeWM.Domain.UserConfigs
   public class SystemTrayComponentConfig : BarComponentConfig
   {
     /// <summary>
-    /// Text to display.
+    /// Expands to show both pinned and unppined icons.
     /// </summary>
     public string ExpandText { get; set; } = "";
 
     /// <summary>
-    /// 
+    /// Collapse to show only pinned icons.
     /// </summary>
     public string CollapseText { get; set; } = "";
   }
