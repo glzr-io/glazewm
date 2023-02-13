7ce98b13d6c21cd5854be824cee62b2dce3b0659
Lars Berger <lars.berger@yahoo.com>
2023-02-13 16:59:25 +0800

    refactor(bar): simplify creation of `NotifyIconViewModel` collections in `SystemTrayComponentViewModel`

--- GlazeWM.Bar/Components/SystemTrayComponentViewModel.cs
+++ GlazeWM.Bar/Components/SystemTrayComponentViewModel.cs
@@ -1,83 +1,54 @@
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Linq;
 using System.Windows.Input;
 using GlazeWM.Bar.Common;
 using GlazeWM.Domain.UserConfigs;
 using ManagedShell;
-using ManagedShell.WindowsTray;
 
 namespace GlazeWM.Bar.Components
 {
   public class SystemTrayComponentViewModel : ComponentViewModel
   {
-    private readonly SystemTrayComponentConfig _systemTrayComponentConfig;
-    public string _ExpandCollapseText = string.Empty;
-    public string ExpandCollapseText
-    {
-      get => _ExpandCollapseText;
-      set
-      {
-        _ExpandCollapseText = value;
-        OnPropertyChanged(nameof(ExpandCollapseText));
-      }
-    }
-    public ICommand ToggleShowAllIconsCommand => new RelayCommand(ToggleShowAllIcons);
-    public ObservableCollection<NotifyIconViewModel> PinnedTrayIcons { get; set; } = new();
-    public ObservableCollection<NotifyIconViewModel> UnpinnedTrayIcons { get; set; } = new();
-    public ICollectionView _pinnedNotifyIcons;
-    public ICollectionView PinnedNotifyIcons
-    {
-      get => _pinnedNotifyIcons;
-      set
-      {
-        _pinnedNotifyIcons = value;
-        OnPropertyChanged(nameof(PinnedNotifyIcons));
-        PinnedTrayIcons = new ObservableCollection<NotifyIconViewModel>(_notificationArea.TrayIcons.Where(e => e.IsPinned).Select(e => new NotifyIconViewModel(e)).ToList());
-        OnPropertyChanged(nameof(PinnedTrayIcons));
-      }
-    }
-    public ICollectionView _unpinnedNotifyIcons;
-    public ICollectionView UnpinnedNotifyIcons
-    {
-      get => _unpinnedNotifyIcons;
-      set
-      {
-        _unpinnedNotifyIcons = value;
-        OnPropertyChanged(nameof(UnpinnedNotifyIcons));
-        UnpinnedTrayIcons = new ObservableCollection<NotifyIconViewModel>(_notificationArea.TrayIcons.Where(e => !e.IsPinned).Select(e => new NotifyIconViewModel(e)).ToList());
-        OnPropertyChanged(nameof(UnpinnedTrayIcons));
-      }
-    }
-    private bool _isExpanded { get; set; } = true;
-    private static NotificationArea _notificationArea { get; set; }
+    private readonly SystemTrayComponentConfig _config;
+
     private static ShellManager _shellManager { get; set; }
+
+    public ICommand ToggleShowAllIconsCommand => new RelayCommand(ToggleShowAllIcons);
+
+    private bool IsExpanded { get; set; } = true;
+
+    public string ExpandCollapseText => IsExpanded
+      ? _config.LabelExpandText
+      : _config.LabelCollapseText;
+
+    public static ObservableCollection<NotifyIconViewModel> PinnedTrayIcons =>
+      new(_pinnedTrayIcons);
+    public static ObservableCollection<NotifyIconViewModel> UnpinnedTrayIcons =>
+      new(_unpinnedTrayIcons);
+
+    private static IEnumerable<NotifyIconViewModel> _pinnedTrayIcons =>
+      _shellManager.NotificationArea.TrayIcons
+        .Where(trayIcon => trayIcon.IsPinned)
+        .Select(trayIcon => new NotifyIconViewModel(trayIcon));
+
+    private static IEnumerable<NotifyIconViewModel> _unpinnedTrayIcons =>
+      _shellManager.NotificationArea.TrayIcons
+        .Where(trayIcon => !trayIcon.IsPinned)
+        .Select(trayIcon => new NotifyIconViewModel(trayIcon));
 
     public SystemTrayComponentViewModel(
       BarViewModel parentViewModel,
       SystemTrayComponentConfig config) : base(parentViewModel, config)
     {
-      _systemTrayComponentConfig = config;
+      _config = config;
       _shellManager ??= new ShellManager();
-      _notificationArea = _shellManager.NotificationArea;
-      UnpinnedNotifyIcons = _notificationArea.UnpinnedIcons;
-      PinnedNotifyIcons = _notificationArea.PinnedIcons;
-      ExpandCollapseText = config.LabelCollapseText;
     }
 
     public void ToggleShowAllIcons()
     {
-      if (_isExpanded)
-      {
-        UnpinnedNotifyIcons = null;
-        ExpandCollapseText = _systemTrayComponentConfig.LabelExpandText;
-      }
-      else
-      {
-        UnpinnedNotifyIcons = _notificationArea.UnpinnedIcons;
-        ExpandCollapseText = _systemTrayComponentConfig.LabelCollapseText;
-      }
-      _isExpanded = !_isExpanded;
+      IsExpanded = !IsExpanded;
+      OnPropertyChanged(nameof(ExpandCollapseText));
     }
   }
 }
