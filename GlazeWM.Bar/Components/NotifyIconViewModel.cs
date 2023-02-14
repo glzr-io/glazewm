5d77502170f82994b5ec8b8e7f17fe245211f7ad
Lars Berger <lars.berger@yahoo.com>
2023-02-14 19:09:27 +0800

    feat(bar): successfully binding to commands defined in `NotifyIconViewModel`

--- GlazeWM.Bar/Components/NotifyIconViewModel.cs
+++ GlazeWM.Bar/Components/NotifyIconViewModel.cs
@@ -11,18 +11,20 @@ namespace GlazeWM.Bar.Components
   {
     public ManagedShell.WindowsTray.NotifyIcon TrayIcon { get; set; }
 
-    public ICommand OnMouseUpCommand => new RelayCommand<object>(OnMouseUp);
-    public ICommand OnMouseDownCommand => new RelayCommand<object>(OnMouseDown);
-    public ICommand OnMouseEnterCommand => new RelayCommand<object>(OnMouseEnter);
-    public ICommand OnMouseLeaveCommand => new RelayCommand<object>(OnMouseLeave);
-    public ICommand OnMouseMoveCommand => new RelayCommand<object>(OnMouseMove);
+    public ICommand OnMouseUpCommand =>
+      new RelayCommand<MouseButtonEventArgs>(OnMouseUp);
+    public ICommand OnMouseDownCommand =>
+      new RelayCommand<MouseButtonEventArgs>(OnMouseDown);
+    public ICommand OnMouseEnterCommand => new RelayCommand<MouseEventArgs>(OnMouseEnter);
+    public ICommand OnMouseLeaveCommand => new RelayCommand<MouseEventArgs>(OnMouseLeave);
+    public ICommand OnMouseMoveCommand => new RelayCommand<MouseEventArgs>(OnMouseMove);
 
     public NotifyIconViewModel(ManagedShell.WindowsTray.NotifyIcon trayIcon)
     {
       TrayIcon = trayIcon;
     }
 
-    private void OnMouseDown(object sender, MouseButtonEventArgs e)
+    private void OnMouseDown(MouseButtonEventArgs e)
     {
       e.Handled = true;
 
@@ -34,16 +36,19 @@ namespace GlazeWM.Bar.Components
         );
     }
 
-    private void OnMouseUp(object sender, MouseButtonEventArgs e)
+    private void OnMouseUp(MouseButtonEventArgs e)
     {
       e.Handled = true;
 
       if (Keyboard.IsKeyDown(Key.LeftAlt))
       {
         if (TrayIcon.IsPinned)
+        {
           TrayIcon.Unpin();
-        else
-          TrayIcon.Pin();
+          return;
+        }
+
+        TrayIcon.Pin();
         return;
       }
 
@@ -54,30 +59,37 @@ namespace GlazeWM.Bar.Components
       );
     }
 
-    private void OnMouseEnter(object sender, MouseEventArgs e)
+    private void OnMouseEnter(MouseEventArgs e)
     {
       e.Handled = true;
 
       // Update icon position for `Shell_NotifyIconGetRect`.
-      var decorator = sender as Decorator;
-      var location = decorator.PointToScreen(new Point(0, 0));
+      var eventSource = e.Source as Image;
+      var location = eventSource.PointToScreen(new Point(0, 0));
       var dpiScale = PresentationSource
-        .FromVisual(decorator)
+        .FromVisual(eventSource)
         .CompositionTarget
         .TransformToDevice
         .M11;
 
-      TrayIcon.Placement = new NativeMethods.Rect { Top = (int)location.Y, Left = (int)location.X, Bottom = (int)(decorator.ActualHeight * dpiScale), Right = (int)(decorator.ActualWidth * dpiScale) };
+      TrayIcon.Placement = new NativeMethods.Rect
+      {
+        Top = (int)location.Y,
+        Left = (int)location.X,
+        Bottom = (int)(eventSource.ActualHeight * dpiScale),
+        Right = (int)(eventSource.ActualWidth * dpiScale)
+      };
+
       TrayIcon.IconMouseEnter(MouseHelper.GetCursorPositionParam());
     }
 
-    private void OnMouseLeave(object sender, MouseEventArgs e)
+    private void OnMouseLeave(MouseEventArgs e)
     {
       e.Handled = true;
       TrayIcon.IconMouseLeave(MouseHelper.GetCursorPositionParam());
     }
 
-    private void OnMouseMove(object sender, MouseEventArgs e)
+    private void OnMouseMove(MouseEventArgs e)
     {
       e.Handled = true;
       TrayIcon.IconMouseMove(MouseHelper.GetCursorPositionParam());
