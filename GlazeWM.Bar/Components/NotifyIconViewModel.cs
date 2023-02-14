c2d13d7176bb145a4eb325b6121292ffb0418e77
Lars Berger <lars.berger@yahoo.com>
2023-02-14 18:06:20 +0800

    feat(bar): add mouse commands to `NotifyIconViewModel`

--- GlazeWM.Bar/Components/NotifyIconViewModel.cs
+++ GlazeWM.Bar/Components/NotifyIconViewModel.cs
@@ -1,5 +1,9 @@
-
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 using GlazeWM.Bar.Common;
+using ManagedShell.Common.Helpers;
+using ManagedShell.Interop;
 
 namespace GlazeWM.Bar.Components
 {
@@ -7,66 +11,76 @@ namespace GlazeWM.Bar.Components
   {
     public ManagedShell.WindowsTray.NotifyIcon TrayIcon { get; set; }
 
+    public ICommand OnMouseUpCommand => new RelayCommand<object>(OnMouseUp);
+    public ICommand OnMouseDownCommand => new RelayCommand<object>(OnMouseDown);
+    public ICommand OnMouseEnterCommand => new RelayCommand<object>(OnMouseEnter);
+    public ICommand OnMouseLeaveCommand => new RelayCommand<object>(OnMouseLeave);
+    public ICommand OnMouseMoveCommand => new RelayCommand<object>(OnMouseMove);
+
     public NotifyIconViewModel(ManagedShell.WindowsTray.NotifyIcon trayIcon)
     {
       TrayIcon = trayIcon;
     }
 
-    // private void NotifyIcon_OnMouseDown(object sender, MouseButtonEventAblankrgs e)
-    // {
-    //   e.Handled = tblankrue;
-    //   if (!Keyboard.IsKeyDown(Key.LeftAlt))
-    //   {
-    //     TrayIcon?.IconMouseDown(e.ChangedButton, MouseHelper.GetCursorPositionParam(), System.Windows.Forms.SystemInformation.DoubleClickTime);
-    //   }
-    // }
+    private void OnMouseDown(object sender, MouseButtonEventArgs e)
+    {
+      e.Handled = true;
 
-    // private void NotifyIcon_OnMouseUp(object sender, MouseButtonEventArgs e)
-    // {
-    //   e.Handled = true;
-    //   if (Keyboard.IsKeyDown(Key.LeftAlt))
-    //   {
-    //     if (TrayIcon.IsPinned)
-    //     {
-    //       TrayIcon.Unpin();
-    //     }
-    //     else
-    //     {
-    //       TrayIcon.Pin();
-    //     }
-    //   }
-    //   else
-    //   {
-    //     TrayIcon?.IconMouseUp(e.ChangedButton, MouseHelper.GetCursorPositionParam(), System.Windows.Forms.SystemInformation.DoubleClickTime);
-    //   }
-    // }
+      if (!Keyboard.IsKeyDown(Key.LeftAlt))
+        TrayIcon.IconMouseDown(
+          e.ChangedButton,
+          MouseHelper.GetCursorPositionParam(),
+          System.Windows.Forms.SystemInformation.DoubleClickTime
+        );
+    }
 
-    // private void NotifyIcon_OnMouseEnter(object sender, MouseEventArgs e)
-    // {
-    //   e.Handled = true;
+    private void OnMouseUp(object sender, MouseButtonEventArgs e)
+    {
+      e.Handled = true;
 
-    //   if (TrayIcon != null)
-    //   {
-    //     // update icon position for Shell_NotifyIconGetRect
-    //     var sendingDecorator = sender as Decorator;
-    //     var location = sendingDecorator.PointToScreen(new Point(0, 0));
-    //     var dpiScale = PresentationSource.FromVisual(this).CompositionTarget.TransformToDevice.M11;
+      if (Keyboard.IsKeyDown(Key.LeftAlt))
+      {
+        if (TrayIcon.IsPinned)
+          TrayIcon.Unpin();
+        else
+          TrayIcon.Pin();
+        return;
+      }
 
-    //     TrayIcon.Placement = new NativeMethods.Rect { Top = (int)location.Y, Left = (int)location.X, Bottom = (int)(sendingDecorator.ActualHeight * dpiScale), Right = (int)(sendingDecorator.ActualWidth * dpiScale) };
-    //     TrayIcon.IconMouseEnter(MouseHelper.GetCursorPositionParam());
-    //   }
-    // }
+      TrayIcon.IconMouseUp(
+        e.ChangedButton,
+        MouseHelper.GetCursorPositionParam(),
+        System.Windows.Forms.SystemInformation.DoubleClickTime
+      );
+    }
 
-    // private void NotifyIcon_OnMouseLeave(object sender, MouseEventArgs e)
-    // {
-    //   e.Handled = true;
-    //   TrayIcon?.IconMouseLeave(MouseHelper.GetCursorPositionParam());
-    // }
+    private void OnMouseEnter(object sender, MouseEventArgs e)
+    {
+      e.Handled = true;
 
-    // private void NotifyIcon_OnMouseMove(object sender, MouseEventArgs e)
-    // {
-    //   e.Handled = true;
-    //   TrayIcon?.IconMouseMove(MouseHelper.GetCursorPositionParam());
-    // }
+      // Update icon position for `Shell_NotifyIconGetRect`.
+      var decorator = sender as Decorator;
+      var location = decorator.PointToScreen(new Point(0, 0));
+      var dpiScale = PresentationSource
+        .FromVisual(decorator)
+        .CompositionTarget
+        .TransformToDevice
+        .M11;
+
+      TrayIcon.Placement = new NativeMethods.Rect { Top = (int)location.Y, Left = (int)location.X, Bottom = (int)(decorator.ActualHeight * dpiScale), Right = (int)(decorator.ActualWidth * dpiScale) };
+      TrayIcon.IconMouseEnter(MouseHelper.GetCursorPositionParam());
+    }
+
+    private void OnMouseLeave(object sender, MouseEventArgs e)
+    {
+      e.Handled = true;
+      TrayIcon.IconMouseLeave(MouseHelper.GetCursorPositionParam());
+    }
+
+    private void OnMouseMove(object sender, MouseEventArgs e)
+    {
+      e.Handled = true;
+      TrayIcon.IconMouseMove(MouseHelper.GetCursorPositionParam());
+    }
   }
 }
