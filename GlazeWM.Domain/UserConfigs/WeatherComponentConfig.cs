df5c91b7ac2d558f952e78694969118f89afad25
blam <notblam@gmail.com>
2023-02-23 06:00:36 -0500

    docs

--- GlazeWM.Domain/UserConfigs/WeatherComponentConfig.cs
+++ GlazeWM.Domain/UserConfigs/WeatherComponentConfig.cs
@@ -23,39 +23,39 @@ namespace GlazeWM.Domain.UserConfigs
     /// </summary>
     public string LabelSun { get; set; } = "";
     /// <summary>
-    /// 
+    /// Icon to represet clear weather at night.
     /// </summary>
     public string LabelMoon { get; set; } = "";
     /// <summary>
-    /// 
+    /// Icon to represent partly cloudy at night.
     /// </summary>
     public string LabelCloudMoon { get; set; } = "";
     /// <summary>
-    /// 
+    /// Icon to represent partly cloudy.
     /// </summary>
     public string LabelCloudSun { get; set; } = "⛅";
     /// <summary>
-    /// 
+    /// Icon to represent light rain at night.
     /// </summary>
     public string LabelCloudMoonRain { get; set; } = "";
     /// <summary>
-    /// 
+    /// Icon to represent light rain.
     /// </summary>
     public string LabelCloudSunRain { get; set; } = "";
     /// <summary>
-    /// 
+    /// Icon to represent heavy rain.
     /// </summary>
     public string LabelCloudRain { get; set; } = "";
     /// <summary>
-    /// 
+    /// Icon to represent snow.
     /// </summary>
     public string LabelSnowflake { get; set; } = "❄";
     /// <summary>
-    /// 
+    /// Icon to represent a thunderstorm.
     /// </summary>
     public string LabelThunderstorm { get; set; } = "";
     /// <summary>
-    /// 
+    /// Icon to represent heavy clouds.  
     /// </summary>
     public string LabelCloud { get; set; } = "☁";
     /// <summary>
