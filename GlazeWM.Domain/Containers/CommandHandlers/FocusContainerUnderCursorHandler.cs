456f668d85692be6983de5370d66afc54e978577
blam <notblam@gmail.com>
2023-03-01 09:53:00 -0500

    fix

--- GlazeWM.Domain/Containers/CommandHandlers/FocusContainerUnderCursorHandler.cs
+++ GlazeWM.Domain/Containers/CommandHandlers/FocusContainerUnderCursorHandler.cs
@@ -13,7 +13,7 @@ namespace GlazeWM.Domain.Containers.CommandHandlers
   {
     private readonly Bus _bus;
     private readonly ContainerService _containerService;
-    private readonly List<IntPtr> _focusedWindows = new();
+    private List<IntPtr> _focusedWindows = new();
     private readonly WindowService _windowService;
     public FocusContainerUnderCursorHandler(Bus bus, ContainerService containerService, WindowService windowService)
     {
@@ -31,9 +31,11 @@ namespace GlazeWM.Domain.Containers.CommandHandlers
       if (_focusedWindows.Contains(windowHandle))
         return CommandResponse.Ok;
 
-      // If the FocusedWindows list didn't contain the window, this must be a new window being focused.
-      _focusedWindows.Clear();
-      _focusedWindows.Add(windowHandle);
+      // If the FocusedWindows list didn't contain the window, this might be a new window being focused.
+      var newFocusedWindowsList = new List<IntPtr>
+      {
+        windowHandle
+      };
 
       // Check if the window is the main window or a child window.
       var parentWindow = GetParent(windowHandle);
@@ -42,19 +44,30 @@ namespace GlazeWM.Domain.Containers.CommandHandlers
       while (parentWindow != IntPtr.Zero)
       {
         windowHandle = parentWindow;
-        _focusedWindows.Add(windowHandle);
+        newFocusedWindowsList.Add(windowHandle);
         parentWindow = GetParent(windowHandle);
       }
 
-      var foundWindow = _windowService
-        .GetWindows()
-        .FirstOrDefault(window => window.Handle == windowHandle);
-
-      if (foundWindow is not null)
+      // Determine if the same window or a new window is being hovered
+      if (_focusedWindows.Contains(newFocusedWindowsList.Last()))
       {
-        SetForegroundWindow(foundWindow.Handle);
-        SetFocus(foundWindow.Handle);
+        _focusedWindows = _focusedWindows.Union(newFocusedWindowsList).ToList();
       }
+      else
+      {
+        _focusedWindows = newFocusedWindowsList;
+        var foundWindow = _windowService
+          .GetWindows()
+          .FirstOrDefault(window => window.Handle == windowHandle);
+
+        if (foundWindow is not null)
+        {
+          // Focus the new window
+          SetForegroundWindow(foundWindow.Handle);
+          SetFocus(foundWindow.Handle);
+        }
+      }
+
       return CommandResponse.Ok;
     }
   }
