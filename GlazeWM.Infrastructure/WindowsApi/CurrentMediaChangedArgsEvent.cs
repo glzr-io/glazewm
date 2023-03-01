67f8dd675d67d086006131217f407ca2f14a5a2d
AreamanM <aryaman.marathe@gmail.com>
2023-03-01 12:12:40 +0000

    feat(infra): SystemMediaInformation

---
+++ GlazeWM.Infrastructure/WindowsApi/CurrentMediaChangedArgsEvent.cs
@@ -0,0 +1,9 @@
+namespace GlazeWM.Infrastructure.WindowsApi
+{
+  public class CurrentMediaChangedEventArgs
+  {
+    public string AlbumTitle { get; set; }
+    public string Artist { get; set; }
+    public string Title { get; set; }
+  }
+}
