062628e1d668ec2396c5afd5e58b566a0be65158
blam <notblam@gmail.com>
2023-04-11 21:23:11 -0400

    not working

--- GlazeWM.Bar/Components/MediaComponentViewModel.xaml.cs
+++ GlazeWM.Bar/Components/MediaComponentViewModel.xaml.cs
@@ -1,27 +1,42 @@
 using System;
+using System.Diagnostics;
 using System.Reactive.Linq;
 using GlazeWM.Domain.UserConfigs;
+using GlazeWM.Infrastructure;
+using GlazeWM.Infrastructure.WindowsApi;
 
 namespace GlazeWM.Bar.Components
 {
   public class MediaComponentViewModel : ComponentViewModel
   {
     private MediaComponentConfig _config => _componentConfig as MediaComponentConfig;
-    public string Text => FormatLabel();
-    private string FormatLabel()
+    private readonly SystemMediaInformation _mediaInfo = ServiceLocator.GetRequiredService<SystemMediaInformation>();
+    private string _formattedText;
+    public string FormattedText
     {
-      return _config.LabelMusicPlaying;
+      get => _formattedText;
+      set
+      {
+        _formattedText = value;
+        OnPropertyChanged(nameof(FormattedText));
+      }
     }
 
     public MediaComponentViewModel(
       BarViewModel parentViewModel,
       MediaComponentConfig config) : base(parentViewModel, config)
     {
-      Observable
-        .Interval(TimeSpan.FromSeconds(10))
-        .TakeUntil(_parentViewModel.WindowClosing)
-        .Subscribe(_ => OnPropertyChanged(nameof(Text)));
+      _mediaInfo.CurrentMediaChanged += (_, CurrentMediaChangedEventArgs) =>
+      {
+        Debug.WriteLine(CurrentMediaChangedEventArgs.Title + " | " + CurrentMediaChangedEventArgs.Artist);
+        FormattedText = CurrentMediaChangedEventArgs.Title + " | " + CurrentMediaChangedEventArgs.Artist;
+      };
 
+      _mediaInfo.CurrentMediaPlaybackChanged += (_, CurrentMediaPlaybackChanged) =>
+      {
+        Debug.WriteLine(CurrentMediaPlaybackChanged.PlaybackState.ToString());
+        FormattedText = CurrentMediaPlaybackChanged.PlaybackState.ToString();
+      };
     }
   }
 }
