fc560425ee45d82349e1ed0c3e251c147f686759
blam <notblam@gmail.com>
2023-04-20 19:08:19 -0400

    wip

---
+++ GlazeWM.Bar/Components/BarSeperatorComponent.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows.Controls;
+
+namespace GlazeWM.Bar.Components
+{
+  /// <summary>
+  /// Interaction logic for BarSeperatorComponent.xaml
+  /// </summary>
+  public partial class BarSeperatorComponent : UserControl
+  {
+    public BarSeperatorComponent()
+    {
+      InitializeComponent();
+    }
+  }
+}
