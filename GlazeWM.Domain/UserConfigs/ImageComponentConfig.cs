9cbbfdf773d41f4ff3b51e0499400e2eee48d314
blam <notblam@gmail.com>
2023-05-28 20:37:36 -0400

    more cleanup

--- GlazeWM.Domain/UserConfigs/ImageComponentConfig.cs
+++ GlazeWM.Domain/UserConfigs/ImageComponentConfig.cs
@@ -5,6 +5,6 @@ namespace GlazeWM.Domain.UserConfigs
     /// <summary>
     /// Path to image
     /// </summary>
-    public string Source { get; set; } = "D:\\Code\\.dotfiles\\Seperators\\arrow_left.png";
+    public string Source { get; set; }
   }
 }
