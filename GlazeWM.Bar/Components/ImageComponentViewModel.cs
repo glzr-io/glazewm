9fdd1f097f4a28014ce9bef4fb63e1a9ad379519
blam <notblam@gmail.com>
2023-05-29 01:15:35 -0400

    cleanup

--- GlazeWM.Bar/Components/ImageComponentViewModel.cs
+++ GlazeWM.Bar/Components/ImageComponentViewModel.cs
@@ -1,17 +1,9 @@
-using GlazeWM.Domain.Containers;
 using GlazeWM.Domain.UserConfigs;
-using GlazeWM.Infrastructure;
-using GlazeWM.Infrastructure.Bussing;
 
 namespace GlazeWM.Bar.Components
 {
   public class ImageComponentViewModel : ComponentViewModel
   {
-    private readonly Bus _bus = ServiceLocator.GetRequiredService<Bus>();
-    private readonly ContainerService _containerService =
-      ServiceLocator.GetRequiredService<ContainerService>();
-    private readonly CommandParsingService _commandParsingService =
-      ServiceLocator.GetRequiredService<CommandParsingService>();
     private ImageComponentConfig _config => _componentConfig as ImageComponentConfig;
 
     public string Source => _config.Source;
@@ -20,22 +12,6 @@ namespace GlazeWM.Bar.Components
       BarViewModel parentViewModel,
       ImageComponentConfig config) : base(parentViewModel, config)
     {
-    }
-
-    private void InvokeCommand(string commandString)
-    {
-      if (string.IsNullOrEmpty(commandString))
-        return;
-
-      var subjectContainer = _containerService.FocusedContainer;
-
-      var parsedCommand = _commandParsingService.ParseCommand(
-        commandString,
-        subjectContainer
-      );
-
-      // Use `dynamic` to resolve the command type at runtime and allow multiple dispatch.
-      _bus.Invoke((dynamic)parsedCommand);
     }
   }
 }
