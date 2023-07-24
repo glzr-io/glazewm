c15cfffc3e62c2089b9a429d802549243f30e854
Lars Berger <lars.berger@yahoo.com>
2023-07-25 01:24:13 +0800

    feat(app): start wm/cli depending on args passed to `Main`

--- GlazeWM.Application/Program.cs
+++ GlazeWM.Application/Program.cs
@@ -20,196 +20,135 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Console;
 
+//// ******
+//// TODO: Create `ThreadUtils`.
+//// TODO: Create `SingleInstanceMutex`.
+//// TODO: How to forward messages from CLI to IPC server?
+//// TODO: Handle message parsing in IPC server.
+
 namespace GlazeWM.Application
 {
   internal static class Program
   {
-    [Verb("command", HelpText = "Invoke a WM command (eg. `command \"focus workspace 1\"`).")]
-    public class InvokeCommandOptions
-    {
-      [Value(
-        0,
-        MetaName = "meta",
-        Required = true,
-        HelpText = "Input file-name including path"
-      )]
-      public string Command { get; set; }
-
-      [Option(
-        'c',
-        "context-container-id",
-        Required = false,
-        HelpText = "ID of container to use as context."
-      )]
-      public string ContextContainerId { get; set; }
-    }
-
-    [Verb("commit", HelpText = "Subscribe to a WM event (eg. `subscribe -e window_focus,window_close`)")]
-    public class SubscribeOptions
-    {
-      [Option(
-        'e',
-        "events",
-        Required = true,
-        HelpText = "WM events to subscribe to."
-      )]
-      public bool Events { get; set; }
-    }
-
-    [Verb(
-      "containers",
-      HelpText = "Get all containers (monitors, workspaces, windows, split containers)."
-    )]
-    public class GetContainersOptions
-    {
-    }
-
-    [Verb("monitors", HelpText = "Get all monitors.")]
-    public class GetMonitorsOptions
-    {
-    }
-
-    [Verb("workspaces", HelpText = "Get all workspaces.")]
-    public class GetWorkspacesOptions
-    {
-    }
-
-    [Verb("windows", HelpText = "Get all windows.")]
-    public class GetWindowsOptions
-    {
-    }
-
-    private const string APP_GUID = "325d0ed7-7f60-4925-8d1b-aa287b26b218";
-
     /// <summary>
-    ///  The main entry point for the application.
+    /// The main entry point for the application. The thread must be an STA
+    /// thread in order to run a message loop.
     /// </summary>
     [STAThread]
-    private static void Main(string[] args)
+    private static int Main(string[] args)
     {
-      // TODO: Remove use of .NET generic host.
-      // TODO: Build shared/base `ServiceCollection` which is passed to each application.
+      using var isSingleInstance = new SingleInstanceMutex();
 
-      Debug.WriteLine("Application started.");
-      Console.WriteLine($"Application started. {args}");
-      var splitArgs = string.Join(",", args).Split(" ");
-      Console.WriteLine($"Application started. {splitArgs}");
+      var parsedArgs = Parser.Default.ParseArguments<StartWmOptions>(args);
 
-      var x = Parser.Default.ParseArguments<
-        InvokeCommandOptions,
-        SubscribeOptions
-      // GetContainersOptions,
-      // GetMonitorsOptions,
-      // GetWorkspacesOptions,
-      // GetWindowsOptions
-      >(args);
-
-      // MessageBox.Show($"x.Value.Command {x.Value.Command}");
-      // MessageBox.Show($"x.Value.Command {x.Value.Command1}");
-      // MessageBox.Show($"x.Value.Command {x.Value.Command2}");
-      // MessageBox.Show($"x.Value.Command {x.Value.Command3}");
-      // Console.WriteLine($"x.Value.Command {x.Value.Command}");
-      // Debug.WriteLine($"x.Value.Command {x.Value.Command}");
-      // Environment.Exit(1);
-
-      // var x = Parser.Default.(splitArgs);
-      // MapResult(
-      //   (InvokeCommandOptions opts) => Debug.WriteLine("jfdiosao"),
-      //   (SubscribeOptions opts) => Debug.WriteLine("jfdiosao"),
-      //   (GetContainersOptions opts) => Debug.WriteLine("jfdiosao"),
-      //   _ => Debug.WriteLine("jfdiosao")
-      // );
-
-      // Prevent multiple app instances using a global UUID mutex.
-      using var mutex = new Mutex(false, "Global\\" + APP_GUID, out var createdNew);
-
-      // return Parser.Default.ParseArguments<
-      //   StartupOptions,
-      //   InvokeCommandOptions
-      //   SubscribeOptions,
-      //   GetContainersOptions,
-      //   GetMonitorsOptions,
-      //   GetWorkspacesOptions,
-      //   GetWindowsOptions
-      // >(args).MapResult(
-      //   (StartupOptions opts) => WindowManager.Startup(opts, createdNew),
-      //   _ => Debug.WriteLine("jfdiosao"),
-      // );
-
-      if (!mutex.WaitOne(0, false))
-      {
-        Debug.Write(
-          "Application is already running. Only one instance of this application is allowed."
-        );
-        return;
-      }
-
-      var host = CreateHost(args);
-      ServiceLocator.Provider = host.Services;
-
-      var startup = ServiceLocator.GetRequiredService<Startup>();
-      startup.Run();
+      return parsedArgs.MapResult(
+        (StartWmOptions opts) => StartWm(opts, isSingleInstance),
+        _ => StartCli(parsedArgs, isSingleInstance)
+      );
     }
 
-    private static IHost CreateHost(string[] args)
+    private int StartWm(StartWmOptions opts, bool isSingleInstance)
     {
-      return Host.CreateDefaultBuilder()
-        .ConfigureAppConfiguration(appConfig =>
+      if (!isSingleInstance)
+        return ExitCode.Error;
+
+      ServiceLocator.Provider = BuildWmServiceProvider();
+
+      var (barService, ipcServer, windowManager) =
+        ServiceLocator.Provider.GetRequiredServices<
+          BarService,
+          IpcServer,
+          WindowManager
+        >();
+
+      ThreadUtils.CreateSTA('GlazeWMBar', () => _barService.StartApp());
+      ThreadUtils.Create('GlazeWMIPC', () => _ipcServer.StartServer());
+
+      // Run window manager on the main thread.
+      windowManager.Start();
+    }
+
+    private void StartCli(ParsedArgs<object> parsedArgs, bool isSingleInstance)
+    {
+      if (isSingleInstance)
+        return ExitCode.Error;
+
+      ServiceLocator.Provider = BuildCliServiceProvider();
+
+      var cli = ServiceLocator.Provider.GetRequiredService<Cli>();
+      cli.Start(parsedArgs);
+    }
+
+    private ServiceProvider BuildWmServiceProvider()
+    {
+      var services = new ServiceCollection();
+      services.AddLoggingService();
+      services.AddExceptionHandler();
+      services.AddInfrastructureServices();
+      services.AddDomainServices();
+      services.AddBarServices();
+      services.AddSingleton<WindowManager>();
+
+      return services.BuildServiceProvider();
+    }
+
+    private ServiceProvider BuildCliServiceProvider()
+    {
+      var services = new ServiceCollection();
+      services.AddLoggingService();
+      services.AddSingleton<IpcClient>();
+      services.AddSingleton<Cli>();
+
+      return services.BuildServiceProvider();
+    }
+  }
+
+  public static class DependencyInjection
+  {
+    public static IServiceCollection AddLoggingService(this IServiceCollection services)
+    {
+      return services.AddLogging(builder =>
+      {
+        builder.ClearProviders();
+        builder.AddConsole();
+        builder.SetMinimumLevel(LogLevel.Debug);
+        builder.AddFormatter<CustomLoggerFormatter, ConsoleLoggerFormatOptions>();
+      });
+    }
+
+    public static IServiceCollection AddExceptionHandler(this IServiceCollection services)
+    {
+      return services
+        .AddOptions<ExceptionHandlingOptions>()
+        .Configure<Bus, ContainerService>((options, bus, containerService) =>
         {
-          appConfig.AddCommandLine(args, new Dictionary<string, string>
+          options.ErrorLogPath = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+            "./.glaze-wm/errors.log"
+          );
+
+          options.ErrorLogMessageDelegate = (Exception exception) =>
           {
-            // Map CLI argument `--config` to `UserConfigPath` configuration key.
-            {"--config", "UserConfigPath"}
-          });
-        })
-        .ConfigureServices((_, services) =>
-        {
-          services.AddInfrastructureServices();
-          services.AddDomainServices();
-          services.AddBarServices();
-          services.AddInterprocessServices();
-          services.AddSingleton<Startup>();
+            var serializeOptions = JsonParser.OptionsFactory(
+              (options) => options.Converters.Add(new JsonContainerConverter())
+            );
 
-          // Configure exception handler.
-          services
-            .AddOptions<ExceptionHandlingOptions>()
-            .Configure<Bus, ContainerService>((options, bus, containerService) =>
-            {
-              options.ErrorLogPath = Path.Combine(
-                Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
-                "./.glaze-wm/errors.log"
-              );
+            var stateDump = JsonParser.ToString(
+              containerService.ContainerTree,
+              serializeOptions
+            );
 
-              options.ErrorLogMessageDelegate = (Exception exception) =>
-              {
-                var serializeOptions = JsonParser.OptionsFactory(
-                  (options) => options.Converters.Add(new JsonContainerConverter())
-                );
+            // History of latest command invocations. Most recent is first.
+            var commandHistory = bus.CommandHistory
+              .Select(command => command.Name)
+              .Reverse();
 
-                var stateDump = JsonParser.ToString(
-                  containerService.ContainerTree,
-                  serializeOptions
-                );
-
-                // History of latest command invocations. Most recent is first.
-                var commandHistory = bus.CommandHistory
-                  .Select(command => command.Name)
-                  .Reverse();
-
-                return $"{DateTime.Now}\n"
-                  + $"{exception}\n"
-                  + $"Command history: {string.Join(", ", commandHistory)} \n"
-                  + $"State dump: {stateDump}\n\n";
-              };
-            });
-        })
-        .ConfigureLogging(builder =>
-        {
-          builder.ClearProviders();
-          builder.AddConsole(options => options.FormatterName = "customFormatter")
-            .AddConsoleFormatter<LogFormatter, ConsoleFormatterOptions>();
-        })
-        .Build();
+            return $"{DateTime.Now}\n"
+              + $"{exception}\n"
+              + $"Command history: {string.Join(", ", commandHistory)} \n"
+              + $"State dump: {stateDump}\n\n";
+          };
+        });
     }
   }
 }
