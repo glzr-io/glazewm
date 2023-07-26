9ba24e4fa8e79eec33a31bca0acbfd190e18ce13
Lars Berger <lars.berger@yahoo.com>
2023-07-27 05:08:52 +0800

    feat(domain): create `WmStartupOptions`

---
+++ GlazeWM.Domain/Common/WmStartupOptions.cs
@@ -0,0 +1,15 @@
+﻿using CommandLine;
+
+namespace GlazeWM.Domain.Common
+{
+  public class WmStartupOptions
+  {
+    [Option(
+      'c',
+      "config",
+      Required = false,
+      HelpText = "Custom path to user config file."
+    )]
+    public string ConfigPath { get; set; }
+  }
+}
