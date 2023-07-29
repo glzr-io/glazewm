5a17136afb29944001520b6132221ac9da6a5682
Lars Berger <lars.berger@yahoo.com>
2023-07-29 17:39:47 +0800

    feat(infra): add `FriendlyName` to `Event`

--- GlazeWM.Infrastructure/Bussing/Event.cs
+++ GlazeWM.Infrastructure/Bussing/Event.cs
@@ -1,7 +1,18 @@
+using GlazeWM.Infrastructure.Utils;
+
 namespace GlazeWM.Infrastructure.Bussing
 {
   public class Event
   {
     public string Name => GetType().Name;
+
+    public string FriendlyName
+    {
+      get
+      {
+        var shortName = Name.Replace("Event", "");
+        return CasingUtil.PascalToSnake(shortName);
+      }
+    }
   }
 }
