216d6134eaf0c625b9885b26f4382c0f169a19a6
Lars Berger <lars.berger@yahoo.com>
2023-08-02 02:56:18 +0800

    refactor(cli): simplify client connection

--- GlazeWM.Application.CLI/Cli.cs
+++ GlazeWM.Application.CLI/Cli.cs
@@ -8,53 +8,19 @@ namespace GlazeWM.Application.CLI
     public static void Start(string[] args, int ipcServerPort)
     {
       var client = new WebsocketClient(ipcServerPort);
-      client.ConnectAsync();
-      Console.WriteLine("fjdoisajfdioas");
+      client.Connect();
+      client.ReceiveAsync();
+      client.SendBinaryAsync(string.Join(" ", args));
 
-      client.Connected.Select((_) =>
-      {
-        Console.WriteLine("Done!");
-        // Thread.Sleep(1000);
-        client.SendBinaryAsync(string.Join(" ", args));
-        // client.SendTextAsync(string.Join(" ", args));
-        Console.WriteLine("Done!");
+      // Special handling is needed for subscribe messages (eg. `subscribe -e
+      // window_focused`).
+      var isContinuousOutput = true;
 
-        // client.Messages.TakeWhile(value => true)
-        return client.Messages;
-        // .Subscribe(message => Console.WriteLine(message));
-        // .Subscribe(message =>
-        // {
-        //   Console.WriteLine(message);
-        // });
-      }).Switch()
+      client.Messages.TakeWhile(value => isContinuousOutput)
         .Subscribe(message => Console.WriteLine(message));
-      // var x = client.ConnectAsync();
-      // // client.ReceiveAsync();
-      // // client.ReceiveAsync();
-      // if (x)
-      // {
+
       var _ = Console.ReadLine();
-      // client.DisconnectAndStop();
       client.Disconnect();
-
-      // var response = _websocketClient.Send(message);
-      // var response = message.MapResult(
-      //   (SubscribeMessage message) => HandleSubscribe(message),
-      //   _ => _websocketClient.Send(parsedArgs.Text)
-      // );
-
-      // Console.WriteLine(response.Message);
-
-      // }
     }
-
-    /// <summary>
-    /// Special handling is needed for subscribe messages (eg. `subscribe -e
-    /// window_focused`).
-    /// </summary>
-    // private void HandleSubscribe(SubscribeMessage message)
-    // {
-    //   string line = Console.ReadLine();
-    // }
   }
 }
--- GlazeWM.Infrastructure/Utils/WebsocketClient.cs
+++ GlazeWM.Infrastructure/Utils/WebsocketClient.cs
@@ -8,11 +8,6 @@ namespace GlazeWM.Infrastructure.Utils
   public class WebsocketClient : WsClient
   {
     /// <summary>
-    /// Connection event to websocket server.
-    /// </summary>
-    public readonly Subject<bool> Connected = new();
-
-    /// <summary>
     /// Messages received from websocket server.
     /// </summary>
     public readonly Subject<string> Messages = new();
@@ -34,15 +29,8 @@ namespace GlazeWM.Infrastructure.Utils
         .SetBody();
     }
 
-    public override void OnWsConnected(HttpResponse response)
-    {
-      Console.WriteLine($"Chat WebSocket client connected a new session with Id {Id}");
-      Connected.OnNext(true);
-    }
-
     public override void OnWsReceived(byte[] buffer, long offset, long size)
     {
-      Console.WriteLine($"Incoming: {Encoding.UTF8.GetString(buffer, (int)offset, (int)size)}");
       var message = Encoding.UTF8.GetString(buffer, (int)offset, (int)size);
       Messages.OnNext(message);
     }
