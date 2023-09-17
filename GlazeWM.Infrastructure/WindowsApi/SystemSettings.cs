47a06c26a25081e2366cf842fe9b89c5ae667b21
Lars Berger <lars.berger@yahoo.com>
2023-09-17 18:05:08 +0800

    feat: remove unused `AreWindowAnimationsEnabled`

--- GlazeWM.Infrastructure/WindowsApi/SystemSettings.cs
+++ GlazeWM.Infrastructure/WindowsApi/SystemSettings.cs
@@ -5,23 +5,6 @@ namespace GlazeWM.Infrastructure.WindowsApi
   public static class SystemSettings
   {
     /// <summary>
-    /// Whether window transition animations are enabled (on minimize, close, etc).
-    /// </summary>
-    public static bool AreWindowAnimationsEnabled()
-    {
-      var animationInfo = AnimationInfo.Create(false);
-
-      SystemParametersInfo(
-        SystemParametersInfoFlags.GetAnimation,
-        animationInfo.CallbackSize,
-        ref animationInfo,
-        0
-      );
-
-      return animationInfo.IsEnabled;
-    }
-
-    /// <summary>
     /// Modify global setting for whether window transition animations are enabled.
     /// </summary>
     public static void SetWindowAnimationsEnabled(bool enabled)
