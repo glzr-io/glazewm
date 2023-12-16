4a7c5b74bfac59004f78556aafdaa8504f96a808
Fuann-Kinoko <838832306@qq.com>
2023-12-17 01:20:53 +0800

    fix cursor_follows_focus in monocle mode

--- GlazeWM.Domain/Containers/CommandHandlers/CenterCursorOnContainerHandler.cs
+++ GlazeWM.Domain/Containers/CommandHandlers/CenterCursorOnContainerHandler.cs
@@ -1,3 +1,4 @@
+using GlazeWM.Domain.Workspaces;
 using GlazeWM.Domain.Containers.Commands;
 using GlazeWM.Domain.UserConfigs;
 using GlazeWM.Infrastructure.Bussing;
@@ -20,6 +21,16 @@ namespace GlazeWM.Domain.Containers.CommandHandlers
 
       if (!isEnabled || command.TargetContainer.IsDetached() || command.TargetContainer.FocusIndex < 0)
         return CommandResponse.Ok;
+      // in monocle mode, center cursor on the current workspace instead of targetContainer
+      var targetWorkspace = WorkspaceService.GetWorkspaceFromChildContainer(command.TargetContainer);
+      if (targetWorkspace.isMonocle)
+      {
+        var rect = targetWorkspace.ToRect();
+        var x = rect.X + (rect.Width / 2);
+        var y = rect.Y + (rect.Height / 2);
+        SetCursorPos(x, y);
+        return CommandResponse.Ok;
+      }
 
       var targetRect = command.TargetContainer.ToRect();
 
