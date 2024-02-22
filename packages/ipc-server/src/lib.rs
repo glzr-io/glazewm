6f4631bc113be65b41c3ac3358c946c18460bfeb
Lars Berger <lars.berger@yahoo.com>
2024-02-23 04:33:42 +0800

    feat: add demo ipc server

--- packages/ipc-server/src/lib.rs
+++ packages/ipc-server/src/lib.rs
@@ -1,3 +1,22 @@
-pub fn add(a: i32, b: i32) -> i32 {
-  a + b
+use std::net::TcpListener;
+use std::thread::spawn;
+
+use tungstenite::accept;
+
+pub fn start_server() {
+  let server = TcpListener::bind("127.0.0.1:6123").unwrap();
+
+  for stream in server.incoming() {
+    spawn(move || {
+      let mut websocket = accept(stream.unwrap()).unwrap();
+      loop {
+        let msg = websocket.read().unwrap();
+
+        // We do not want to send back ping/pong messages.
+        if msg.is_binary() || msg.is_text() {
+          websocket.send(msg).unwrap();
+        }
+      }
+    });
+  }
 }
