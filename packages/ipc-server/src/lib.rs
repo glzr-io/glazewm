74166b6afe84dff2d35bd582b88bf90a9631d62c
Lars Berger <lars.berger@yahoo.com>
2024-02-23 05:04:52 +0800

    feat: pass message_tx to ipc server

--- packages/ipc-server/src/lib.rs
+++ packages/ipc-server/src/lib.rs
@@ -1,22 +1,45 @@
 use std::net::TcpListener;
-use std::thread::spawn;
 
+use tokio::sync::mpsc::UnboundedSender;
 use tungstenite::accept;
 
-pub fn start_server() {
-  let server = TcpListener::bind("127.0.0.1:6123").unwrap();
+pub enum IpcMessage {
+  Monitors,
+  Windows,
+  InvokeCommand,
+  Subscribe,
+}
 
-  for stream in server.incoming() {
-    spawn(move || {
-      let mut websocket = accept(stream.unwrap()).unwrap();
-      loop {
-        let msg = websocket.read().unwrap();
+pub struct IpcServer {
+  message_tx: UnboundedSender<IpcMessage>,
+}
 
-        // We do not want to send back ping/pong messages.
-        if msg.is_binary() || msg.is_text() {
-          websocket.send(msg).unwrap();
+impl IpcServer {
+  pub fn new(message_tx: UnboundedSender<IpcMessage>) -> Self {
+    Self { message_tx }
+  }
+
+  pub async fn start(&self) {
+    let server = TcpListener::bind("127.0.0.1:6123").unwrap();
+
+    for stream in server.incoming() {
+      tokio::spawn(async move {
+        let mut websocket = accept(stream.unwrap()).unwrap();
+        loop {
+          let msg = websocket.read().unwrap();
+
+          // We do not want to send back ping/pong messages.
+          if msg.is_binary() || msg.is_text() {
+            websocket.send(msg).unwrap();
+          }
         }
-      }
-    });
+      });
+    }
+  }
+
+  pub async fn stop(&self) {
+    // Stop the server
   }
 }
+
+pub fn start_server() {}
