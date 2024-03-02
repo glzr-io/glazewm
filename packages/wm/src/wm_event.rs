a43d76f413a5fbac0bd0dab26570106a4735ba78
Lars Berger <lars.berger@yahoo.com>
2024-03-03 06:15:26 +0800

    feat: add separate structs for wm events

--- packages/wm/src/wm_event.rs
+++ packages/wm/src/wm_event.rs
@@ -5,46 +5,70 @@ use crate::{
   containers::Container,
 };
 
+pub struct BindingModeChangedEvent {
+  pub new_binding_mode: String,
+}
+
+pub struct FocusChangedEvent {
+  pub focused_container: Container,
+}
+
+pub struct FocusedContainerMovedEvent {
+  pub focused_container: Container,
+}
+
+pub struct MonitorAddedEvent {
+  pub added_monitor: Container,
+}
+
+pub struct MonitorRemovedEvent {
+  pub removed_id: Uuid,
+  pub removed_device_name: String,
+}
+
+pub struct NativeFocusSyncedEvent {
+  pub focused_container: Container,
+}
+
+pub struct TilingDirectionChangedEvent {
+  pub new_tiling_direction: TilingDirection,
+}
+
+pub struct WindowManagedEvent {
+  pub managed_window: Container,
+}
+
+pub struct WindowUnmanagedEvent {
+  pub unmanaged_id: Uuid,
+  pub unmanaged_handle: WindowHandle,
+}
+
+pub struct WorkspaceActivatedEvent {
+  pub activated_workspace: Container,
+}
+
+pub struct WorkspaceDeactivatedEvent {
+  pub deactivated_id: Uuid,
+  pub deactivated_name: String,
+}
+
+pub struct WorkingAreaResizedEvent {
+  pub affected_monitor: Container,
+}
+
 #[derive(Debug)]
 pub enum WmEvent {
-  BindingModeChanged {
-    new_binding_mode: String,
-  },
-  FocusChanged {
-    focused_container: Container,
-  },
-  FocusedContainerMoved {
-    focused_container: Container,
-  },
-  MonitorAdded {
-    added_monitor: Container,
-  },
-  MonitorRemoved {
-    removed_id: Uuid,
-    removed_device_name: String,
-  },
-  NativeFocusSynced {
-    focused_container: Container,
-  },
-  TilingDirectionChanged {
-    new_tiling_direction: TilingDirection,
-  },
+  BindingModeChanged(BindingModeChangedEvent),
+  FocusChanged(FocusChangedEvent),
+  FocusedContainerMoved(FocusedContainerMovedEvent),
+  MonitorAdded(MonitorAddedEvent),
+  MonitorRemoved(MonitorRemovedEvent),
+  NativeFocusSynced(NativeFocusSyncedEvent),
+  TilingDirectionChanged(TilingDirectionChangedEvent),
   UserConfigReloaded,
-  WindowManaged {
-    managed_window: Container,
-  },
-  WindowUnmanaged {
-    unmanaged_id: Uuid,
-    unmanaged_handle: WindowHandle,
-  },
-  WorkspaceActivated {
-    activated_workspace: Container,
-  },
-  WorkspaceDeactivated {
-    deactivated_id: Uuid,
-    deactivated_name: String,
-  },
-  WorkingAreaResized {
-    affected_monitor: Container,
-  },
+  WindowManaged(WindowManagedEvent),
+  WindowUnmanaged(WindowUnmanagedEvent),
+  WorkspaceActivated(WorkspaceActivatedEvent),
+  WorkspaceDeactivated(WorkspaceDeactivatedEvent),
+  WorkingAreaResized(WorkingAreaResizedEvent),
 }
