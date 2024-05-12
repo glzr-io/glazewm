ada8b1579e655dbc587706e92434c1cbd3c65dbe
Lars Berger <lars.berger@yahoo.com>
2024-05-12 18:14:56 +0800

    feat: add `deactivate_workspace` command

---
+++ packages/wm/src/workspaces/commands/deactivate_workspace.rs
@@ -0,0 +1,22 @@
+use crate::{
+  containers::{commands::detach_container, traits::CommonGetters},
+  wm_event::WmEvent,
+  wm_state::WmState,
+  workspaces::Workspace,
+};
+
+/// Deactivates a given workspace. This removes the container from its
+/// parent monitor and emits a `WorkspaceDeactivated` event.
+pub fn deactivate_workspace(
+  workspace: Workspace,
+  state: &WmState,
+) -> anyhow::Result<()> {
+  detach_container(workspace.clone().into())?;
+
+  state.emit_event(WmEvent::WorkspaceDeactivated {
+    deactivated_id: workspace.id(),
+    deactivated_name: workspace.config().name,
+  });
+
+  Ok(())
+}
--- packages/wm/src/workspaces/commands/mod.rs
+++ packages/wm/src/workspaces/commands/mod.rs
@@ -1,3 +1,5 @@
 mod activate_workspace;
+mod deactivate_workspace;
 
 pub use activate_workspace::*;
+pub use deactivate_workspace::*;
