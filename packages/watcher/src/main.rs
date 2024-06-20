56c9cb267bcf93a09358231adc15a58d60ce263f
holby <justin.holby@aboutgolf.com>
2024-06-20 04:03:34 -0400

    feat: watcher (WIP)

--- packages/watcher/src/main.rs
+++ packages/watcher/src/main.rs
@@ -1,23 +1,55 @@
+use anyhow::Context;
+
 use wm::ipc_client::IpcClient;
-use wm::user_config::UserConfig;
+use wm::ipc_server::{ClientResponseData, ServerMessage};
 
 #[tokio::main]
-async fn main() {
-  let config = UserConfig::new(None).await.unwrap();
-  match IpcClient::connect().await {
-    Ok(mut client) => {
-      loop {
-        client.send("Watcher process | PING").await.unwrap();
-        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
-      }
-    },
-    Err(e) => {
-      eprintln!("Failed to connect to IPC server: {:?}", e);
-      loop {
-        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+async fn main() -> anyhow::Result<()> {
+  let mut client = IpcClient::connect().await?;
+
+  let subscription_message =
+    "subscribe -e window_managed window_unmanaged";
+
+  client
+    .send(&subscription_message)
+    .await
+    .context("Failed to send command to IPC server.")?;
+
+  let client_response = client
+    .client_response(&subscription_message)
+    .await
+    .context("Failed to receive response from IPC server.")?;
+
+  match listen_events(&mut client, &client_response).await {
+    Ok(_) => {}
+    Err(err) => eprintln!("{}", err),
+  }
+
+  Ok(())
+}
+
+async fn listen_events(
+  ipc_client: &mut IpcClient,
+  client_response: &ServerMessage,
+) -> anyhow::Result<()> {
+  if let ServerMessage::ClientResponse(client_response) = client_response {
+    match &client_response.data {
+      // For event subscriptions, omit the initial response message and
+      // continuously output subsequent event messages.
+      Some(ClientResponseData::EventSubscribe(data)) => loop {
+        let event_subscription = ipc_client
+          .event_subscription(&data.subscription_id)
+          .await
+          .context("Failed to receive response from IPC server.")?;
+
+        println!("{}", serde_json::to_string(&event_subscription)?);
+      },
+      // For all other messages, output and exit when the first response
+      // message is received.
+      _ => {
+        println!("{}", serde_json::to_string(&client_response)?);
       }
     }
   }
-
-  println!("config: {:?}", config);
+  Ok(())
 }
