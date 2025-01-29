2bceafa9acf685e730f02a3f59e38c4add1fe208
lars-berger <lars.berger@yahoo.com>
2025-01-29 16:10:47 +0800

    linting

--- packages/wm/src/commands/window/manage_window.rs
+++ packages/wm/src/commands/window/manage_window.rs
@@ -204,14 +204,13 @@ fn window_state_to_create(
     ));
   }
 
-  // Initialize windows that can't be resized and popup windows as floating.
-  if !native_window.is_resizable() 
-    || native_window.is_popup() {
+  // Initialize non-resizable windows and popups as floating.
+  if !native_window.is_resizable() || native_window.is_popup() {
     return Ok(WindowState::Floating(
       config.value.window_behavior.state_defaults.floating.clone(),
     ));
   }
-    
+
   Ok(WindowState::default_from_config(&config.value))
 }
 
