988470619bc6c1fc3bf6be0d9e605f20af0a3a79
Lars Berger <lars.berger@yahoo.com>
2025-06-20 11:26:05 +0200

    feat: separate platform modules

--- packages/wm-platform/src/lib.rs
+++ packages/wm-platform/src/lib.rs
@@ -3,22 +3,12 @@
 #![feature(iterator_try_collect)]
 #![feature(once_cell_try)]
 
-mod com;
-mod event_listener;
-mod event_window;
-mod keyboard_hook;
-mod native_monitor;
-mod native_window;
-mod platform;
-mod single_instance;
-mod window_event_hook;
+#[cfg(target_os = "macos")]
+mod macos;
+#[cfg(target_os = "windows")]
+mod windows;
 
-pub use com::*;
-pub use event_listener::*;
-pub use event_window::*;
-pub use keyboard_hook::*;
-pub use native_monitor::*;
-pub use native_window::*;
-pub use platform::*;
-pub use single_instance::*;
-pub use window_event_hook::*;
+#[cfg(target_os = "macos")]
+pub use macos::*;
+#[cfg(target_os = "windows")]
+pub use windows::*;
