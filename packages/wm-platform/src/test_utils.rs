38d657fa68739c6d0c5dfb029fe8d76f7eb95d46
Alex Popiel <tapopiel@gmail.com>
2026-04-05 13:07:41 -0700

    test: support non-functional test instance creation for platform types

---
+++ packages/wm-platform/src/test_utils.rs
@@ -0,0 +1,58 @@
+//! Utilities for testing.
+//!
+//! Available via the `test_utils` Cargo feature.
+use std::sync::{atomic::AtomicBool, Arc};
+
+use crate::platform_impl;
+#[cfg(target_os = "macos")]
+pub use crate::WindowId;
+pub use crate::{Dispatcher, Display, NativeWindow};
+
+impl Dispatcher {
+  /// Creates a mock `Dispatcher` for use in tests.
+  ///
+  /// Calling any methods on the mock is undefined behavior and may panic.
+  #[must_use]
+  pub fn mock() -> Self {
+    Self::new(None, Arc::new(AtomicBool::new(false)))
+  }
+}
+
+impl NativeWindow {
+  /// Creates a mock `NativeWindow` for use in tests.
+  ///
+  /// Calling any methods on the mock is undefined behavior and may panic.
+  #[must_use]
+  pub fn mock() -> Self {
+    #[cfg(target_os = "windows")]
+    {
+      platform_impl::NativeWindow::new(0).into()
+    }
+    #[cfg(target_os = "macos")]
+    {
+      #[allow(invalid_value)]
+      platform_impl::NativeWindow::new(
+        WindowId(0),
+        unsafe { std::mem::zeroed() },
+        unsafe { std::mem::zeroed() },
+      )
+      .into()
+    }
+  }
+}
+
+impl Display {
+  /// Creates a mock `Display` for use in tests.
+  ///
+  /// Calling any methods on the mock is undefined behavior and may panic.
+  #[must_use]
+  pub fn mock() -> Self {
+    Self {
+      #[cfg(target_os = "windows")]
+      inner: platform_impl::Display::new(0),
+      #[cfg(target_os = "macos")]
+      #[allow(invalid_value)]
+      inner: unsafe { std::mem::zeroed() },
+    }
+  }
+}
